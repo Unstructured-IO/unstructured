d429e9b305f2fec56e5e910c481ec8b08c75b45c
asymness <asym1999@gmail.com>
2022-10-03 18:30:30 +0500

    feat: Implement `stage_csv_for_prodigy` brick (#13)
    * Refactor metadata validation and implement stage_csv_for_prodigy brick
    
    * Refactor unit tests for metadata validation and add tests for Prodigy CSV brick
    
    * Add stage_csv_for_prodigy description and example in docs
    
    * Bump version and update changelog
    
    * added _csv_ to function name
    
    * update changelog line to 0.2.1-dev2
    
    Co-authored-by: Matt Robinson <mrobinson@unstructuredai.io>

--- test_unstructured/staging/test_prodigy.py
+++ test_unstructured/staging/test_prodigy.py
@@ -1,4 +1,6 @@
 import pytest
+import csv
+import os
 
 import unstructured.staging.prodigy as prodigy
 from unstructured.documents.elements import Title, NarrativeText
@@ -22,6 +24,41 @@ def metadata_with_id():
 @pytest.fixture
 def metadata_with_invalid_length():
     return [{"score": 0.1}, {"category": "paragraph"}, {"type": "text"}]
+
+
+@pytest.fixture
+def output_csv_file(tmp_path):
+    return os.path.join(tmp_path, "prodigy_data.csv")
+
+
+def test_validate_prodigy_metadata(elements):
+    validated_metadata = prodigy._validate_prodigy_metadata(elements, metadata=None)
+    assert len(validated_metadata) == len(elements)
+    assert all(not data for data in validated_metadata)
+
+
+def test_validate_prodigy_metadata_with_valid_metadata(elements, valid_metadata):
+    validated_metadata = prodigy._validate_prodigy_metadata(elements, metadata=valid_metadata)
+    assert len(validated_metadata) == len(elements)
+
+
+@pytest.mark.parametrize(
+    "invalid_metadata_fixture, exception_message",
+    [
+        ("metadata_with_id", 'The key "id" is not allowed with metadata parameter at index: 1'),
+        (
+            "metadata_with_invalid_length",
+            "The length of metadata parameter does not match with length of elements parameter.",
+        ),
+    ],
+)
+def test_validate_prodigy_metadata_with_invalid_metadata(
+    elements, invalid_metadata_fixture, exception_message, request
+):
+    invalid_metadata = request.getfixturevalue(invalid_metadata_fixture)
+    with pytest.raises(ValueError) as validation_exception:
+        prodigy._validate_prodigy_metadata(elements, invalid_metadata)
+    assert str(validation_exception.value) == exception_message
 
 
 def test_convert_to_prodigy_data(elements):
@@ -54,20 +91,24 @@ def test_convert_to_prodigy_data_with_valid_metadata(elements, valid_metadata):
     assert prodigy_data[1]["meta"] == {"id": elements[1].id, **valid_metadata[1]}
 
 
-@pytest.mark.parametrize(
-    "invalid_metadata_fixture, exception_message",
-    [
-        ("metadata_with_id", 'The key "id" is not allowed with metadata parameter at index: 1'),
-        (
-            "metadata_with_invalid_length",
-            "The length of metadata parameter does not match with length of elements parameter.",
-        ),
-    ],
-)
-def test_convert_to_prodigy_data_with_invalid_metadata(
-    elements, invalid_metadata_fixture, exception_message, request
-):
-    invalid_metadata = request.getfixturevalue(invalid_metadata_fixture)
-    with pytest.raises(ValueError) as validation_exception:
-        prodigy.stage_for_prodigy(elements, invalid_metadata)
-    assert str(validation_exception.value) == exception_message
+def test_stage_csv_for_prodigy(elements, output_csv_file):
+    with open(output_csv_file, "w+") as csv_file:
+        prodigy_csv_string = prodigy.stage_csv_for_prodigy(elements)
+        csv_file.write(prodigy_csv_string)
+
+    fieldnames = ["text", "id"]
+    with open(output_csv_file, "r") as csv_file:
+        csv_rows = csv.DictReader(csv_file)
+        assert all(set(row.keys()) == set(fieldnames) for row in csv_rows)
+
+
+def test_stage_csv_for_prodigy_with_metadata(elements, valid_metadata, output_csv_file):
+    with open(output_csv_file, "w+") as csv_file:
+        prodigy_csv_string = prodigy.stage_csv_for_prodigy(elements, valid_metadata)
+        csv_file.write(prodigy_csv_string)
+
+    fieldnames = set(["text", "id"]).union(*(data.keys() for data in valid_metadata))
+    fieldnames = [fieldname.lower() for fieldname in fieldnames]
+    with open(output_csv_file, "r") as csv_file:
+        csv_rows = csv.DictReader(csv_file)
+        assert all(set(row.keys()) == set(fieldnames) for row in csv_rows)
