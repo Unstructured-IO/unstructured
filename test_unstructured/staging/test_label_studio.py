9592fca09fb5ec3b57253e06ef893bd5bc380ca2
Matt Robinson <mrobinson@unstructuredai.io>
2022-10-03 13:15:15 -0400

    added test for NER annotations

--- test_unstructured/staging/test_label_studio.py
+++ test_unstructured/staging/test_label_studio.py
@@ -96,6 +96,44 @@ def test_stage_with_annotation():
     ]
 
 
+def test_stage_with_annotation_for_ner():
+    element = NarrativeText(text="A big brown bear")
+    annotations = [
+        label_studio.LabelStudioAnnotation(
+            result=[
+                label_studio.LabelStudioResult(
+                    type="labels",
+                    value={"start": 12, "end": 16, "text": "bear", "labels": ["PER"]},
+                    from_name="label",
+                    to_name="text",
+                )
+            ]
+        )
+    ]
+    label_studio_data = label_studio.stage_for_label_studio([element], [annotations])
+    assert label_studio_data == [
+        {
+            "data": {"text": "A big brown bear", "ref_id": "8f458d5d0635df3975ceb9109cef9e12"},
+            "annotations": [
+                {
+                    "result": [
+                        {
+                            "type": "labels",
+                            "value": {"start": 12, "end": 16, "text": "bear", "labels": ["PER"]},
+                            "from_name": "label",
+                            "id": None,
+                            "to_name": "text",
+                            "hidden": False,
+                            "read_only": False,
+                        }
+                    ],
+                    "was_canceled": False,
+                }
+            ],
+        }
+    ]
+
+
 def test_stage_with_annotation_raises_with_mismatched_lengths():
     element = NarrativeText(text="A big brown bear")
     annotations = [
@@ -111,9 +149,7 @@ def test_stage_with_annotation_raises_with_mismatched_lengths():
         )
     ]
     with pytest.raises(ValueError):
-        label_studio_data = label_studio.stage_for_label_studio(
-            [element], [annotations, annotations]
-        )
+        label_studio.stage_for_label_studio([element], [annotations, annotations])
 
 
 def test_stage_with_annotation_raises_with_invalid_type():
