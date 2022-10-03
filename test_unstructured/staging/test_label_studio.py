dbc05206f4a9f827e405759503a1e6ae56fd5a0f
Yuming Long <305248291@qq.com>
2022-10-03 18:09:33 -0400

    test with unittest.mock

--- test_unstructured/staging/test_label_studio.py
+++ test_unstructured/staging/test_label_studio.py
@@ -3,10 +3,28 @@ import unstructured.staging.label_studio as label_studio
 
 from unstructured.documents.elements import Title, NarrativeText
 
+from label_studio_sdk.client import Client
+from unittest.mock import patch
+
 
 @pytest.fixture
 def elements():
     return [Title(text="Title 1"), NarrativeText(text="Narrative 1")]
+
+
+def test_upload_label_studio_data_with_sdk(elements):
+    client = Client(url="http://fake.url", api_key="fake_key")
+    with patch("requests.Session.request") as mocked_post:
+        mocked_post.return_value.status_code = 201
+        client.check_connection()
+        # Create a new project
+        project = client.start_project()
+        project.id = 1
+        # Upload data to the project
+        label_studio_data = label_studio.stage_for_label_studio(elements)
+        project.import_tasks(label_studio_data)
+        _, kwargs = mocked_post.call_args
+        assert kwargs["json"] == label_studio_data
 
 
 def test_convert_to_label_studio_data(elements):
