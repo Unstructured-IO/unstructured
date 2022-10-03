34a6ee95da87f87d6a991faba4385c9395c88eb0
Alan Bertl <alan@unstructured.io>
2022-10-03 09:21:25 -0500

    streamline to just 1 semver regex and drop sed extraction from changelog

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-CHANGELOG_SEMVER="^((?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-(?:(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?:[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)$"
-VERSION_SEMVER="(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-((0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?"
-set -o pipefail
-# Pull out most recent semver from CHANGELOG.md
-LAST_VERSION=$(sed 's/#\+ //' CHANGELOG.md | grep -o -m 1 -E "${CHANGELOG_SEMVER}")
+SEMVER="(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-((0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?"
+# Pull out semver appearing earliest in CHANGELOG.md
+LAST_VERSION=$(grep -o -m 1 -E "${SEMVER}" CHANGELOG.md)
 if [[ $LAST_VERSION ]];
 then
 	# Replace semver in __version__.py with semver obtained from CHANGELOG.md
-	sed -i -r "s/$VERSION_SEMVER/$LAST_VERSION/" unstructured/__version__.py
+	sed -i -r "s/$SEMVER/$LAST_VERSION/" unstructured/__version__.py
 fi
