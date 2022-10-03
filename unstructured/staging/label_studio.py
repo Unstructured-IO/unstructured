e1c5788ab059c395c08371bf33cf8e7fa8e3f783
Matt Robinson <mrobinson@unstructuredai.io>
2022-10-03 10:12:51 -0400

    added types for label studio annotations

--- unstructured/staging/label_studio.py
+++ unstructured/staging/label_studio.py
@@ -1,9 +1,50 @@
-from typing import Dict, List
+from dataclasses import dataclass
+from typing import Any, Dict, List, Union
 
 from unstructured.documents.elements import Text
 
 
 LABEL_STUDIO_TYPE = List[Dict[str, Dict[str, str]]]
+
+
+@dataclass
+class LabelStudioResult:
+    """Class for representing a LabelStudio annotation result.
+    ref: https://labelstud.io/guide/export.html#Label-Studio-JSON-format-of-annotated-tasks"""
+
+    id: str
+    type: str  # The type of tag used to annotate the task
+    value: Dict[str, Any]  # The values for
+    # NOTE(robinson) - See here for a list of object and control tags. Also provides the formats
+    # for the value parameter
+    # ref: https://labelstud.io/tags/
+    from_name: str = "tag"  # Name of the source object tag
+    to_name: str = "text"  # Name of the destination control tag
+
+
+@dataclass
+class LabelStudioReview:
+    """Class for representing a LablStudio review. Reviews are only available in the
+    Enterprise offering.
+    ref: https://labelstud.io/guide/export.html#Label-Studio-JSON-format-of-annotated-tasks"""
+
+    id: str
+    # NOTE(robinson) - created_by is a dictionary containing the user ID, email, first name,
+    # and last name of the reviewer
+    created_by: Dict[str, Union[str, int]]
+    accepted: bool
+
+
+@dataclass
+class LabelStudioAnnotation:
+    """Class for representing LabelStudio annotations.
+    ref: https://labelstud.io/guide/export.html#Label-Studio-JSON-format-of-annotated-tasks"""
+
+    id: str
+    lead_time: float  # Time in seconds to label the task
+    result: List[LabelStudioResult]  # An array consisting of the annotation results
+    completed_by: int  # User ID for the user who completed the task
+    was_canceled: bool = False  # Indicates whether or not the annotation was canceled
 
 
 def stage_for_label_studio(
