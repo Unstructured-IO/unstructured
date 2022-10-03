d90ea12c5b48ff213c23c3355742a565af8399c1
Alan Bertl <alan@unstructured.io>
2022-10-03 10:28:52 -0500

    add check option and error handling

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,9 +1,54 @@
 #!/bin/bash
+
+CHECK=0
+while getopts ":c" opt; do
+  case $opt in
+    c)
+      CHECK=1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG. Use -c to show changes without applying, use no options to apply changes." >&2
+      exit 1
+      ;;
+  esac
+done
+
+CHANGELOGFILE="CHANGELOG.md"
+VERSIONFILE="unstructured/__version__.py"
 SEMVER="(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-((0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?"
-# Pull out semver appearing earliest in CHANGELOG.md
-LAST_VERSION=$(grep -o -m 1 -E "${SEMVER}" CHANGELOG.md)
-if [[ $LAST_VERSION ]];
+# Pull out semver appearing earliest in CHANGELOGFILE
+LAST_VERSION=$(grep -o -m 1 -E "${SEMVER}" "$CHANGELOGFILE")
+CURR_VERSION=$(grep -o -m 1 -E "${SEMVER}" "$VERSIONFILE")
+if [ -z $CURR_VERSION ];
 then
-	# Replace semver in __version__.py with semver obtained from CHANGELOG.md
-	sed -i -r "s/$SEMVER/$LAST_VERSION/" unstructured/__version__.py
+	# No match to semver regex in VERSIONFILE, so nothing to replace
+	printf "Error: No semver version found in file $VERSIONFILE.\n";
+	exit 1;
+fi
+if [ -z $LAST_VERSION ];
+then
+	# No match to semver regex in CHANGELOGFILE, so no version to go from
+	printf "Error: Unable to find latest version from $CHANGELOGFILE.\n"
+	exit 1
+else
+	# Replace semver in VERSIONFILE with semver obtained from CHANGELOGFILE
+	TMPFILE=$(mktemp /tmp/new_version.XXXXXX)
+	sed -r "s/$SEMVER/$LAST_VERSION/" "$VERSIONFILE" > "$TMPFILE"
+	DIFF=$(diff "$VERSIONFILE"  "$TMPFILE" )
+	if [ $CHECK == 1 ];
+	then
+		if [ -z "$DIFF" ];
+		then
+			printf "verion sync would make no changes.\n";
+			rm "$TMPFILE"
+			exit 0;
+		else
+			printf "version sync would make the following changes:\n$DIFF\n";
+			rm "$TMPFILE"
+			exit 1;
+		fi
+	else
+		cp "$TMPFILE" "$VERSIONFILE" 
+		rm "$TMPFILE"
+	fi
 fi
