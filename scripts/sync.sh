82967ab49ca383d90606a2082addf9f82307eb54
Alan Bertl <alan@unstructured.io>
2022-10-03 00:09:10 -0500

    fix script issues

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 SEMVER="^((?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*))$"
 SEMVER="^((?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-(?:(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?:[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)$"
 set -o pipefail
-LASTVERSION="`cat CHANGELOG.md | sed 's/#\+ //' | grep -o -m 1 -E "${SEMVER}"`"
-if [ -z "${VAR}" ];
+LASTVERSION=$(sed 's/#\+ //' CHANGELOG.md | grep -o -m 1 -E "${SEMVER}")
+if [[ $LASTVERSION ]];
 then
 	echo '__version__ = "'"$LASTVERSION"'"  # pragma: no cover' > unstructured/__version__.py
 fi
