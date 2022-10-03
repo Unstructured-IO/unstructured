d491c7d09422aa5780908f23a9b5e2a8621a9fc5
Matt Robinson <mrobinson@unstructuredai.io>
2022-10-03 11:32:37 -0400

    tweaks to get upload to work

--- unstructured/staging/label_studio.py
+++ unstructured/staging/label_studio.py
@@ -8,19 +8,46 @@ from unstructured.documents.elements import Text
 LABEL_STUDIO_TYPE = List[Dict[str, Dict[str, str]]]
 
 
+VALID_TYPE = [
+    "labels",
+    "hypertextlabels",
+    "paragraphlabels",
+    "rectangle",
+    "keypoint",
+    "polygon",
+    "brush",
+    "ellipse",
+    "rectanglelabels",
+    "keypointlabels",
+    "polygonlabels",
+    "brushlabels",
+    "ellipselabels",
+    "timeserieslabels",
+    "choices",
+    "number",
+    "taxonomy",
+    "textarea",
+    "rating",
+    "pairwise",
+    "videorectangle",
+]
+
+
 @dataclass
 class LabelStudioResult:
     """Class for representing a LabelStudio annotation result.
     ref: https://labelstud.io/guide/export.html#Label-Studio-JSON-format-of-annotated-tasks"""
 
-    id: str
     type: str  # The type of tag used to annotate the task
     value: Dict[str, Any]  # The values for
+    from_name: str  # Name of the source object tag (i.e. "sentiment" for the sentiment template)
+    id: Optional[str] = None
     # NOTE(robinson) - See here for a list of object and control tags. Also provides the formats
     # for the value parameter
     # ref: https://labelstud.io/tags/
-    from_name: str = "tag"  # Name of the source object tag
     to_name: str = "text"  # Name of the destination control tag
+    hidden: bool = False
+    read_only: bool = False
 
     def to_dict(self):
         return self.__dict__
@@ -47,24 +74,30 @@ class LabelStudioAnnotation:
     """Class for representing LabelStudio annotations.
     ref: https://labelstud.io/guide/export.html#Label-Studio-JSON-format-of-annotated-tasks"""
 
-    id: str
-    lead_time: float  # Time in seconds to label the task
-    result: LabelStudioResult  # The result of the annotation
-    reviews: List[LabelStudioReview]  # An array consisting of the review results
-    completed_by: int  # User ID for the user who completed the task
+    result: List[LabelStudioResult]  # The result of the annotation
+    id: Optional[str] = None
+    lead_time: Optional[float] = None  # Time in seconds to label the task
+    completed_by: Optional[int] = None  # User ID for the user who completed the task
+    reviews: Optional[List[LabelStudioReview]] = None  # An array of the review results
     was_canceled: bool = False  # Indicates whether or not the annotation was canceled
 
     def to_dict(self):
         annotation_dict = deepcopy(self.__dict__)
-        annotation_dict["result"] = annotation_dict["result"].to_dict()
-        if "reviews" in annotation_dict:
+        annotation_dict["result"] = [r.to_dict() for r in annotation_dict["result"]]
+        if "reviews" in annotation_dict and annotation_dict["reviews"] is not None:
             annotation_dict["reviews"] = [r.to_dict() for r in annotation_dict["reviews"]]
-        return annotation_dict
+
+        _annotation_dict = deepcopy(annotation_dict)
+        for key, value in annotation_dict.items():
+            if value is None:
+                _annotation_dict.pop(key)
+
+        return _annotation_dict
 
 
 def stage_for_label_studio(
     elements: List[Text],
-    annotations: Optional[List[Optional[LabelStudioAnnotation]]] = None,
+    annotations: Optional[List[LabelStudioAnnotation]] = None,
     text_field: str = "text",
     id_field: str = "ref_id",
 ) -> LABEL_STUDIO_TYPE:
@@ -75,10 +108,17 @@ def stage_for_label_studio(
             raise ValueError("The length of elements and annotations must match.")
 
     label_studio_data: LABEL_STUDIO_TYPE = list()
-    for element in elements:
+    for i, element in enumerate(elements):
         data: Dict[str, str] = dict()
         data[text_field] = element.text
         if isinstance(element.id, str):
             data[id_field] = element.id
-        label_studio_data.append({"data": data})
+
+        labeling_example: Dict[str, Any] = dict()
+        labeling_example["data"] = data
+        if annotations is not None:
+            annotation: LabelStudioAnnotation = annotations[i]
+            labeling_example["annotations"] = [annotation.to_dict()]
+        label_studio_data.append(labeling_example)
+
     return label_studio_data
