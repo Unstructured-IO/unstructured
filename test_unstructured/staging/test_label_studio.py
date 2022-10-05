bbfe9cfc65d7b93cbcfd24ff0fa39ee01d239855
Yuming Long <305248291@qq.com>
2022-10-05 13:51:47 -0400

    comment

--- test_unstructured/staging/test_label_studio.py
+++ test_unstructured/staging/test_label_studio.py
@@ -22,8 +22,8 @@ def test_upload_label_studio_data_with_sdk(caplog, elements):
     ===================
     1. Remove file `test_unstructured/vcr_fixtures/cassettes/label_studio_upload.yaml`,
         which will be recreated later.
-    2. Install the label-studio package by running command `pip install -U label-studio`
-    3. Run `label-studio`, and login or set up label studio account on pop-up website.
+    2. Install the label-studio package by running command `pip install -U label-studio`.
+    3. Run command `label-studio`, and login or set up label studio account on pop-up website.
     4. Update `LABEL_STUDIO_URL` and `API_KEY` below.
     5. Run this test once, and VCR will record the HTTP request to the yaml file.
     6. Kill the label studio instance and run the test again, VCR will replay the response.
