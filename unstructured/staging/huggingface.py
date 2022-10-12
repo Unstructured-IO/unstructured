863c55633074683f6bca3d1508bcbd3c7048fe87
Matt Robinson <mrobinson@unstructuredai.io>
2022-10-12 11:16:47 -0400

    first pass on chunk by attention window

---
+++ unstructured/staging/huggingface.py
@@ -0,0 +1,57 @@
+from typing import Callable, Optional, List
+
+from transformers import PreTrainedTokenizer
+
+
+def chunk_by_attention_window(
+    text: str,
+    tokenizer: PreTrainedTokenizer,
+    buffer: int = 10,
+    max_input_size: Optional[int] = None,
+    split_function: Callable[[str], List[str]] = lambda text: text.split(" "),
+    chunk_separator: str = " ",
+) -> List[str]:
+    """Splits a string of text into chunks that will fit into a model's attention
+    window.
+
+    Parameters
+    ----------
+    text: The raw input text for the model
+    tokenizer: The transformers tokenizer for the model
+    buffer: Indicates the number of tokens to leave as a buffer for the attention window. This
+        is to account for special tokens like [CLS] that can appear at the beginning or
+        end of an input sequence.
+    max_input_size: The size of the attention window for the model. If not specified, will
+        use the model_max_length attribute on the tokenizer object.
+    split_function: The function used to split the text into chunks to consider for adding to the
+        attention window.
+    chunk_separator: The string used to concat adjacent chunks when reconstructing the text
+    """
+    max_input_size = tokenizer.model_max_length if max_input_size is None else max_input_size
+    max_chunk_size = max_input_size - buffer
+
+    split_text: List[str] = split_function(text)
+    chunks: List[str] = list()
+    chunk_text = str()
+    chunk_size = 0
+
+    for segment in split_text:
+        tokens = tokenizer.tokenize(segment)
+        num_tokens = len(tokens)
+        if num_tokens > max_chunk_size:
+            raise ValueError(
+                f"The number of tokens in the segment is {num_tokens}. "
+                f"The maximum number of tokens is {max_chunk_size}. "
+                "Consider using a different split_function to reduce the size "
+                "of the segments under consideration. The text that caused the "
+                "error is: \n\n{segment}"
+            )
+
+        if chunk_size + num_tokens > max_chunk_size:
+            chunk_text = str()
+            chunk_size = 0
+            chunks.append(chunk_text)
+        else:
+            chunk_text += chunk_separator + segment
+
+    return chunks
