13debb239e7b393e4e687bd371ba80b927fb124b
asymness <asym1999@gmail.com>
2022-10-13 20:11:47 +0500

    Implement convert_to_isd_csv function

--- unstructured/staging/base.py
+++ unstructured/staging/base.py
@@ -1,3 +1,5 @@
+import io
+import csv
 from typing import Dict, List
 
 from unstructured.documents.elements import Text
@@ -10,3 +12,17 @@ def convert_to_isd(elements: List[Text]) -> List[Dict[str, str]]:
         section = dict(text=element.text, type=element.category)
         isd.append(section)
     return isd
+
+
+def convert_to_isd_csv(elements: List[Text]) -> str:
+    """
+    Returns the representation of document elements as an Initial Structured Document (ISD)
+    in CSV Format.
+    """
+    csv_fieldnames: List[str] = ["type", "text"]
+    rows: List[Dict[str, str]] = convert_to_isd(elements)
+    with io.StringIO() as buffer:
+        csv_writer = csv.DictWriter(buffer, fieldnames=csv_fieldnames)
+        csv_writer.writeheader()
+        csv_writer.writerows(rows)
+        return buffer.getvalue()
