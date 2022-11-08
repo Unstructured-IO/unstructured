a7e50fb2279442a5a0cf008d20f6e79dfe6e5e0a
Benjamin Torres <benjamin@unstructured.io>
2022-11-08 15:16:15 -0600

    Added fields for description in PyPi

--- setup.py
+++ setup.py
@@ -17,22 +17,29 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import os
-
 from setuptools import setup, find_packages
 
 from unstructured.__version__ import __version__
 
-cwd = os.path.dirname(os.path.abspath(__file__))
-
-# Read in README.md for our long_description
-with open(os.path.join(cwd, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
-
 setup(
     name="unstructured",
     description="A library that prepares raw documents for downstream ML tasks.",
-    long_description=long_description,
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    keywords="NLP PDF HTML CV XML parsing preprocessing",
+    url="https://github.com/Unstructured-IO/unstructured",
+    python_requires=">=3.8.0",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
     author="Unstructured Technologies",
     author_email="devops@unstructuredai.io",
     license="Apache-2.0",
