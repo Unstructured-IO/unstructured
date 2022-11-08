6b3e86c508029812e87485e0dce1c26978c92a09
benjats07 <benjats07@users.noreply.github.com>
2022-11-08 15:22:43 -0600

    docs: Added long description to PyPi (#58)
    * docs: Added long description to PyPi
    
    * Added fields for description in PyPi

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
 from setuptools import setup, find_packages
 
 from unstructured.__version__ import __version__
@@ -25,6 +24,22 @@ from unstructured.__version__ import __version__
 setup(
     name="unstructured",
     description="A library that prepares raw documents for downstream ML tasks.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    keywords="NLP PDF HTML CV XML parsing preprocessing",
+    url="https://github.com/Unstructured-IO/unstructured",
+    python_requires=">=3.8.0",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
     author="Unstructured Technologies",
     author_email="devops@unstructuredai.io",
     license="Apache-2.0",
