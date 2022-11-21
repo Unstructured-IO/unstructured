3b6747390c0d4e02ed8d350deb409bb66d4ec0e0
Matt Robinson <mrobinson@unstructuredai.io>
2022-11-21 16:39:11 -0500

    import to nlp for backward compatibility

---
+++ test_unstructured/nlp/test_partition.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+from unstructured.nlp.partition import (
+    is_bulleted_text,
+    is_possible_narrative_text,
+    is_possible_title,
+    partition_pdf,
+)
---
+++ unstructured/nlp/partition.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+from unstructured.partition.pdf import partition_pdf
+from unstructured.partition.text_type import (
+    is_bulleted_text,
+    is_possible_narrative_text,
+    is_possible_title,
+)
