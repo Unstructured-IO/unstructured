3cddd08386a73d0e76c6846a5defe0e44ace5264
Matt Robinson <mrobinson@unstructuredai.io>
2022-12-12 09:07:37 -0500

    switch default parameters to None

--- unstructured/partition/html.py
+++ unstructured/partition/html.py
@@ -4,7 +4,9 @@ from unstructured.documents.elements import Element
 from unstructured.documents.html import HTMLDocument
 
 
-def partition_html(filename: str = "", file: Optional[IO] = None, text: str = "") -> List[Element]:
+def partition_html(
+    filename: Optional[str] = None, file: Optional[IO] = None, text: Optional[str] = None
+) -> List[Element]:
     """Partitions an HTML document into its constituent elements.
 
     Parameters
@@ -13,23 +15,24 @@ def partition_html(filename: str = "", file: Optional[IO] = None, text: str = ""
         A string defining the target filename path.
     file
         A file-like object using "r" mode --> open(filename, "r").
-        text
-                The string representation of the HTML document.
+    text
+        The string representation of the HTML document.
     """
     if not any([filename, file, text]):
         raise ValueError("One of filename, file, or text must be specified.")
 
-    if filename and not file and not text:
+    if filename is not None and not file and not text:
         document = HTMLDocument.from_file(filename)
         elements = document.elements
 
-    elif file and not filename and not text:
-        text = file.read()
-        document = HTMLDocument.from_string(text)
+    elif file is not None and not filename and not text:
+        file_text = file.read()
+        document = HTMLDocument.from_string(file_text)
         elements = document.elements
 
-    elif text and not filename and not file:
-        document = HTMLDocument.from_string(text)
+    elif text is not None and not filename and not file:
+        _text: str = str(text)
+        document = HTMLDocument.from_string(_text)
         elements = document.elements
 
     else:
