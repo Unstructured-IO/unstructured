3c19c7cd8af3defcc3a12312306f90b4a14c1e7d
Matt Robinson <mrobinson@unstructured.io>
2022-12-12 09:22:10 -0500

    feat: Add partition_html brick (#91)
    * update readme
    
    * updated sphinx docs
    
    * bump version; changelog
    
    * clear cache; retrigger ci
    
    * rename test file
    
    * switch default parameters to None
    
    * typo in the changelog
    
    * add in text output

---
+++ unstructured/partition/html.py
@@ -0,0 +1,41 @@
+from typing import IO, List, Optional
+
+from unstructured.documents.elements import Element
+from unstructured.documents.html import HTMLDocument
+
+
+def partition_html(
+    filename: Optional[str] = None, file: Optional[IO] = None, text: Optional[str] = None
+) -> List[Element]:
+    """Partitions an HTML document into its constituent elements.
+
+    Parameters
+    ----------
+     filename
+        A string defining the target filename path.
+    file
+        A file-like object using "r" mode --> open(filename, "r").
+    text
+        The string representation of the HTML document.
+    """
+    if not any([filename, file, text]):
+        raise ValueError("One of filename, file, or text must be specified.")
+
+    if filename is not None and not file and not text:
+        document = HTMLDocument.from_file(filename)
+        elements = document.elements
+
+    elif file is not None and not filename and not text:
+        file_text = file.read()
+        document = HTMLDocument.from_string(file_text)
+        elements = document.elements
+
+    elif text is not None and not filename and not file:
+        _text: str = str(text)
+        document = HTMLDocument.from_string(_text)
+        elements = document.elements
+
+    else:
+        raise ValueError("Only one of filename, file, or text can be specified.")
+
+    return elements
