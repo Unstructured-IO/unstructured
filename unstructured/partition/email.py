4160bc797f85d56bcebe45fe9efe797c6f3c64a4
Mallori Harrell <mallori@Malloris-MacBook-Pro.local>
2023-01-03 09:27:16 -0600

    add write to file from string or bytes

--- unstructured/partition/email.py
+++ unstructured/partition/email.py
@@ -16,7 +16,7 @@ from unstructured.partition.html import partition_html
 VALID_CONTENT_SOURCES: Final[List[str]] = ["text/html"]
 
 
-def extract_attachment_info(message: Message, output_dir: str = None) -> List[Dict[str, str]]:
+def extract_attachment_info(message: Message, output_dir: Optional[str] = None) -> List[Dict[str, str]]:
     list_attachments = []
     attachment_info = {}
     for part in message.walk():
@@ -37,8 +37,14 @@ def extract_attachment_info(message: Message, output_dir: str = None) -> List[Di
             for attachment in list_attachments:
                 if output_dir:
                     filename = output_dir + "/" + attachment["filename"]
-                    with open(filename, "wb") as f:
-                        f.write(attachment["payload"])
+
+                    if type(attachment["payload"]):        
+                        with open(filename, "w") as f:
+                            f.write(attachment["payload"])
+                    else:
+                        with open(filename, "wb") as f:
+                            f.write(attachment["payload"])
+
     return list_attachments
 
 
