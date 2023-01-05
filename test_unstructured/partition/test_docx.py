b3687828708a764e6defd865991ccb4fcc6102cb
Matt Robinson <mrobinson@unstructured.io>
2023-01-05 15:13:39 -0500

    feat: add `partition_docx` for Word documents (#131)
    * first pass on docx parsing
    
    * linting, linting, linting
    
    * test docx with filename
    
    * added documentation
    
    * more tests; version bump
    
    * typo
    
    * another typo
    
    * another typo!
    
    * it -> its
    
    * save -> saved
    
    * remove None since it's the default argument

---
+++ test_unstructured/partition/test_docx.py
@@ -0,0 +1,72 @@
+import os
+import pytest
+
+import docx
+
+from unstructured.documents.elements import ListItem, NarrativeText, Title, Text
+from unstructured.partition.docx import partition_docx
+
+
+@pytest.fixture
+def mock_document():
+    document = docx.Document()
+
+    document.add_paragraph("These are a few of my favorite things:", style="Heading 1")
+    ***REMOVED*** NOTE(robinson) - this should get picked up as a list item due to the •
+    document.add_paragraph("• Parrots", style="Normal")
+    document.add_paragraph("Hockey", style="List Bullet")
+    ***REMOVED*** NOTE(robinson) - this should get picked up as a title
+    document.add_paragraph("Analysis", style="Normal")
+    ***REMOVED*** NOTE(robinson) - this should get dropped because it is empty
+    document.add_paragraph("", style="Normal")
+    ***REMOVED*** NOTE(robinson) - this should get picked up as a narrative text
+    document.add_paragraph("This is my first thought. This is my second thought.", style="Normal")
+    document.add_paragraph("This is my third thought.", style="Body Text")
+    ***REMOVED*** NOTE(robinson) - this should just be regular text
+    document.add_paragraph("2023")
+
+    return document
+
+
+@pytest.fixture
+def expected_elements():
+    return [
+        Title("These are a few of my favorite things:"),
+        ListItem("Parrots"),
+        ListItem("Hockey"),
+        Title("Analysis"),
+        NarrativeText("This is my first thought. This is my second thought."),
+        NarrativeText("This is my third thought."),
+        Text("2023"),
+    ]
+
+
+def test_partition_docx_with_filename(mock_document, expected_elements, tmpdir):
+    filename = os.path.join(tmpdir.dirname, "mock_document.docx")
+    mock_document.save(filename)
+
+    elements = partition_docx(filename=filename)
+    assert elements == expected_elements
+
+
+def test_partition_docx_with_file(mock_document, expected_elements, tmpdir):
+    filename = os.path.join(tmpdir.dirname, "mock_document.docx")
+    mock_document.save(filename)
+
+    with open(filename, "rb") as f:
+        elements = partition_docx(file=f)
+    assert elements == expected_elements
+
+
+def test_partition_docx_raises_with_both_specified(mock_document, tmpdir):
+    filename = os.path.join(tmpdir.dirname, "mock_document.docx")
+    mock_document.save(filename)
+
+    with open(filename, "rb") as f:
+        with pytest.raises(ValueError):
+            partition_docx(filename=filename, file=f)
+
+
+def test_partition_docx_raises_with_neither():
+    with pytest.raises(ValueError):
+        partition_docx()
