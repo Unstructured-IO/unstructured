0839164b8483b07036b6a815339d7dfb3ee9e5fd
Matt Robinson <mrobinson@unstructuredai.io>
2023-01-06 15:09:56 -0500

    auto tests for docx

--- test_unstructured/partition/test_auto.py
+++ test_unstructured/partition/test_auto.py
@@ -1,7 +1,10 @@
 import os
 import pathlib
+import pytest
 
-from unstructured.documents.elements import NarrativeText, Title, ListItem
+import docx
+
+from unstructured.documents.elements import NarrativeText, Title, Text, ListItem
 from unstructured.partition.auto import partition
 
 EXAMPLE_DOCS_DIRECTORY = pathlib.Path(__file__).parent.resolve()
@@ -28,3 +31,54 @@ def test_auto_partition_email_from_file():
         elements = partition(file=f)
     assert len(elements) > 0
     assert elements == EXPECTED_EMAIL_OUTPUT
+
+
+@pytest.fixture
+def mock_docx_document():
+    document = docx.Document()
+
+    document.add_paragraph("These are a few of my favorite things:", style="Heading 1")
+    # NOTE(robinson) - this should get picked up as a list item due to the •
+    document.add_paragraph("• Parrots", style="Normal")
+    document.add_paragraph("Hockey", style="List Bullet")
+    # NOTE(robinson) - this should get picked up as a title
+    document.add_paragraph("Analysis", style="Normal")
+    # NOTE(robinson) - this should get dropped because it is empty
+    document.add_paragraph("", style="Normal")
+    # NOTE(robinson) - this should get picked up as a narrative text
+    document.add_paragraph("This is my first thought. This is my second thought.", style="Normal")
+    document.add_paragraph("This is my third thought.", style="Body Text")
+    # NOTE(robinson) - this should just be regular text
+    document.add_paragraph("2023")
+
+    return document
+
+
+@pytest.fixture
+def expected_docx_elements():
+    return [
+        Title("These are a few of my favorite things:"),
+        ListItem("Parrots"),
+        ListItem("Hockey"),
+        Title("Analysis"),
+        NarrativeText("This is my first thought. This is my second thought."),
+        NarrativeText("This is my third thought."),
+        Text("2023"),
+    ]
+
+
+def test_auto_partition_docx_with_filename(mock_docx_document, expected_docx_elements, tmpdir):
+    filename = os.path.join(tmpdir.dirname, "mock_document.docx")
+    mock_docx_document.save(filename)
+
+    elements = partition(filename=filename)
+    assert elements == expected_docx_elements
+
+
+def test_auto_partition_docx_with_file(mock_docx_document, expected_docx_elements, tmpdir):
+    filename = os.path.join(tmpdir.dirname, "mock_document.docx")
+    mock_docx_document.save(filename)
+
+    with open(filename, "rb") as f:
+        elements = partition(file=f)
+    assert elements == expected_docx_elements
