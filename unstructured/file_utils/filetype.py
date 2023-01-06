88ac1fce8d82507d1ce8d0d1f03cac7073c0da74
Matt Robinson <mrobinson@unstructuredai.io>
2023-01-06 12:47:08 -0500

    first pass on filetype detection

---
+++ unstructured/file_utils/filetype.py
@@ -0,0 +1,53 @@
+from enum import Enum
+import os
+from typing import IO, Optional
+
+import magic
+
+from unstructured.logger import logger
+
+
+DOCX_MIME_TYPE = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+
+
+class FileType(Enum):
+    PDF = 1
+    DOCX = 2
+    JPG = 3
+    TXT = 4
+    EML = 5
+
+
+def detect_filetype(filename: str = "", file: Optional[IO] = None) -> Optional[FileType]:
+    """Use libmagic to determine a file's type. Helps determine which partition brick
+    to use for a given file. A return value of None indicates a non-supported file type."""
+    if filename and file:
+        raise ValueError("Only one of filename or file should be specified.")
+
+    if filename:
+        _, extension = os.path.splitext(filename)
+        mime_type = magic.from_file(filename, mime=True)
+    elif file is not None:
+        extension = None
+        # NOTE(robinson) - the python-magic docs recommend reading at least the first 2048 bytes
+        # ref: https://github.com/ahupp/python-magic#usage
+        mime_type = magic.from_buffer(file.read(2048), mime=True)
+    else:
+        raise ValueError("No filename nor file were specified.")
+
+    if mime_type == "application/pdf":
+        return FileType.PDF
+    elif mime_type == DOCX_MIME_TYPE:
+        return FileType.DOCX
+    elif mime_type == "image/jpeg":
+        return FileType.JPG
+    elif mime_type == "text/plain":
+        if extension and extension == ".eml":
+            return FileType.EML
+        else:
+            return FileType.TXT
+    else:
+        logger.warn(
+            f"MIME type was {mime_type}. This file type is not currently supported in unstructured."
+        )
+        return None
