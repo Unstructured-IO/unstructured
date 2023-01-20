8d3e616846e58a4e0d435563de36417c062c6a9a
Matt Robinson <mrobinson@unstructured.io>
2023-01-20 08:55:11 -0500

    feat: add ability to parse `LayoutElement` lists (#165)
    * added ability to split list items
    
    * changelog and version bump
    
    * retrigger ci

--- unstructured/partition/common.py
+++ unstructured/partition/common.py
@@ -1,14 +1,18 @@
+from typing import List, Union
+
 from unstructured.documents.elements import (
     Element,
     CheckBox,
     FigureCaption,
+    ListItem,
     NarrativeText,
     Text,
     Title,
 )
+from unstructured.nlp.patterns import UNICODE_BULLETS_RE, ENUMERATED_BULLETS_RE
 
 
-def normalize_layout_element(layout_element) -> Element:
+def normalize_layout_element(layout_element) -> Union[Element, List[Element]]:
     """Converts a list of unstructured_inference DocumentLayout objects to a list of
     unstructured Elements."""
 
@@ -27,9 +31,26 @@ def normalize_layout_element(layout_element) -> Element:
         return NarrativeText(text=text, coordinates=coordinates)
     elif element_type == "Figure":
         return FigureCaption(text=text, coordinates=coordinates)
+    elif element_type == "List":
+        return layout_list_to_list_items(text, coordinates)
     elif element_type == "Checked":
         return CheckBox(checked=True, coordinates=coordinates)
     elif element_type == "Unchecked":
         return CheckBox(checked=False, coordinates=coordinates)
     else:
         return Text(text=text, coordinates=coordinates)
+
+
+def layout_list_to_list_items(text: str, coordinates: List[float]) -> List[Element]:
+    """Converts a list LayoutElement to a list of ListItem elements."""
+    split_items = ENUMERATED_BULLETS_RE.split(text)
+    # NOTE(robinson) - this means there wasn't a match for the enumerated bullets
+    if len(split_items) == 1:
+        split_items = UNICODE_BULLETS_RE.split(text)
+
+    list_items: List[Element] = list()
+    for text_segment in split_items:
+        if len(text_segment.strip()) > 0:
+            list_items.append(ListItem(text=text_segment.strip(), coordinates=coordinates))
+
+    return list_items
