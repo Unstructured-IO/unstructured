83519c2a9f695d5614785667562db077d87e48ea
MthwRobinson <MthwRobinson@users.noreply.github.com>
2023-01-27 17:08:59 +0000

    deploy: e6cfde5c4a52b854ce9229cf84ce751f783dd995

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.5-dev0',
+    VERSION: '0.4.5-dev1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
