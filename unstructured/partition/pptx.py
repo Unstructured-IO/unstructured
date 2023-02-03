ff7db9c4a30890cd0edcff73ecf124a63e47bc3e
Matt Robinson <mrobinson@unstructured.io>
2023-02-03 17:12:33 -0500

    fix: preserve the order of shapes in `partition_pptx` output (#193)
    * order the shapes top to bottom and left to right
    
    * added tests for ordering
    
    * update change log and bump version
    
    * more tests
    
    * don't need enumerate
    
    * n -> on

--- unstructured/partition/pptx.py
+++ unstructured/partition/pptx.py
@@ -2,7 +2,7 @@ from typing import IO, List, Optional
 
 import pptx
 
-from unstructured.documents.elements import Element, ListItem, NarrativeText, Title
+from unstructured.documents.elements import Element, ListItem, NarrativeText, Text, Title
 from unstructured.partition.text_type import (
     is_possible_narrative_text,
     is_possible_title,
@@ -35,8 +35,15 @@ def partition_pptx(filename: Optional[str] = None, file: Optional[IO] = None) ->
 
     elements: List[Element] = list()
     for slide in presentation.slides:
-        for shape in slide.shapes:
+        for shape in _order_shapes(slide.shapes):
+            ***REMOVED*** NOTE(robinson) - we don't deal with tables yet, but so future humans can find
+            ***REMOVED*** it again, here are docs on how to deal with tables. The check for tables should
+            ***REMOVED*** be `if shape.has_table`
+            ***REMOVED*** ref: https://python-pptx.readthedocs.io/en/latest/user/table.html***REMOVED***adding-a-table
             if not shape.has_text_frame:
+                continue
+            ***REMOVED*** NOTE(robinson) - avoid processing shapes that are not on the actual slide
+            if shape.top < 0 or shape.left < 0:
                 continue
             for paragraph in shape.text_frame.paragraphs:
                 text = paragraph.text
@@ -48,8 +55,15 @@ def partition_pptx(filename: Optional[str] = None, file: Optional[IO] = None) ->
                     elements.append(NarrativeText(text=text))
                 elif is_possible_title(text):
                     elements.append(Title(text=text))
+                else:
+                    elements.append(Text(text=text))
 
     return elements
+
+
+def _order_shapes(shapes):
+    """Orders the shapes from top to bottom and left to right."""
+    return sorted(shapes, key=lambda x: (x.top, x.left))
 
 
 def _is_bulleted_paragraph(paragraph) -> bool:
