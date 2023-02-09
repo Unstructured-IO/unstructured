24d48e9cba1c5684f481d4314d9f780bda7e2834
Matt Robinson <mrobinson@unstructuredai.io>
2023-02-09 17:18:02 -0500

    more unicode quotes; isd dataframe column fix

--- test_unstructured/staging/test_base_staging.py
+++ test_unstructured/staging/test_base_staging.py
@@ -48,7 +48,7 @@ def test_convert_to_isd_csv(output_csv_file):
         isd_csv_string = base.convert_to_isd_csv(elements)
         csv_file.write(isd_csv_string)
 
-    fieldnames = ["type", "text", "coordinates", "element_id"]
+    fieldnames = ["type", "text"]
     with open(output_csv_file, "r") as csv_file:
         csv_rows = csv.DictReader(csv_file)
         assert all(set(row.keys()) == set(fieldnames) for row in csv_rows)
