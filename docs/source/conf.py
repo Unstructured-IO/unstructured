94f76ad574130c40a46c43ca7fa7800362b5e51c
Matt Robinson <mrobinson@unstructured.io>
2023-02-09 10:40:28 -0500

    docs: switch theme and style refresh (#209)
    * add furo theme
    
    * switch theme to furo
    
    * css for custom sidebar
    
    * remove unnecessary images
    
    * removed unnecessary fonts
    
    * fix logo background
    
    * hide package name
    
    * add favico, tweak colors
    
    * copyright 2023
    
    * update copyright years
    
    * update hover colors
    
    * fix title tab

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,12 +15,12 @@ sys.path.insert(0, os.path.abspath("../.."))
 
 from unstructured.__version__ import __version__
 
-project = "unstructured"
-copyright = "2022, Unstructured Technologies"
+project = "Unstructured"
+copyright = "2022-2023, Unstructured Technologies"
 author = "Unstructured Technologies"
 
-***REMOVED*** The full version, including alpha/beta/rc tags
-release = __version__
+***REMOVED*** The full version, excluding alpha/beta/rc tags
+release = __version__.split("-")[0]
 
 ***REMOVED*** -- General configuration ---------------------------------------------------
 
@@ -46,7 +46,7 @@ exclude_patterns = []
 ***REMOVED*** The theme to use for HTML and HTML Help pages.  See the documentation for
 ***REMOVED*** a list of builtin themes.
 ***REMOVED***
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 
 ***REMOVED*** Add any paths that contain custom static files (such as style sheets) here,
 ***REMOVED*** relative to this directory. They are copied after the builtin static files,
@@ -54,4 +54,83 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
 ***REMOVED*** Adding a custom css file in order to add custom css file and can change the necessary elements.
+html_favicon = "_static/images/unstructured_small.png"
 html_css_files = ["unstructured.css"]
+html_js_files = ["js/githubStargazers.js", "js/sidebarScrollPosition.js"]
+
+html_theme_options = {
+    "sidebar_hide_name": True,
+    "light_logo": "images/logo-light-mode.svg",
+    "dark_logo": "images/logo-dark-mode.svg",
+    "light_css_variables": {
+        "color-sidebar-background": "***REMOVED***FFFFFF",
+        "color-sidebar-background-border": "***REMOVED***e9eaed",
+        "color-sidebar-caption-text": "***REMOVED***484848",
+        "color-sidebar-link-text": "***REMOVED***484848",
+        "color-sidebar-link-text--top-level": "***REMOVED***484848",
+        "color-sidebar-item-background--current": "transparent",
+        "color-sidebar-item-background--hover": "transparent",
+        "color-sidebar-item-expander-background": "transparent",
+        "color-sidebar-item-expander-background--hover": "transparent",
+        "color-sidebar-search-text": "***REMOVED***484848",
+        "color-sidebar-search-background": "***REMOVED***FFFFFF",
+        "color-sidebar-search-background--focus": "***REMOVED***FFFFFF",
+        "color-sidebar-search-border": "***REMOVED***b9b9b9",
+        "color-sidebar-search-border-focus": "***REMOVED***484848",
+        "color-sidebar-current-text": "***REMOVED***ff675f",
+        "color-content-foreground": "***REMOVED***484848",
+        "color-toc-title": "***REMOVED***212529",
+        "color-toc-item-text--hover": "***REMOVED***484848",
+        "color-toc-item-text--active": "***REMOVED***484848",
+        "color-table-header": "***REMOVED***FDDACA",
+        "color-table-bg": "***REMOVED***FFE5D9",
+        "color-table-row": "***REMOVED***FEEDE6",
+        "color-link": "***REMOVED***ff675f",
+        "color-link--hover": "***REMOVED***ff675f",
+        "content-padding": "5em",
+        "content-padding--small": "2em",
+        "color-search-icon": "***REMOVED***484848",
+        "color-search-placeholder": "***REMOVED***484848",
+        "color-literal": "***REMOVED***FF675F",
+        "toc-spacing-vertical": "3em",
+        "color-page-info": "***REMOVED***646776",
+        "toc-item-spacing-vertical": "1em",
+        "color-img-background": "***REMOVED***ffffff",
+        "sidebar-tree-space-above": "0",
+        "sidebar-caption-space-above": "0",
+    },
+    "dark_css_variables": {
+        "color-sidebar-background": "***REMOVED***131416",
+        "color-sidebar-background-border": "***REMOVED***303335",
+        "color-sidebar-caption-text": "***REMOVED***FFFFFF",
+        "color-sidebar-link-text": "***REMOVED***FFFFFF",
+        "color-sidebar-link-text--top-level": "***REMOVED***FFFFFF",
+        "color-sidebar-item-background--current": "none",
+        "color-sidebar-item-background--hover": "none",
+        "color-sidebar-item-expander-background": "transparent",
+        "color-sidebar-item-expander-background--hover": "transparent",
+        "color-sidebar-search-text": "***REMOVED***FFFFFF",
+        "color-sidebar-search-background": "***REMOVED***131416",
+        "color-sidebar-search-background--focus": "transparent",
+        "color-sidebar-search-border": "***REMOVED***FFFFFF",
+        "color-sidebar-search-border-focus": "***REMOVED***FFFFFF",
+        "color-sidebar-search-foreground": "***REMOVED***FFFFFF",
+        "color-sidebar-current-text": "***REMOVED***FFC2BF",
+        "color-content-foreground": "***REMOVED***FFFFFF",
+        "color-toc-title": "***REMOVED***FFFFFF",
+        "color-toc-item-text--hover": "***REMOVED***FFFFFF",
+        "color-toc-item-text--active": "***REMOVED***FFFFFF",
+        "color-table-header": "***REMOVED***131416",
+        "color-table-bg": "***REMOVED***232427",
+        "color-table-row": "***REMOVED***444444",
+        "color-link": "***REMOVED***FFC2BF",
+        "color-link--hover": "***REMOVED***FFC2BF",
+        "color-search-icon": "***REMOVED***FFFFFF",
+        "color-search-placeholder": "***REMOVED***FFFFFF",
+        "color-literal": "***REMOVED***F8C0A7",
+        "color-page-info": "***REMOVED***FFFFFF",
+        "color-img-background": "***REMOVED***757575",
+        "sidebar-tree-space-above": "0",
+        "sidebar-caption-space-above": "0",
+    },
+}
