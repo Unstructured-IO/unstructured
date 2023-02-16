b79654a5363423fde0b1cb5772d854ec41892692
Matt Robinson <mrobinson@unstructuredai.io>
2023-02-16 17:06:22 -0500

    update docs and readme

---
+++ test_unstructured/partition/test_doc.py
@@ -0,0 +1,96 @@
+import os
+import pytest
+
+import docx
+
+from unstructured.documents.elements import Address, ListItem, NarrativeText, Title, Text
+from unstructured.partition.common import convert_office_doc
+from unstructured.partition.doc import partition_doc
+from unstructured.partition.docx import partition_docx
+
+
+@pytest.fixture
+def mock_document():
+    document = docx.Document()
+
+    document.add_paragraph("These are a few of my favorite things:", style="Heading 1")
+    # NOTE(robinson) - this should get picked up as a list item due to the •
+    document.add_paragraph("• Parrots", style="Normal")
+    # NOTE(robinson) - this should get dropped because it's empty
+    document.add_paragraph("• ", style="Normal")
+    document.add_paragraph("Hockey", style="List Bullet")
+    # NOTE(robinson) - this should get dropped because it's empty
+    document.add_paragraph("", style="List Bullet")
+    # NOTE(robinson) - this should get picked up as a title
+    document.add_paragraph("Analysis", style="Normal")
+    # NOTE(robinson) - this should get dropped because it is empty
+    document.add_paragraph("", style="Normal")
+    # NOTE(robinson) - this should get picked up as a narrative text
+    document.add_paragraph("This is my first thought. This is my second thought.", style="Normal")
+    document.add_paragraph("This is my third thought.", style="Body Text")
+    # NOTE(robinson) - this should just be regular text
+    document.add_paragraph("2023")
+    # NOTE(robinson) - this should be an address
+    document.add_paragraph("DOYLESTOWN, PA 18901")
+
+    return document
+
+
+@pytest.fixture
+def expected_elements():
+    return [
+        Title("These are a few of my favorite things:"),
+        ListItem("Parrots"),
+        ListItem("Hockey"),
+        Title("Analysis"),
+        NarrativeText("This is my first thought. This is my second thought."),
+        NarrativeText("This is my third thought."),
+        Text("2023"),
+        Address("DOYLESTOWN, PA 18901"),
+    ]
+
+
+def test_partition_doc_with_filename(mock_document, expected_elements, tmpdir):
+    docx_filename = os.path.join(tmpdir.dirname, "mock_document.docx")
+    doc_filename = os.path.join(tmpdir.dirname, "mock_document.doc")
+    mock_document.save(docx_filename)
+    convert_office_doc(docx_filename, tmpdir.dirname, "doc")
+
+    elements = partition_doc(filename=doc_filename)
+    assert elements == expected_elements
+
+
+def test_partition_matches_partition_docx(mock_document, expected_elements, tmpdir):
+    docx_filename = os.path.join(tmpdir.dirname, "mock_document.docx")
+    doc_filename = os.path.join(tmpdir.dirname, "mock_document.doc")
+    mock_document.save(docx_filename)
+    convert_office_doc(docx_filename, tmpdir.dirname, "doc")
+
+    partition_doc(filename=doc_filename) == partition_docx(filename=docx_filename)
+
+
+def test_partition_doc_with_file(mock_document, expected_elements, tmpdir):
+    docx_filename = os.path.join(tmpdir.dirname, "mock_document.docx")
+    doc_filename = os.path.join(tmpdir.dirname, "mock_document.doc")
+    mock_document.save(docx_filename)
+    convert_office_doc(docx_filename, tmpdir.dirname, "doc")
+
+    with open(doc_filename, "rb") as f:
+        elements = partition_doc(file=f)
+    assert elements == expected_elements
+
+
+def test_partition_doc_raises_with_both_specified(mock_document, tmpdir):
+    docx_filename = os.path.join(tmpdir.dirname, "mock_document.docx")
+    doc_filename = os.path.join(tmpdir.dirname, "mock_document.doc")
+    mock_document.save(docx_filename)
+    convert_office_doc(docx_filename, tmpdir.dirname, "doc")
+
+    with open(doc_filename, "rb") as f:
+        with pytest.raises(ValueError):
+            partition_doc(filename=doc_filename, file=f)
+
+
+def test_partition_doc_raises_with_neither():
+    with pytest.raises(ValueError):
+        partition_doc()
--- unstructured/partition/doc.py
+++ unstructured/partition/doc.py
@@ -1,9 +1,9 @@
 import os
-import subprocess
 import tempfile
 from typing import IO, List, Optional
 
 from unstructured.documents.elements import Element
+from unstructured.partition.common import convert_office_doc
 from unstructured.partition.docx import partition_docx
 
 
@@ -35,37 +35,8 @@ def partition_doc(filename: Optional[str] = None, file: Optional[IO] = None) -> 
     base_filename, _ = os.path.splitext(filename_no_path)
 
     with tempfile.TemporaryDirectory() as tmpdir:
-        _convert_to_docx(filename, tmpdir)
+        convert_office_doc(filename, tmpdir, target_format="docx")
         docx_filename = os.path.join(tmpdir, f"{base_filename}.docx")
         elements = partition_docx(filename=docx_filename)
 
     return elements
-
-
-def _convert_to_docx(input_filename, output_directory):
-    """Converts a .doc file to a .docx file using the libreoffice CLI."""
-    # NOTE(robinson) - In the future can also include win32com client as a fallback for windows
-    # users who do not have LibreOffice installed
-    # ref: https://stackoverflow.com/questions/38468442/
-    #       multiple-doc-to-docx-file-conversion-using-python
-    try:
-        subprocess.call(
-            [
-                "soffice",
-                "--headless",
-                "--convert-to",
-                "docx",
-                "--outdir",
-                output_directory,
-                input_filename,
-            ]
-        )
-    except FileNotFoundError:
-        raise FileNotFoundError(
-            """soffice command was not found. Please install libreoffice
-on your system and try again.
-
-- Install instructions: https://www.libreoffice.org/get-help/install-howto/
-- Mac: https://formulae.brew.sh/cask/libreoffice
-- Debian: https://wiki.debian.org/LibreOffice"""
-        )
