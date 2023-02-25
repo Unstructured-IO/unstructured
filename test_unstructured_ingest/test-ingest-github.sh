3a72028d73500bec2b124ce79d0707091147fdb2
Tom Aarsen <Cubiegamedev@gmail.com>
2023-02-25 14:04:06 +0100

    Add a test_unstructured_ingest test script
    Sadly I can't easily test this script myself as I can't run 'diff' locally

---
+++ test_unstructured_ingest/test-ingest-github.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+unstructured-ingest --github-url dcneiner/Downloadify --github-file-glob '*.html,*.txt' --structured-output-dir github-downloadify-output --verbose
+
+if ! diff -ru github-downloadify-output test_unstructured_ingest/expected-structured-output/github-downloadify ; then
+   echo
+   echo "There are differences from the previously checked-in structured outputs."
+   echo 
+   echo "If these differences are acceptable, copy the outputs from"
+   echo "s3-small-batch-output/ to test_unstructured_ingest/expected-structured-output/s3-small-batch/ after running"
+   echo 
+   echo "  PYTHONPATH=. python examples/ingest/s3-small-batch/main.py --structured-output-dir s3-small-batch-output"
+   echo
+   exit 1
+fi
