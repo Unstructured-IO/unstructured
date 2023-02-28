d183093f949e3617cde9c7a9a43d79bfe66d2e09
Alvaro Bartolome <alvarobartt@yahoo.com>
2023-02-28 20:32:00 +0100

    `os.mkdir` just if `download_dir` doesn't exist

--- unstructured/ingest/connector/s3_connector.py
+++ unstructured/ingest/connector/s3_connector.py
@@ -175,7 +175,8 @@ class S3Connector(BaseConnector):
             raise ValueError(
                 f"No objects found in {self.config.s3_url} -- response list object is {response}",
             )
-        os.mkdir(self.config.download_dir)
+        if not Path(self.config.download_dir).exists():
+            os.mkdir(self.config.download_dir)
 
     def _list_objects(self):
         response = self.s3_cli.list_objects_v2(**self._list_objects_kwargs)
