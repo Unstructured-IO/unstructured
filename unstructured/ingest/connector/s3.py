80255244097dbc0bb06f39914e0b4c3107142634
Alvaro Bartolome <alvarobartt@yahoo.com>
2023-03-01 12:45:47 +0100

    Update `requires_depedencies` params

--- unstructured/ingest/connector/s3.py
+++ unstructured/ingest/connector/s3.py
@@ -14,11 +14,11 @@ class SimpleS3Config(SimpleFsspecConfig):
 
 
 class S3IngestDoc(FsspecIngestDoc):
-    @requires_dependencies(["s3fs"], extras="s3")
+    @requires_dependencies(["s3fs", "fsspec"], extras="s3")
     def get_file(self):
         super().get_file()
 
 
-@requires_dependencies(["s3fs"], extras="s3")
+@requires_dependencies(["s3fs", "fsspec"], extras="s3")
 class S3Connector(FsspecConnector):
     pass
