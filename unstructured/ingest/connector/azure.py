169ae34ada29155d3e457e6eb32b5964458c6d47
Alvaro Bartolome <alvarobartt@yahoo.com>
2023-03-10 07:59:43 +0100

    Add `AzureBlobStorageConnector`

---
+++ unstructured/ingest/connector/azure.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+from unstructured.ingest.connector.fsspec import (
+    FsspecConnector,
+    FsspecIngestDoc,
+    SimpleFsspecConfig,
+)
+from unstructured.utils import requires_dependencies
+
+
+@dataclass
+class SimpleAzureBlobStorageConfig(SimpleFsspecConfig):
+    pass
+
+
+class AzureBlobStorageIngestDoc(FsspecIngestDoc):
+    @requires_dependencies(["adlfs", "fsspec"], extras="azure")
+    def get_file(self):
+        super().get_file()
+
+
+@requires_dependencies(["adlfs", "fsspec"], extras="azure")
+class AzureBlobStorageConnector(FsspecConnector):
+    pass
