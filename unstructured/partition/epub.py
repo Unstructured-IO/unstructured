cf05d6cc5ee99a231f98b60d13ab2f5ffc4ba709
Matt Robinson <mrobinson@unstructuredai.io>
2023-03-13 14:49:11 -0400

    added epub partitioner and file conversion

---
+++ unstructured/partition/epub.py
@@ -0,0 +1,29 @@
+from typing import IO, Optional
+
+from unstructured.file_utils.file_conversion import convert_epub_to_html
+from unstructured.partition.html import partition_html
+
+
+def partition_epub(
+    filename: Optional[str] = None,
+    file: Optional[IO] = None,
+    include_page_breaks: bool = False,
+):
+    """Partitions an EPUB document. The document is first converted to HTML and then
+    partitoned using partiton_html.
+
+        Parameters
+    ----------
+     filename
+        A string defining the target filename path.
+    file
+        A file-like object using "rb" mode --> open(filename, "rb").
+    include_page_breaks
+        If True, the output will include page breaks if the filetype supports it
+    """
+    html_text = convert_epub_to_html(filename=filename, file=file)
+    return partition_html(
+        text=html_text,
+        include_page_breaks=include_page_breaks,
+        encoding="unicode",
+    )
