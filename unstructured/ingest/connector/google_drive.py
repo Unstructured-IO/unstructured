2028dea9eed4b4c612348ff563f64c09c3c53ad2
natygyoon <ygyoon@uwaterloo.ca>
2023-03-21 18:33:22 -0400

    fix

--- unstructured/ingest/connector/google_drive.py
+++ unstructured/ingest/connector/google_drive.py
@@ -86,9 +86,8 @@ class SimpleGoogleDriveConfig(BaseConnectorConfig):
         if self.extension and self.extension not in EXT_TO_FILETYPE.keys():
             raise ValueError(
                 f"Extension not supported. "
-                f"Value MUST be one of {', '.join([k for k in EXT_TO_FILETYPE.keys() if k is not None])}.",
+                f"Value MUST be one of {', '.join([k for k in EXT_TO_FILETYPE if k is not None])}.",
             )
-
         self.service = create_service_account_object(self.service_account_key, self.drive_id)
 
 
