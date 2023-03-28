75cf233702db00202e985e1ad7e24ce2e6a3aeac
Matt Robinson <mrobinson@unstructured.io>
2023-03-28 16:15:22 -0400

    feat: add `partition_msg` for MSFT Outlook files (#412)
    * added msg-parser dependency
    
    * pass through kwargs in convert_file_to_text
    
    * added partition_msg for processing msft outlook files
    
    * version bump and changelog
    
    * added tests for partition_msg
    
    * added test for msg with plain text
    
    * add partition_msg docs; fix underlines in integration docs
    
    * add .msg to file list
    
    * finish tests for auto msg
    
    * linting, linting, linting

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,17 @@
+## 0.5.8-dev0
+
+### Enhancements
+
+* `detect_filetype` now does a final fallback to file extension.
+
+### Features
+
+* Added `partition_msg` for processing MSFT Outlook .msg files.
+
+### Fixes
+
+* `convert_file_to_text` now passes through the `source_format` and `target_format` kwargs.
+
 ## 0.5.7
 
 ### Enhancements
---
+++ test_unstructured/partition/test_msg.py
@@ -0,0 +1,60 @@
+import os
+import pathlib
+
+import msg_parser
+import pytest
+
+from unstructured.documents.elements import ListItem, NarrativeText, Title
+from unstructured.partition.msg import partition_msg
+
+DIRECTORY = pathlib.Path(__file__).parent.resolve()
+EXAMPLE_DOCS_DIRECTORY = os.path.join(DIRECTORY, "..", "..", "example-docs")
+
+EXPECTED_MSG_OUTPUT = [
+    NarrativeText(text="This is a test email to use for unit tests."),
+    Title(text="Important points:"),
+    ListItem(text="Roses are red"),
+    ListItem(text="Violets are blue"),
+]
+
+
+def test_partition_msg_from_filename():
+    filename = os.path.join(EXAMPLE_DOCS_DIRECTORY, "fake-email.msg")
+    elements = partition_msg(filename=filename)
+    assert elements == EXPECTED_MSG_OUTPUT
+
+
+class MockMsOxMessage:
+    def __init__(self, filename):
+        self.body = "Here is an email with plain text."
+
+
+def test_partition_msg_from_filename_with_text_content(monkeypatch):
+    monkeypatch.setattr(msg_parser, "MsOxMessage", MockMsOxMessage)
+    filename = os.path.join(EXAMPLE_DOCS_DIRECTORY, "fake-email.msg")
+    elements = partition_msg(filename=filename)
+    assert str(elements[0]) == "Here is an email with plain text."
+
+
+def test_partition_msg_raises_with_missing_file():
+    filename = os.path.join(EXAMPLE_DOCS_DIRECTORY, "doesnt-exist.msg")
+    with pytest.raises(FileNotFoundError):
+        partition_msg(filename=filename)
+
+
+def test_partition_msg_from_file():
+    filename = os.path.join(EXAMPLE_DOCS_DIRECTORY, "fake-email.msg")
+    with open(filename, "rb") as f:
+        elements = partition_msg(file=f)
+    assert elements == EXPECTED_MSG_OUTPUT
+
+
+def test_partition_msg_raises_with_both_specified():
+    filename = os.path.join(EXAMPLE_DOCS_DIRECTORY, "fake-email.msg")
+    with open(filename, "rb") as f, pytest.raises(ValueError):
+        partition_msg(filename=filename, file=f)
+
+
+def test_partition_msg_raises_with_neither():
+    with pytest.raises(ValueError):
+        partition_msg()
