ef70c9e62da57aaa0fd7b6e0eda610582287f391
natygyoon <ygyoon@uwaterloo.ca>
2023-03-28 19:18:38 -0400

    lint

--- unstructured/ingest/main.py
+++ unstructured/ingest/main.py
@@ -66,7 +66,7 @@ class MainProcess:
         num_docs_all = len(docs)
         docs = [doc for doc in docs if not doc.has_output()]
         if self.max_docs is not None:
-            docs = docs[:min(len(docs), self.max_docs)]
+            docs = docs[: min(len(docs), self.max_docs)]
         num_docs_to_process = len(docs)
         if num_docs_to_process == 0:
             logger.info(
