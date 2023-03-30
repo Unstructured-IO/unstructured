d231b200758dfd4d9e817308ec298489c1b06559
Crag Wolfe <crag@unstructuredai.io>
2023-03-29 18:08:05 -0700

    pin protobuf

--- setup.py
+++ setup.py
@@ -77,6 +77,13 @@ setup(
         ],
         "local-inference": [
             "unstructured-inference>=0.3.0",
+            # NOTE(crag): workaround issue for error output below
+            # ERROR test_unstructured/partition/test_common.py - TypeError: Descriptors cannot not be created directly.
+            # If this call came from a _pb2.py file, your generated code is out of date and must be regenerated with protoc >= 3.19.0.
+            # If you cannot immediately regenerate your protos, some other possible workarounds are:
+            #  1. Downgrade the protobuf package to 3.20.x or lower.
+            #  2. Set PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python (but this will use pure-Python parsing and will be much slower).
+            "protobuf<3.21",
         ],
         "s3": ["s3fs", "fsspec"],
         "azure": ["adlfs", "fsspec"],
@@ -88,7 +95,10 @@ setup(
         "gitlab": ["python-gitlab"],
         "reddit": ["praw"],
         "wikipedia": ["wikipedia"],
-        "google-drive": ["google-api-python-client"],
+        "google-drive": ["google-api-python-client",
+                         # consistency with local-inference-pin
+                         "protobuf<3.21",
+                         ],
     },
     package_dir={"unstructured": "unstructured"},
     package_data={"unstructured": ["nlp/*.txt"]},
