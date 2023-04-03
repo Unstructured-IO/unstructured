fff9742ff0835fc971e110b903c49d70631270be
natygyoon <ygyoon@uwaterloo.ca>
2023-04-03 16:50:57 -0400

    version

--- unstructured/__version__.py
+++ unstructured/__version__.py
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-__version__ = "0.5.9-dev0"  # pragma: no cover
-=======
-__version__ = "0.5.9"  # pragma: no cover
->>>>>>> 555b95b8f7d20d108895d53a12c214e63737ec57
+__version__ = "0.5.10-dev0"  # pragma: no cover
+
