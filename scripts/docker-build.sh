8da06500f1c5fb5bf9ce3d5f0e85bf0d85c3f527
Ryan Nikolaidis <1208590+ryannikolaidis@users.noreply.github.com>
2023-04-05 14:39:04 -0700

    add registry cache to build

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 set -euo pipefail
+DOCKER_BUILD_PLATFORM="${DOCKER_BUILD_PLATFORM:-linux/amd64}"
+if [[ $DOCKER_BUILD_PLATFORM == "linux/amd64" ]]; then
+    DOCKER_ARCH_TAG="amd"
+elif [[ $DOCKER_BUILD_PLATFORM == "linux/arm64" ]]; then
+    DOCKER_ARCH_TAG="arm"
+else
+    echo "Unsupported platform: $DOCKER_BUILD_PLATFORM"
+    exit 1
+fi
+DOCKER_BUILD_REPOSITORY="${DOCKER_BUILD_REPOSITORY:-quay.io/unstructured-io/build-unstructured}"
+PIPELINE_PACKAGE="${PIPELINE_PACKAGE:-general}"
+PIP_VERSION="${PIP_VERSION:-21.0.1}"
 
-DOCKER_BUILDKIT=1 docker buildx build --load --platform=linux/amd64 -f Dockerfile \
+DOCKER_BUILDKIT=1 docker buildx build --load --platform="$DOCKER_BUILD_PLATFORM" -f Dockerfile \
   --build-arg PIP_VERSION="$PIP_VERSION" \
+  --build-arg BUILDKIT_INLINE_CACHE=1 \
   --progress plain \
+  --cache-from "$DOCKER_BUILD_REPOSITORY":$DOCKER_ARCH_TAG \
   -t unstructured-dev:latest .
