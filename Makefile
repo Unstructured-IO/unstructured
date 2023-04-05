9c0df8b5575d08c5a55dff6dc7efd90d3c662225
Ryan Nikolaidis <1208590+ryannikolaidis@users.noreply.github.com>
2023-04-05 15:10:34 -0700

    update build image name as var

--- Makefile
+++ Makefile
@@ -187,11 +187,11 @@ check-coverage:
 # Docker targets are provided for convenience only and are not required in a standard development environment
 
 DOCKER_PLATFORM ?= linux/amd64
-DOCKER_IMAGE ?= unstructured-dev:latest
+DOCKER_IMAGE ?= unstructured:dev
 
 .PHONY: docker-build
 docker-build:
-	PIP_VERSION=${PIP_VERSION} ./scripts/docker-build.sh
+	PIP_VERSION=${PIP_VERSION} DOCKER_BUILD_IMAGE_NAME=${DOCKER_IMAGE} ./scripts/docker-build.sh
 
 .PHONY: docker-start-bash
 docker-start-bash:
--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,21 +2,14 @@
 
 set -euo pipefail
 DOCKER_BUILD_PLATFORM="${DOCKER_BUILD_PLATFORM:-linux/amd64}"
-if [[ $DOCKER_BUILD_PLATFORM == "linux/amd64" ]]; then
-    DOCKER_ARCH_TAG="amd"
-elif [[ $DOCKER_BUILD_PLATFORM == "linux/arm64" ]]; then
-    DOCKER_ARCH_TAG="arm"
-else
-    echo "Unsupported platform: $DOCKER_BUILD_PLATFORM"
-    exit 1
-fi
 DOCKER_BUILD_REPOSITORY="${DOCKER_BUILD_REPOSITORY:-quay.io/unstructured-io/unstructured}"
 PIPELINE_PACKAGE="${PIPELINE_PACKAGE:-general}"
 PIP_VERSION="${PIP_VERSION:-22.2.1}"
+DOCKER_BUILD_IMAGE_NAME="${DOCKER_BUILD_IMAGE_NAME:-unstructured:dev}"
 
 DOCKER_BUILDKIT=1 docker buildx build --load --platform="$DOCKER_BUILD_PLATFORM" -f Dockerfile \
   --build-arg PIP_VERSION="$PIP_VERSION" \
   --build-arg BUILDKIT_INLINE_CACHE=1 \
   --progress plain \
   --cache-from "$DOCKER_BUILD_REPOSITORY":latest \
-  -t unstructured:dev .
+  -t "$DOCKER_BUILD_IMAGE_NAME" .
