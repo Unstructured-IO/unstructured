578f7f4faa53196946907a214ab8f144cc1b8389
Crag Wolfe <crag@unstructuredai.io>
2023-04-09 11:29:27 -0700

    try setting OMP_THREAD_LIMIT=1 for more reproducible tesseract output

--- test_unstructured_ingest/test-ingest.sh
+++ test_unstructured_ingest/test-ingest.sh
@@ -5,6 +5,8 @@ set -eux -o pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"/.. || exit 1
 
+export OMP_THREAD_LIMIT=1
+
 ./test_unstructured_ingest/test-ingest-s3.sh
 ./test_unstructured_ingest/test-ingest-azure.sh
 ./test_unstructured_ingest/test-ingest-github.sh
