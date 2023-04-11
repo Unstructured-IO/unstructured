1f105af9e147ea5b0143209dab8fe9b2f6583a8e
Crag Wolfe <crag@unstructured.io>
2023-04-11 05:45:59 +0000

    bring back if block

--- test_unstructured_ingest/test-ingest-biomed-path.sh
+++ test_unstructured_ingest/test-ingest-biomed-path.sh
@@ -3,11 +3,11 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"/.. || exit 1
 
-#if [[ "$(find test_unstructured_ingest/expected-structured-output/biomed-ingest-output-path/ -type f -size +10k | wc -l)" != 1 ]]; then
-#    echo "The test fixtures in test_unstructured_ingest/expected-structured-output/biomed-ingest-output-path/ look suspicious. At least one of the files is too small."
-#    echo "Did you overwrite test fixtures with bad outputs?"
-#    exit 1
-#fi
+if [[ "$(find test_unstructured_ingest/expected-structured-output/biomed-ingest-output-path/ -type f -size +10k | wc -l)" != 1 ]]; then
+    echo "The test fixtures in test_unstructured_ingest/expected-structured-output/biomed-ingest-output-path/ look suspicious. At least one of the files is too small."
+    echo "Did you overwrite test fixtures with bad outputs?"
+    exit 1
+fi
 
 PYTHONPATH=. ./unstructured/ingest/main.py \
     --metadata-exclude filename \
