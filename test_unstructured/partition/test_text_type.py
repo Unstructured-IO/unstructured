5689287980e88ca93341ec95162035723df90c15
Matt Robinson <mrobinson@unstructuredai.io>
2023-04-14 13:07:21 -0400

    update tests

--- test_unstructured/partition/test_text_type.py
+++ test_unstructured/partition/test_text_type.py
@@ -212,10 +212,9 @@ def test_contains_english_word(text, expected, monkeypatch):
         ("Intellectual Property in the United States", True),
         ("Intellectual property helps incentivize innovation.", False),
         ("THIS IS ALL CAPS. BUT IT IS TWO SENTENCES.", False),
-        ("LOOK AT THIS IT IS CAPS BUT NOT A TITLE.", False),
         ("This Has All Caps. It's Weird But Two Sentences", False),
         ("The Business Report is expected within 6 hours of closing", False),
-        ("", False),
+        ("", True),
     ],
 )
 def test_contains_exceeds_cap_ratio(text, expected, monkeypatch):
