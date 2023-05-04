ba57374df91d8094e181d58704443abcb6c50aac
Matt Robinson <mrobinson@unstructuredai.io>
2023-05-04 14:04:43 -0400

    skip tests if not supported or in docker

--- test_unstructured/partition/test_auto.py
+++ test_unstructured/partition/test_auto.py
@@ -33,6 +33,7 @@ EXPECTED_EMAIL_OUTPUT = [
 
 is_in_docker = os.path.exists("/.dockerenv")
 rtf_not_supported = "rtf" not in pypandoc.get_pandoc_formats()[0]
+odt_not_supported = "odt" not in pypandoc.get_pandoc_formats()[0]
 
 
 def test_auto_partition_email_from_filename():
@@ -463,12 +464,16 @@ def test_auto_partition_works_with_unstructured_jsons_from_file():
     assert elements[0].text == "News Around NOAA"
 
 
+@pytest.mark.skipif(is_in_docker, reason="Skipping this test in Docker container")
+@pytest.mark.skipif(odt_not_supported, reason="odt not supported in this version of pypandoc.")
 def test_auto_partition_odt_from_filename():
     filename = os.path.join(EXAMPLE_DOCS_DIRECTORY, "fake.odt")
     elements = partition(filename=filename)
     assert elements == [Title("Lorem ipsum dolor sit amet.")]
 
 
+@pytest.mark.skipif(is_in_docker, reason="Skipping this test in Docker container")
+@pytest.mark.skipif(odt_not_supported, reason="odt not supported in this version of pypandoc.")
 def test_auto_partition_odt_from_file():
     filename = os.path.join(EXAMPLE_DOCS_DIRECTORY, "fake.odt")
     with open(filename, "rb") as f:
