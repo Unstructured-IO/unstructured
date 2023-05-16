083777d60d636c9a97c491ae9d0131b346cdd9d4
Matt Robinson <mrobinson@unstructuredai.io>
2023-05-16 11:16:47 -0400

    add support for files

--- unstructured/partition/xlsx.py
+++ unstructured/partition/xlsx.py
@@ -1,13 +1,38 @@
-from typing import List
+from tempfile import SpooledTemporaryFile
+from typing import IO, BinaryIO, List, Optional, Union, cast
 
 import pandas as pd
 
 from unstructured.documents.elements import Element, ElementMetadata, Table
+from unstructured.partition.common import exactly_one, spooled_to_bytes_io_if_needed
 from unstructured.partition.html import partition_html
 
 
-def partition_xlsx(filename: str) -> List[Element]:
-    sheets = pd.read_excel(filename, sheet_name=None)
+def partition_xlsx(
+    filename: Optional[str] = None,
+    file: Optional[Union[IO, SpooledTemporaryFile]] = None,
+    metadata_filename: Optional[str] = None,
+) -> List[Element]:
+    """Partitions Microsoft Excel Documents in .xlsx format into its document elements.
+
+    Parameters
+    ----------
+    filename
+        A string defining the target filename path.
+    file
+        A file-like object using "rb" mode --> open(filename, "rb").
+    metadata_filename
+        The filename to use for the metadata. Relevant because partition_doc converts the
+        document to .xlsx before partition. We want the original source filename in the
+        metadata.
+    """
+    exactly_one(filename=filename, file=file)
+
+    if filename:
+        sheets = pd.read_excel(filename, sheet_name=None)
+    else:
+        f = spooled_to_bytes_io_if_needed(cast(Union[BinaryIO, SpooledTemporaryFile], file))
+        sheets = pd.read_excel(f, sheet_name=None)
 
     elements: List[Element] = []
     page_number = 0
