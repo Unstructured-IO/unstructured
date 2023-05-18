695ed8f17c5cb7812985787c37c8ad6b14963c22
yuming <305248291@qq.com>
2023-05-18 16:39:17 -0400

    add test to test set

--- test_unstructured_ingest/test-ingest-wikipedia.sh
+++ test_unstructured_ingest/test-ingest-wikipedia.sh
@@ -11,6 +11,7 @@ PYTHONPATH=. ./unstructured/ingest/main.py \
     --structured-output-dir wikipedia-ingest-output \
     --num-processes 2 \
     --partition-strategy hi_res \
+    --download-dir files-ingest-download \
     --preserve-downloads \
     --verbose
 
