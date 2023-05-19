69ef1846236f450f19a27fe2c421e843519404ab
Matt Robinson <mrobinson@unstructuredai.io>
2023-05-19 14:04:41 -0400

    test number of files

--- test_unstructured_ingest/test-ingest-s3.sh
+++ test_unstructured_ingest/test-ingest-s3.sh
@@ -25,23 +25,29 @@ OVERWRITE_FIXTURES=${OVERWRITE_FIXTURES:-false}
 set +e
 
 # to update ingest test fixtures, run scripts/ingest-test-fixtures-update.sh on x86_64
-if [[ "$OVERWRITE_FIXTURES" != "false" ]]; then
+# if [[ "$OVERWRITE_FIXTURES" != "false" ]]; then
+#
+#     cp s3-small-batch-output/small-pdf-set/* test_unstructured_ingest/expected-structured-output/s3-small-batch/small-pdf-set/
+#
+# elif ! diff -ru test_unstructured_ingest/expected-structured-output/s3-small-batch s3-small-batch-output ; then
+#     echo
+#     echo "There are differences from the previously checked-in structured outputs."
+#     echo
+#     echo "If these differences are acceptable, overwrite by the fixtures by setting the env var:"
+#     echo
+#     echo "  export OVERWRITE_FIXTURES=true"
+#     echo
+#     echo "and then rerun this script."
+#     echo
+#     echo "NOTE: You'll likely just want to run scripts/ingest-test-fixtures-update.sh on x86_64 hardware"
+#     echo "to update fixtures for CI,"
+#     echo
+#     exit 1
+#
+# fi
 
-    cp s3-small-batch-output/small-pdf-set/* test_unstructured_ingest/expected-structured-output/s3-small-batch/small-pdf-set/
-
-elif ! diff -ru test_unstructured_ingest/expected-structured-output/s3-small-batch s3-small-batch-output ; then
-    echo
-    echo "There are differences from the previously checked-in structured outputs."
-    echo
-    echo "If these differences are acceptable, overwrite by the fixtures by setting the env var:"
-    echo
-    echo "  export OVERWRITE_FIXTURES=true"
-    echo
-    echo "and then rerun this script."
-    echo
-    echo "NOTE: You'll likely just want to run scripts/ingest-test-fixtures-update.sh on x86_64 hardware"
-    echo "to update fixtures for CI,"
-    echo
-    exit 1
-
+if [ "$(find 's3-small-batch-output' -type f -printf '.' | wc -c)" != 3 ]; then
+   echo
+   echo "3 files should have been created."
+   exit 1
 fi
