779b897be746e86afc3e38a77552b37670cc2b3a
Matt Robinson <mrobinson@unstructuredai.io>
2023-05-19 14:14:32 -0400

    only test good outputs

--- test_unstructured_ingest/test-ingest-s3.sh
+++ test_unstructured_ingest/test-ingest-s3.sh
@@ -5,11 +5,11 @@ set -e
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 cd "$SCRIPT_DIR"/.. || exit 1
 
-if [[ "$(find test_unstructured_ingest/expected-structured-output/s3-small-batch/ -type f -size +20k | wc -l)" -ne 3 ]]; then
-    echo "The test fixtures in test_unstructured_ingest/expected-structured-output/ look suspicious. At least one of the files is too small."
-    echo "Did you overwrite test fixtures with bad outputs?"
-    exit 1
-fi
+# if [[ "$(find test_unstructured_ingest/expected-structured-output/s3-small-batch/ -type f -size +20k | wc -l)" -ne 3 ]]; then
+#     echo "The test fixtures in test_unstructured_ingest/expected-structured-output/ look suspicious. At least one of the files is too small."
+#     echo "Did you overwrite test fixtures with bad outputs?"
+#     exit 1
+# fi
 
 PYTHONPATH=. ./unstructured/ingest/main.py \
     --metadata-exclude filename,file_directory \
@@ -25,29 +25,29 @@ OVERWRITE_FIXTURES=${OVERWRITE_FIXTURES:-false}
 set +e
 
 # to update ingest test fixtures, run scripts/ingest-test-fixtures-update.sh on x86_64
-# if [[ "$OVERWRITE_FIXTURES" != "false" ]]; then
-#
-#     cp s3-small-batch-output/small-pdf-set/* test_unstructured_ingest/expected-structured-output/s3-small-batch/small-pdf-set/
-#
-# elif ! diff -ru test_unstructured_ingest/expected-structured-output/s3-small-batch s3-small-batch-output ; then
-#     echo
-#     echo "There are differences from the previously checked-in structured outputs."
-#     echo
-#     echo "If these differences are acceptable, overwrite by the fixtures by setting the env var:"
-#     echo
-#     echo "  export OVERWRITE_FIXTURES=true"
-#     echo
-#     echo "and then rerun this script."
-#     echo
-#     echo "NOTE: You'll likely just want to run scripts/ingest-test-fixtures-update.sh on x86_64 hardware"
-#     echo "to update fixtures for CI,"
-#     echo
-#     exit 1
-#
-# fi
+if [[ "$OVERWRITE_FIXTURES" != "false" ]]; then
 
-if [ "$(find 's3-small-batch-output' -type f -printf '.' | wc -c)" != 3 ]; then
-   echo
-   echo "3 files should have been created."
-   exit 1
+    cp s3-small-batch-output/small-pdf-set/* test_unstructured_ingest/expected-structured-output/s3-small-batch/small-pdf-set/
+
+elif ! diff -ru test_unstructured_ingest/expected-structured-output/s3-small-batch s3-small-batch-output ; then
+    echo
+    echo "There are differences from the previously checked-in structured outputs."
+    echo
+    echo "If these differences are acceptable, overwrite by the fixtures by setting the env var:"
+    echo
+    echo "  export OVERWRITE_FIXTURES=true"
+    echo
+    echo "and then rerun this script."
+    echo
+    echo "NOTE: You'll likely just want to run scripts/ingest-test-fixtures-update.sh on x86_64 hardware"
+    echo "to update fixtures for CI,"
+    echo
+    exit 1
+
 fi
+#
+# if [ "$(find 's3-small-batch-output' -type f -printf '.' | wc -c)" != 3 ]; then
+#    echo
+#    echo "3 files should have been created."
+#    exit 1
+# fi
