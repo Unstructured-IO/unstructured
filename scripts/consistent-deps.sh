19d7c4431c2998a5768d56ca9d65fca049461add
Alan Bertl <alan@unstructured.io>
2023-05-23 21:39:25 -0500

    Add some comments

--- scripts/consistent-deps.sh
+++ scripts/consistent-deps.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+####################################################################################################
+# Check depedency consistency by forcing pip to resolve all the requirement .txt files at once
+# (without installing).
+####################################################################################################
+
 echo "Checking consistency of dependencies..."
 
+# Joins an array of strings using the specified delimiter.
 function join_by {
   local d=${1-} f=${2-}
   if shift 2; then
@@ -9,11 +15,14 @@ function join_by {
   fi
 }
 
+# NOTE(alan): Add any dependency files here we don't want to include in the resolution.
 excludefiles=("requirements/build.txt")
 
+# Build an array of requirements files.
 shopt -s nullglob
 reqfiles=(requirements/*.txt)
 
+# Remove the excluded files from the array of requirements files.
 for excludefile in "${excludefiles[@]}"; do
   for i in "${!reqfiles[@]}"; do
     if [[ ${reqfiles[i]} = "$excludefile" ]]; then
@@ -22,10 +31,11 @@ for excludefile in "${excludefiles[@]}"; do
   done
 done
 
-
+# Turn the requirement files array into pip -r flags.
 reqstring=$(join_by ' -r ' "${reqfiles[@]}")
 reqstring="-r ${reqstring}"
 
+# This pip command will attempt to resolve the dependencies without installing anything.
 pipcommand="pip install --dry-run --ignore-installed ${reqstring}"
 if $pipcommand >> /dev/null;
 then
