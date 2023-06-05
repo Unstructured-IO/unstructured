722a4be1e065f828fe3e1c240312569e24cb54ab
christinestraub <christinemstraub@gmail.com>
2023-06-05 10:58:14 -0700

    test: fix local test ingest script

--- test_unstructured_ingest/test-ingest-local.sh
+++ test_unstructured_ingest/test-ingest-local.sh
@@ -16,8 +16,8 @@ PYTHONPATH=. ./unstructured/ingest/main.py \
 
 set +e
 
-if [ "$(find 'local-ingest-output' -type f -printf '.' | wc -c)" != 7 ]; then
+if [ "$(find 'local-ingest-output' -type f -printf '.' | wc -c)" != 9 ]; then
    echo
-   echo "7 files should have been created."
+   echo "9 files should have been created."
    exit 1
 fi
