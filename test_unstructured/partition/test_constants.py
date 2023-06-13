53563a24114b2525816958a1601e5ed5af8fabc4
Coniferish <johnjennings702@gmail.com>
2023-06-13 11:47:49 -0500

    undo changes for testing locally

--- test_unstructured/partition/test_constants.py
+++ test_unstructured/partition/test_constants.py
@@ -8,20 +8,20 @@ EXPECTED_TABLE = """<table border="1" class="dataframe">
     <tr>
       <td>Blues</td>
       <td>STL</td>
-      <td>1.0</td>
+      <td>1</td>
     </tr>
     <tr>
       <td>Flyers</td>
       <td>PHI</td>
-      <td>2.0</td>
+      <td>2</td>
     </tr>
     <tr>
       <td>Maple Leafs</td>
       <td>TOR</td>
-      <td>13.0</td>
+      <td>13</td>
     </tr>
   </tbody>
 </table>"""
 
 
-EXPECTED_TEXT = "Team Location Stanley Cups Blues STL 1.0 Flyers PHI 2.0 Maple Leafs TOR 13.0"
+EXPECTED_TEXT = "Team Location Stanley Cups Blues STL 1.0 Flyers PHI 2.0 Maple Leafs TOR 13"
