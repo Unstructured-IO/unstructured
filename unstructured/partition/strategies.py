43286afd99691b9a57988d91dc1497e88bfbbb61
yuming <305248291@qq.com>
2023-06-13 11:41:10 -0400

    more note

--- unstructured/partition/strategies.py
+++ unstructured/partition/strategies.py
@@ -76,6 +76,7 @@ def determine_pdf_or_image_strategy(
     if is_image:
         # Note(yuming): There is no fast strategy for images,
         # use ocr_only as a fallback plan for consistency with PDFs.
+        # This can be removed once unstructured-api use auto as the default strategy.
         if strategy == "fast":
             strategy = "ocr_only"
         validate_strategy(strategy, "image")
