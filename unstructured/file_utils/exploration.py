0f0fc7d80add325124c197b2791289d899ac7ff7
Matt Robinson <mrobinson@unstructuredai.io>
2023-06-15 15:19:58 -0400

    fix file content checking bug

--- unstructured/file_utils/exploration.py
+++ unstructured/file_utils/exploration.py
@@ -61,7 +61,10 @@ def get_file_info_from_file_contents(
         data["filename"] = []
 
     for i, file_content in enumerate(file_contents):
-        _, content_string = file_content.split(",")
+        if "," in file_content:
+            _, content_string = file_content.split(",")
+        else:
+            content_string = file_content
         content_bytes = base64.b64decode(content_string)
         f = io.BytesIO(content_bytes)
         filetype = detect_filetype(file=f)
