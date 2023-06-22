249c13a7be8a6d9a3b5089774b3971d72816a544
Emily Chen <emily@unstructured.io>
2023-06-21 19:36:24 -0700

    Add a test to exercise --ocr-languages parameter with ingest CLI

---
+++ test_unstructured_ingest/test-ingest-parameters.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+cd "$SCRIPT_DIR"/.. || exit 1
+
+PYTHONPATH=. ./unstructured/ingest/main.py \
+    --metadata-exclude filename,file_directory,metadata.data_source.date_processed \
+    --local-input-path example-docs/english-and-korean.png \
+    --structured-output-dir parameterized-ingest-output \
+    --partition-strategy hi_res \
+    --ocr-languages eng+kor \
+    --verbose \
+    --reprocess
+
+set +e
+
+if [ "$(cat parameterized-ingest-output/example-docs/english-and-korean.png.json | grep 안녕하세요| wc -l)" != 1 ]; then
+   echo
+   echo "--ocr-languages parameter did not work. Did not partition with the specified language pack."
+   exit 1
+fi
