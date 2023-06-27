54c59ccbd15d4461b199deb049b5c682ae77adb7
Matt Robinson <mrobinson@unstructuredai.io>
2023-06-27 12:54:44 -0400

    added test for splitting long document

--- test_unstructured/partition/test_text.py
+++ test_unstructured/partition/test_text.py
@@ -154,3 +154,10 @@ def test_partition_text_extract_regex_metadata():
     assert elements[0].metadata.regex_metadata == {
         "speaker": [{"text": "SPEAKER 1", "start": 0, "end": 9}],
     }
+
+
+def test_partition_text_splits_long_text(filename="example-docs/norwich-city.txt"):
+    elements = partition_text(filename=filename)
+    assert len(elements) > 0
+    assert elements[0].text.startswith("Iwan Roberts")
+    assert elements[-1].text.endswith("External links")
