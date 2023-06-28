773d9a4f375064d706eeba07c7c92176edc5e1c6
qued <64741807+qued@users.noreply.github.com>
2023-06-27 23:06:08 -0500

    feat: choose model (#824)
    Added the ability to select the hi_res model via the environment variable UNSTRUCTURED_HI_RES_MODEL_NAME. Variable must be a string that matches up with a model name defined in unstructured_inference.
    
    Also removed code related to old unstructured_inference API which has been removed from currently pinned version of unstructured-inference and is no longer running as a service.

--- unstructured/partition/image.py
+++ unstructured/partition/image.py
@@ -9,9 +9,6 @@ from unstructured.partition.pdf import partition_pdf_or_image
 def partition_image(
     filename: str = "",
     file: Optional[bytes] = None,
-    url: Optional[str] = None,
-    template: Optional[str] = None,
-    token: Optional[str] = None,
     include_page_breaks: bool = False,
     ocr_languages: str = "eng",
     strategy: str = "auto",
@@ -25,14 +22,6 @@ def partition_image(
         A string defining the target filename path.
     file
         A file-like object as bytes --> open(filename, "rb").
-    template
-        A string defining the model to be used. Default None uses default model ("layout/image" url
-        if using the API).
-    url
-        A string endpoint to self-host an inference API, if desired. If None, local inference will
-        be used.
-    token
-        A string defining the authentication token for a self-host url, if applicable.
     ocr_languages
         The languages to use for the Tesseract agent. To use a language, you'll first need
         to install the appropriate Tesseract language pack.
@@ -46,15 +35,10 @@ def partition_image(
     """
     exactly_one(filename=filename, file=file)
 
-    if template is None:
-        template = "layout/image"
-
     return partition_pdf_or_image(
         filename=filename,
         file=file,
-        url=url,
-        template=template,
-        token=token,
+        is_image=True,
         include_page_breaks=include_page_breaks,
         ocr_languages=ocr_languages,
         strategy=strategy,
