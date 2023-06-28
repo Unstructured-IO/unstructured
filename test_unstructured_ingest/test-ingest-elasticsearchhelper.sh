28740ce34dcc8d43a4a1ff8033b01e3d3d130c82
Ahmet Melek <ahmetmeleq@gmail.com>
2023-06-28 23:27:51 +0100

    isolate elasticsearch cluster creation and filling in a new script

---
+++ test_unstructured_ingest/test-ingest-elasticsearchhelper.sh
@@ -0,0 +1,35 @@
+# Create the elasticsearch cluster and get the container id
+output=$(docker run -d --rm -p 9200:9200 -p 9300:9300 -e "xpack.security.enabled=false" -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:8.7.0)
+container_id=$(echo "$output" | cut -c 1-12)
+
+echo $container_id > "test_unstructured_ingest/test-ingest-elasticsearch-docker_container_id.txt" &
+
+url="http://localhost:9200/_cluster/health"
+status_code=0
+retry_count=0
+max_retries=6
+
+# Check the cluster status repeatedly until it becomes live or maximum retries are reached
+while [ "$status_code" -ne 200 ] && [ "$retry_count" -lt "$max_retries" ]; do
+
+  # Send a GET request to the cluster health API
+  response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+  status_code="$response"
+
+  # Process the files only when the ES cluster is live
+  if [ "$status_code" -eq 200 ]; then
+    echo "Cluster is live."
+
+    python examples/ingest/elasticsearch/elasticsearch_cluster.py
+
+  else
+    ((retry_count++))
+    echo "Cluster is not available. Retrying in 5 seconds... (Attempt $retry_count)"
+    sleep 5
+  fi
+done
+
+# If cluster has not got live, exit after a certain number of tries
+if [ "$status_code" -ne 200 ]; then
+  echo "Cluster took an unusually long time to create (>25 seconds). Expected time is around 10 seconds. Exiting."
+fi
