a516a912ed551147d9b65dd820743f028990e644
rvztz <rvztz@MacBook-Pro-de-rvztz.local>
2023-06-29 16:40:10 -0600

    Linted

--- unstructured/ingest/connector/one_drive.py
+++ unstructured/ingest/connector/one_drive.py
@@ -3,6 +3,7 @@ import os
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import TYPE_CHECKING
+
 from unstructured.file_utils.filetype import EXT_TO_FILETYPE
 from unstructured.ingest.interfaces import (
     BaseConnector,
@@ -10,7 +11,6 @@ from unstructured.ingest.interfaces import (
     BaseIngestDoc,
     StandardConnectorConfig,
 )
-
 from unstructured.ingest.logger import logger
 from unstructured.utils import requires_dependencies
 
@@ -20,49 +20,51 @@ if TYPE_CHECKING:
 
 MAX_MB_SIZE = 512_000_000
 
+
 @dataclass
 class SimpleOneDriveConfig(BaseConnectorConfig):
-
     client_id: str
     client_credential: str = field(repr=False)
-    user_pname: str 
-    tenant: str= field(repr=False)
+    user_pname: str
+    tenant: str = field(repr=False)
     authority_url: str = field(repr=False)
     recursive: bool = False
 
     def __post_init__(self):
         if not (self.client_id and self.client_credential and self.user_pname):
             raise ValueError(
-                'Please provide one of the following mandatory values:'
-                '\n-client_id\n-client_credential\n-user_principal_name'
+                "Please provide one of the following mandatory values:"
+                "\n-client_id\n-client_credential\n-user_principal_name",
             )
         self.token_factory = self._acquire_token
 
     @requires_dependencies(["msal"])
     def _acquire_token(self):
         from msal import ConfidentialClientApplication
+
         try:
             app = ConfidentialClientApplication(
-                authority=f'{self.authority_url}/{self.tenant}',
+                authority=f"{self.authority_url}/{self.tenant}",
                 client_id=self.client_id,
-                client_credential=self.client_credential)
+                client_credential=self.client_credential,
+            )
             token = app.acquire_token_for_client(scopes=["https://graph.microsoft.com/.default"])
         except ValueError as exc:
             logger.error("Couldn't set up credentials for OneDrive")
             raise exc
         return token
 
+
 @dataclass
 class OneDriveIngestDoc(BaseIngestDoc):
-
     config: SimpleOneDriveConfig
     file: "driveItem"
 
     def __post_init__(self):
-        self.ext = ''.join(Path(self.file.name).suffixes)
+        self.ext = "".join(Path(self.file.name).suffixes)
         if not self.ext:
-            raise ValueError(f"Unsupported file without extension. ")
-        
+            raise ValueError("Unsupported file without extension.")
+
         if self.ext not in EXT_TO_FILETYPE.keys():
             raise ValueError(
                 f"Extension not supported. "
@@ -70,20 +72,20 @@ class OneDriveIngestDoc(BaseIngestDoc):
             )
         self._set_download_paths()
 
-    def _set_download_paths(self) -> str:
+    def _set_download_paths(self) -> None:
         """Sets download and output directories"""
-        dpath = Path(f'{self.standard_config.download_dir}')
-        opath = Path(f'{self.standard_config.output_dir}')
+        dpath = Path(f"{self.standard_config.download_dir}")
+        opath = Path(f"{self.standard_config.output_dir}")
 
-        if pref := self.file.get_property('parentReference','').path.split(':')[-1]:
-            odir = pref[1:] if pref[0] == '/' else pref
-            dpath = dpath if odir == '' else (dpath / odir).resolve()
-            opath = opath if odir == '' else (opath / odir).resolve()
+        if pref := self.file.get_property("parentReference", "").path.split(":")[-1]:
+            odir = pref[1:] if pref[0] == "/" else pref
+            dpath = dpath if odir == "" else (dpath / odir).resolve()
+            opath = opath if odir == "" else (opath / odir).resolve()
 
         dname = f'{self.file.get_property("id")}-{self.file.name}'
         self.download_dir = dpath
         self.download_filepath = (dpath / dname).resolve()
-        oname = f'{dname[:-len(self.ext)]}.json'
+        oname = f"{dname[:-len(self.ext)]}.json"
         self.output_dir = opath
         self.output_filepath = (opath / oname).resolve()
 
@@ -107,7 +109,7 @@ class OneDriveIngestDoc(BaseIngestDoc):
         """Determine if structured output for this doc already exists."""
         return self._output_filename().is_file() and self._output_filename().stat()
 
-    @requires_dependencies(['office365'])
+    @requires_dependencies(["office365"])
     def get_file(self):
         if (
             not self.standard_config.re_download
@@ -118,19 +120,19 @@ class OneDriveIngestDoc(BaseIngestDoc):
             return
 
         try:
-            fsize = self.file.get_property('size', 0)
+            fsize = self.file.get_property("size", 0)
             self.output_dir.mkdir(parents=True, exist_ok=True)
-            
+
             if not self.download_dir.is_dir():
                 logger.debug(f"Creating directory: {self.download_dir}")
                 self.download_dir.mkdir(parents=True, exist_ok=True)
 
             if fsize > MAX_MB_SIZE:
-                logger.info(f'Downloading file with size: {fsize} bytes in chunks')
-                with self.filename.open(mode='wb') as f:
-                    self.file.download_session(f, chunk_size = 1024 * 1024 * 100).execute_query()
+                logger.info(f"Downloading file with size: {fsize} bytes in chunks")
+                with self.filename.open(mode="wb") as f:
+                    self.file.download_session(f, chunk_size=1024 * 1024 * 100).execute_query()
             else:
-                with self.filename.open(mode='wb') as f:
+                with self.filename.open(mode="wb") as f:
                     self.file.download(f).execute_query()
 
         except Exception as e:
@@ -138,12 +140,11 @@ class OneDriveIngestDoc(BaseIngestDoc):
             logger.error(e)
             return
 
-        logger.info(f'File downloaded: {self.filename}')
+        logger.info(f"File downloaded: {self.filename}")
         return
 
     def write_result(self):
         """Write the structured json result for this doc. result must be json serializable."""
-
 
         if self.standard_config.download_only:
             return
@@ -154,13 +155,12 @@ class OneDriveIngestDoc(BaseIngestDoc):
             logger.debug(f"Creating directory: {self.output_dir}")
             self.output_dir.mkdir(parents=True, exist_ok=True)
 
-        with output_filename.open(mode='w') as output_f:
+        with output_filename.open(mode="w") as output_f:
             output_f.write(json.dumps(self.isd_elems_no_filename[0], ensure_ascii=False, indent=2))
         logger.info(f"Wrote {output_filename}")
 
 
 class OneDriveConnector(BaseConnector):
-
     config: SimpleOneDriveConfig
 
     def __init__(self, standard_config: StandardConnectorConfig, config: SimpleOneDriveConfig):
@@ -170,21 +170,22 @@ class OneDriveConnector(BaseConnector):
         )
         self._set_client()
 
-    @requires_dependencies(['office365'])
+    @requires_dependencies(["office365"])
     def _set_client(self):
         from office365.graph_client import GraphClient
+
         self.client = GraphClient(self.config.token_factory)
-    
+
     def _list_objects(self, folder, recursive) -> list:
         drive_items = folder.children.get().execute_query()
         files = [d for d in drive_items if d.is_file]
         if not recursive:
-            return files 
+            return files
         folders = [d for d in drive_items if d.is_folder]
         for f in folders:
             files += self._list_objects(f, recursive)
         return files
-    
+
     def cleanup(self, cur_dir=None):
         if not self.cleanup_files:
             return
@@ -208,4 +209,3 @@ class OneDriveConnector(BaseConnector):
         drive = self.client.users[self.config.user_pname].drive.get().execute_query()
         files = self._list_objects(drive.root, self.config.recursive)
         return [OneDriveIngestDoc(self.standard_config, self.config, f) for f in files]
-        
--- unstructured/ingest/main.py
+++ unstructured/ingest/main.py
@@ -379,28 +379,28 @@ class MainProcess:
 )
 @click.option(
     "--ms-client-id",
-    default = None,
-    help="Microsoft app client ID"
+    default=None,
+    help="Microsoft app client ID",
 )
 @click.option(
     "--ms-client-cred",
     default=None,
-    help="Microsoft App client secret"
+    help="Microsoft App client secret",
 )
 @click.option(
     "--ms-authority-url",
-    default='https://login.microsoftonline.com',
-    help="Authentication token provider for MS apps, default is https://login.microsoftonline.com"
+    default="https://login.microsoftonline.com",
+    help="Authentication token provider for MS apps, default is https://login.microsoftonline.com",
 )
 @click.option(
     "--ms-tenant",
-    default='common',
-    help="ID or domain name associated with your Azure AD instance"
+    default="common",
+    help="ID or domain name associated with your Azure AD instance",
 )
 @click.option(
     "--ms-user-pname",
     default=None,
-    help="User principal name, usually is your Azure AD email."
+    help="User principal name, usually is your Azure AD email.",
 )
 @click.option(
     "--download-dir",
@@ -793,22 +793,21 @@ def main(
     elif ms_client_id or ms_user_pname:
         from unstructured.ingest.connector.one_drive import (
             OneDriveConnector,
-            SimpleOneDriveConfig
+            SimpleOneDriveConfig,
         )
 
-        doc_connector = OneDriveConnector(
+        doc_connector = OneDriveConnector( # type: ignore
             standard_config=standard_config,
             config=SimpleOneDriveConfig(
-                client_id= ms_client_id,
-                client_credential= ms_client_cred,
+                client_id=ms_client_id,
+                client_credential=ms_client_cred,
                 user_pname=ms_user_pname,
                 tenant=ms_tenant,
                 authority_url=ms_authority_url,
-                recursive=recursive
-            )
-            
+                recursive=recursive,
+            ),
         )
-    
+
     elif local_input_path:
         from unstructured.ingest.connector.local import (
             LocalConnector,
@@ -828,7 +827,7 @@ def main(
 
     else:
         logger.error("No connector-specific option was specified!")
-        sys.exit(1)
+        sys.exit(1)        
 
     process_document_with_partition_strategy = partial(
         process_document,
