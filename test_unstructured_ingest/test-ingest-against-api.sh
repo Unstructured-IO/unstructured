b169391b141894f272caaad4991ecbd61550db7d
Crag Wolfe <crag@unstructuredai.io>
2023-06-28 18:53:27 -0700

    shellcheck again

--- test_unstructured_ingest/test-ingest-against-api.sh
+++ test_unstructured_ingest/test-ingest-against-api.sh
@@ -6,22 +6,22 @@ set -e
 # shellcheck disable=SC2317
 exit 0
 
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-cd "$SCRIPT_DIR"/.. || exit 1
-
-PYTHONPATH=. ./unstructured/ingest/main.py \
-    --local-input-path example-docs \
-    --local-file-glob "*.pdf" \
-    --structured-output-dir api-ingest-output \
-    --partition-by-api \
-    --partition-strategy hi_res \
-    --verbose \
-    --reprocess
-
-set +e
-
-if [ "$(find 'api-ingest-output' -type f -printf '.' | wc -c)" != 8 ]; then
-   echo
-   echo "8 files should have been created."
-   exit 1
-fi
+#SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+#cd "$SCRIPT_DIR"/.. || exit 1
+#
+#PYTHONPATH=. ./unstructured/ingest/main.py \
+#    --local-input-path example-docs \
+#    --local-file-glob "*.pdf" \
+#    --structured-output-dir api-ingest-output \
+#    --partition-by-api \
+#    --partition-strategy hi_res \
+#    --verbose \
+#    --reprocess
+#
+#set +e
+#
+#if [ "$(find 'api-ingest-output' -type f -printf '.' | wc -c)" != 8 ]; then
+#   echo
+#   echo "8 files should have been created."
+#   exit 1
+#fi
