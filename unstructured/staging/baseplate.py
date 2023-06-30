ceb25899fdbff025bb50406c62b2f3914d51bcfe
Emily Chen <emily@unstructured.io>
2023-06-30 11:45:20 -0700

    Assume Baseplate can't convert object values to a column

--- unstructured/staging/baseplate.py
+++ unstructured/staging/baseplate.py
@@ -32,13 +32,27 @@ def stage_for_baseplate(elements: List[Text]) -> BaseplateRows:
     https://docs.baseplate.ai/api-reference/documents/overview
     https://docs.baseplate.ai/api-reference/documents/upsert-data-rows
     """
+
+    def flatten_dict(dictionary, parent_key="", separator="_"):
+        flattened_dict = {}
+        for key, value in dictionary.items():
+            new_key = f"{parent_key}{separator}{key}" if parent_key else key
+            if isinstance(value, dict):
+                flattened_dict.update(flatten_dict(value, new_key, separator))
+            else:
+                flattened_dict[new_key] = value
+        return flattened_dict
+
     rows: List[BaseplateRow] = []
     for element in elements:
         element_dict = element.to_dict()
         metadata = element_dict.pop("metadata")
         row: BaseplateRow = {
-            "data": element_dict,
-            "metadata": metadata,
+            # Baseplate maps each key in the row's data object to a column in the dataset and
+            # each key in the row's metadata object to a metadata column in the dataset.
+            # We infer that Baseplate cannot map a nested object to a column in its dataset.
+            "data": flatten_dict(element_dict),
+            "metadata": flatten_dict(metadata),
         }
         rows.append(row)
 
