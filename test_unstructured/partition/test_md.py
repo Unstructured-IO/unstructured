9722883a467bc3805dc32c7d0a17665303435f97
kravetsmic <kravets.mic@gmail.com>
2023-07-13 12:10:13 +0300

    feat: add test for md partition

--- test_unstructured/partition/test_md.py
+++ test_unstructured/partition/test_md.py
@@ -68,7 +68,9 @@ def test_partition_md_from_url():
     with open(filename) as f:
         text = f.read()
 
-    response = MockResponse(text=text, status_code=200, headers={"Content-Type": "text/markdown"})
+    response = MockResponse(
+        text=text, status_code=200, headers={"Content-Type": "text/markdown"}
+    )
     with patch.object(requests, "get", return_value=response) as _:
         elements = partition_md(url="https://fake.url")
 
@@ -82,7 +84,9 @@ def test_partition_md_from_url_raises_with_bad_status_code():
     with open(filename) as f:
         text = f.read()
 
-    response = MockResponse(text=text, status_code=500, headers={"Content-Type": "text/html"})
+    response = MockResponse(
+        text=text, status_code=500, headers={"Content-Type": "text/html"}
+    )
     with patch.object(requests, "get", return_value=response) as _:
         with pytest.raises(ValueError):
             partition_md(url="https://fake.url")
@@ -139,3 +143,103 @@ def test_partition_md_from_text_exclude_metadata():
     elements = partition_md(text=text, include_metadata=False)
     for i in range(len(elements)):
         assert elements[i].metadata.to_dict() == {}
+
+
+def test_partition_md_metadata_date(
+    mocker,
+    filename="example-docs/README.md",
+):
+    mocked_last_modification_date = "2029-07-05T09:24:28"
+
+    mocker.patch(
+        "unstructured.partition.md.get_last_modified_date",
+        return_value=mocked_last_modification_date,
+    )
+
+    elements = partition_md(
+        filename=filename,
+    )
+
+    assert elements[0].metadata.date == mocked_last_modification_date
+
+
+def test_partition_md_with_custom_metadata_date(
+    mocker,
+    filename="example-docs/README.md",
+):
+    mocked_last_modification_date = "2029-07-05T09:24:28"
+    expected_last_modification_date = "2020-07-05T09:24:28"
+
+    mocker.patch(
+        "unstructured.partition.md.get_last_modified_date",
+        return_value=mocked_last_modification_date,
+    )
+
+    elements = partition_md(
+        filename=filename, metadata_date=expected_last_modification_date
+    )
+
+    assert elements[0].metadata.date == expected_last_modification_date
+
+
+def test_partition_md_from_file_metadata_date(
+    mocker,
+    filename="example-docs/README.md",
+):
+    mocked_last_modification_date = "2029-07-05T09:24:28"
+
+    mocker.patch(
+        "unstructured.partition.md.get_last_modified_date_from_file",
+        return_value=mocked_last_modification_date,
+    )
+
+    with open(filename, "rb") as f:
+        elements = partition_md(
+            file=f,
+        )
+
+    assert elements[0].metadata.date == mocked_last_modification_date
+
+
+def test_partition_md_from_file_with_custom_metadata_date(
+    mocker,
+    filename="example-docs/README.md",
+):
+    mocked_last_modification_date = "2029-07-05T09:24:28"
+    expected_last_modification_date = "2020-07-05T09:24:28"
+
+    mocker.patch(
+        "unstructured.partition.md.get_last_modified_date_from_file",
+        return_value=mocked_last_modification_date,
+    )
+
+    with open(filename, "rb") as f:
+        elements = partition_md(file=f, metadata_date=expected_last_modification_date)
+
+    assert elements[0].metadata.date == expected_last_modification_date
+
+
+def test_partition_md_from_text_metadata_date(
+    filename="example-docs/README.md",
+):
+    with open(filename) as f:
+        text = f.read()
+
+    elements = partition_md(
+        text=text,
+    )
+
+    assert elements[0].metadata.date is None
+
+
+def test_partition_md_from_text_with_custom_metadata_date(
+    filename="example-docs/README.md",
+):
+    expected_last_modification_date = "2020-07-05T09:24:28"
+
+    with open(filename) as f:
+        text = f.read()
+
+    elements = partition_md(text=text, metadata_date=expected_last_modification_date)
+
+    assert elements[0].metadata.date == expected_last_modification_date
