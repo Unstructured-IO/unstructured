706ac9a3c771d9ed6d1b504d3b0c26bf189720c4
kravetsmic <kravets.mic@gmail.com>
2023-07-13 17:11:00 +0300

    feat: add metadata_date for .xlsx partition

--- unstructured/partition/xlsx.py
+++ unstructured/partition/xlsx.py
@@ -1,4 +1,5 @@
 from tempfile import SpooledTemporaryFile
+from datetime import datetime
 from typing import IO, BinaryIO, List, Optional, Union, cast
 
 import lxml.html
@@ -11,7 +12,12 @@ from unstructured.documents.elements import (
     process_metadata,
 )
 from unstructured.file_utils.filetype import FileType, add_metadata_with_filetype
-from unstructured.partition.common import exactly_one, spooled_to_bytes_io_if_needed
+from unstructured.partition.common import (
+    exactly_one,
+    spooled_to_bytes_io_if_needed,
+    get_last_modified_date,
+    get_last_modified_date_from_file,
+)
 
 
 @process_metadata()
@@ -21,6 +27,7 @@ def partition_xlsx(
     file: Optional[Union[IO[bytes], SpooledTemporaryFile]] = None,
     metadata_filename: Optional[str] = None,
     include_metadata: bool = True,
+    metadata_date: Optional[datetime] = None,
     **kwargs,
 ) -> List[Element]:
     """Partitions Microsoft Excel Documents in .xlsx format into its document elements.
@@ -33,14 +40,21 @@ def partition_xlsx(
         A file-like object using "rb" mode --> open(filename, "rb").
     include_metadata
         Determines whether or not metadata is included in the output.
+    modification_date
+        The day of the last modification
     """
     exactly_one(filename=filename, file=file)
-
+    last_modification_date = None
     if filename:
         sheets = pd.read_excel(filename, sheet_name=None)
+        last_modification_date = get_last_modified_date(filename)
+
     else:
-        f = spooled_to_bytes_io_if_needed(cast(Union[BinaryIO, SpooledTemporaryFile], file))
+        f = spooled_to_bytes_io_if_needed(
+            cast(Union[BinaryIO, SpooledTemporaryFile], file)
+        )
         sheets = pd.read_excel(f, sheet_name=None)
+        last_modification_date = get_last_modified_date_from_file(file)
 
     elements: List[Element] = []
     page_number = 0
@@ -55,6 +69,7 @@ def partition_xlsx(
                 page_name=sheet_name,
                 page_number=page_number,
                 filename=metadata_filename or filename,
+                date=metadata_date or last_modification_date,
             )
         else:
             metadata = ElementMetadata()
