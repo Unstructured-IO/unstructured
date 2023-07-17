f08d848cd0aa27e50395308d4d0a8e759064ac30
kravetsmic <kravets.mic@gmail.com>
2023-07-17 11:20:18 +0300

    feat: add tests to html partition

--- test_unstructured/partition/test_html_partition.py
+++ test_unstructured/partition/test_html_partition.py
@@ -8,6 +8,7 @@ from requests.models import Response
 
 from unstructured.cleaners.core import clean_extra_whitespace
 from unstructured.documents.elements import Title
+from unstructured.documents.html import HTMLTitle
 from unstructured.partition.html import partition_html
 
 DIRECTORY = pathlib.Path(__file__).parent.resolve()
@@ -319,3 +320,61 @@ def test_partition_html_from_filename_exclude_metadata():
     assert "PageBreak" not in [elem.category for elem in elements]
     assert elements[0].metadata.filename is None
     assert elements[0].metadata.file_directory is None
+
+
+def test_partition_html_with_include_element_types(
+    filename="example-docs/winter-sports.epub",
+):
+    element_types = [HTMLTitle]
+    elements = partition_html(
+        filename=filename,
+        include_metadata=False,
+        include_element_types=element_types,
+    )
+
+    for element in elements:
+        assert type(element) in element_types
+
+
+def test_partition_html_with_exclude_element_types(
+    filename="example-docs/winter-sports.epub",
+):
+    element_types = [HTMLTitle]
+    elements = partition_html(
+        filename=filename,
+        include_metadata=False,
+        exclude_element_types=element_types,
+    )
+
+    for element in elements:
+        assert type(element) not in element_types
+
+
+def test_partition_html_from_file_with_include_element_types(
+    filename="example-docs/winter-sports.epub",
+):
+    element_types = [HTMLTitle]
+    with open(filename, "rb") as f:
+        elements = partition_html(
+            file=f,
+            include_metadata=False,
+            include_element_types=element_types,
+        )
+
+    for element in elements:
+        assert type(element) in element_types
+
+
+def test_partition_html_from_file_with_exclude_element_types(
+    filename="example-docs/winter-sports.epub",
+):
+    element_types = [HTMLTitle]
+    with open(filename, "rb") as f:
+        elements = partition_html(
+            file=f,
+            include_metadata=False,
+            exclude_element_types=element_types,
+        )
+
+    for element in elements:
+        assert type(element) not in element_types
