4cae15b4495dad9efa043b2aaf7a1cc382e50cac
kravetsmic <kravets.mic@gmail.com>
2023-07-18 10:00:41 +0300

    feat: add tests to rst partition

--- test_unstructured/partition/test_rst.py
+++ test_unstructured/partition/test_rst.py
@@ -1,4 +1,5 @@
 from unstructured.documents.elements import Title
+from unstructured.documents.html import HTMLTitle
 from unstructured.partition.rst import partition_rst
 
 
@@ -10,7 +11,9 @@ def test_partition_rst_from_filename(filename="example-docs/README.rst"):
         assert element.metadata.filename == "README.rst"
 
 
-def test_partition_rst_from_filename_with_metadata_filename(filename="example-docs/README.rst"):
+def test_partition_rst_from_filename_with_metadata_filename(
+    filename="example-docs/README.rst",
+):
     elements = partition_rst(filename=filename, metadata_filename="test")
     assert all(element.metadata.filename == "test" for element in elements)
 
@@ -24,7 +27,9 @@ def test_partition_rst_from_file(filename="example-docs/README.rst"):
         assert element.metadata.filename is None
 
 
-def test_partition_rst_from_file_with_metadata_filename(filename="example-docs/README.rst"):
+def test_partition_rst_from_file_with_metadata_filename(
+    filename="example-docs/README.rst",
+):
     with open(filename, "rb") as f:
         elements = partition_rst(file=f, metadata_filename="test")
     assert elements[0] == Title("Example Docs")
@@ -32,7 +37,9 @@ def test_partition_rst_from_file_with_metadata_filename(filename="example-docs/R
         assert element.metadata.filename == "test"
 
 
-def test_partition_rst_from_filename_exclude_metadata(filename="example-docs/README.rst"):
+def test_partition_rst_from_filename_exclude_metadata(
+    filename="example-docs/README.rst",
+):
     elements = partition_rst(filename=filename, include_metadata=False)
 
     for i in range(len(elements)):
@@ -45,3 +52,61 @@ def test_partition_rst_from_file_exclude_metadata(filename="example-docs/README.
 
     for i in range(len(elements)):
         assert elements[i].metadata.to_dict() == {}
+
+
+def test_partition_rst_with_include_element_types(
+    filename="example-docs/README.rst",
+):
+    element_types = [HTMLTitle]
+    elements = partition_rst(
+        filename=filename,
+        include_metadata=False,
+        include_element_types=element_types,
+    )
+
+    for element in elements:
+        assert type(element) in element_types
+
+
+def test_partition_rst_with_exclude_element_types(
+    filename="example-docs/README.rst",
+):
+    element_types = [HTMLTitle]
+    elements = partition_rst(
+        filename=filename,
+        include_metadata=False,
+        exclude_element_types=element_types,
+    )
+
+    for element in elements:
+        assert type(element) not in element_types
+
+
+def test_partition_rst_from_file_with_include_element_types(
+    filename="example-docs/README.rst",
+):
+    element_types = [HTMLTitle]
+    with open(filename, "rb") as f:
+        elements = partition_rst(
+            file=f,
+            include_metadata=False,
+            include_element_types=element_types,
+        )
+
+    for element in elements:
+        assert type(element) in element_types
+
+
+def test_partition_rst_from_file_with_exclude_element_types(
+    filename="example-docs/README.rst",
+):
+    element_types = [HTMLTitle]
+    with open(filename, "rb") as f:
+        elements = partition_rst(
+            file=f,
+            include_metadata=False,
+            exclude_element_types=element_types,
+        )
+
+    for element in elements:
+        assert type(element) not in element_types
