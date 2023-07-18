98ecf5ff10c4c6d5fd8a38236a6e29805e32e9cd
kravetsmic <kravets.mic@gmail.com>
2023-07-18 09:13:29 +0300

    feat: add include/exclude to partition_odt

--- unstructured/partition/odt.py
+++ unstructured/partition/odt.py
@@ -12,6 +12,8 @@ def partition_odt(
     file: Optional[IO[bytes]] = None,
     include_metadata: bool = True,
     metadata_filename: Optional[str] = None,
+    include_element_types: Optional[List[Element]] = None,
+    exclude_element_types: Optional[List[Element]] = None,
     **kwargs,
 ) -> List[Element]:
     """Partitions Open Office Documents in .odt format into its document elements.
@@ -22,10 +24,16 @@ def partition_odt(
         A string defining the target filename path.
     file
         A file-like object using "rb" mode --> open(filename, "rb").
+    include_element_types
+        Determines which Elements included in the output.
+    exclude_element_types
+        Determines which Elements excluded in the output.
     """
     return convert_and_partition_docx(
         source_format="odt",
         filename=filename,
         file=file,
         metadata_filename=metadata_filename,
+        include_element_types=include_element_types,
+        exclude_element_types=exclude_element_types,
     )
