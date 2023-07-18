c7aaa82412e9bb4ed10f67b02633555dbd01ee2e
kravetsmic <kravets.mic@gmail.com>
2023-07-18 10:06:34 +0300

    feat: add tests to rtf partition

--- test_unstructured/partition/test_rtf.py
+++ test_unstructured/partition/test_rtf.py
@@ -2,6 +2,7 @@ import os
 import pathlib
 
 from unstructured.documents.elements import Title
+from unstructured.documents.html import HTMLTitle
 from unstructured.partition.rtf import partition_rtf
 
 DIRECTORY = pathlib.Path(__file__).parent.resolve()
@@ -55,3 +56,61 @@ def test_partition_rtf_from_file_exclude_metadata():
         elements = partition_rtf(file=f, include_metadata=False)
     for i in range(len(elements)):
         assert elements[i].metadata.to_dict() == {}
+
+
+def test_partition_rtf_with_include_element_types(
+    filename="example-docs/fake-doc.rtf",
+):
+    element_types = [HTMLTitle]
+    elements = partition_rtf(
+        filename=filename,
+        include_metadata=False,
+        include_element_types=element_types,
+    )
+
+    for element in elements:
+        assert type(element) in element_types
+
+
+def test_partition_rtf_with_exclude_element_types(
+    filename="example-docs/fake-doc.rtf",
+):
+    element_types = [HTMLTitle]
+    elements = partition_rtf(
+        filename=filename,
+        include_metadata=False,
+        exclude_element_types=element_types,
+    )
+
+    for element in elements:
+        assert type(element) not in element_types
+
+
+def test_partition_rtf_from_file_with_include_element_types(
+    filename="example-docs/fake-doc.rtf",
+):
+    element_types = [HTMLTitle]
+    with open(filename, "rb") as f:
+        elements = partition_rtf(
+            file=f,
+            include_metadata=False,
+            include_element_types=element_types,
+        )
+
+    for element in elements:
+        assert type(element) in element_types
+
+
+def test_partition_rtf_from_file_with_exclude_element_types(
+    filename="example-docs/fake-doc.rtf",
+):
+    element_types = [HTMLTitle]
+    with open(filename, "rb") as f:
+        elements = partition_rtf(
+            file=f,
+            include_metadata=False,
+            exclude_element_types=element_types,
+        )
+
+    for element in elements:
+        assert type(element) not in element_types
