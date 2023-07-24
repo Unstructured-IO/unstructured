67574ec6e18b26468a2968d67cea44c66c96eb0a
yuming <305248291@qq.com>
2023-07-24 17:48:40 -0400

    maybe don't rm

--- scripts/test-unstructured-api-unit.sh
+++ scripts/test-unstructured-api-unit.sh
@@ -14,5 +14,5 @@ trap 'handle_error' EXIT
 git clone https://github.com/Unstructured-IO/unstructured-api.git --depth 1
 cd unstructured-api && make install && cd ../
 make install-project-local
+pip show unstructured | grep Version
 cd unstructured-api && make test && cd ../
-rm -rf unstructured-api
