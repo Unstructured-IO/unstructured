196efa09b13e72a4ce3d21b7419895b28abc12a6
Jason Scheirer <jason@unstructured.io>
2023-07-24 10:06:13 -0700

    chore: Add encoding param to ingest (#955)
    * Add encoding param to ingest

---
+++ test_unstructured_ingest/test-ingest-local-single-file-with-encoding.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+cd "$SCRIPT_DIR"/.. || exit 1
+OUTPUT_FOLDER_NAME=local-single-file-with-encoding
+OUTPUT_DIR=$SCRIPT_DIR/structured-output/$OUTPUT_FOLDER_NAME
+
+PYTHONPATH=. ./unstructured/ingest/main.py \
+    --metadata-exclude filename,file_directory,metadata.data_source.date_processed \
+    --local-input-path example-docs/fake-html-cp1252.html \
+    --structured-output-dir "$OUTPUT_DIR" \
+    --encoding cp1252 \
+    --verbose \
+    --reprocess
+
+set +e
+
+sh "$SCRIPT_DIR"/check-diff-expected-output.sh $OUTPUT_FOLDER_NAME
