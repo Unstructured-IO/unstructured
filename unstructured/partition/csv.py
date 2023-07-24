c6022b95dc675b81b0256a6f1fab112d180d2c03
kravetsmic <kravets.mic@gmail.com>
2023-07-24 11:52:03 +0300

    feat: add include_path_in_metadata_filename to csv

--- unstructured/partition/csv.py
+++ unstructured/partition/csv.py
@@ -11,7 +11,10 @@ from unstructured.documents.elements import (
     process_metadata,
 )
 from unstructured.file_utils.filetype import FileType, add_metadata_with_filetype
-from unstructured.partition.common import exactly_one, spooled_to_bytes_io_if_needed
+from unstructured.partition.common import (
+    exactly_one,
+    spooled_to_bytes_io_if_needed,
+)
 
 
 @process_metadata()
@@ -21,6 +24,7 @@ def partition_csv(
     file: Optional[Union[IO[bytes], SpooledTemporaryFile]] = None,
     metadata_filename: Optional[str] = None,
     include_metadata: bool = True,
+    include_path_in_metadata_filename: Optional[bool] = False,
     **kwargs,
 ) -> List[Element]:
     """Partitions Microsoft Excel Documents in .csv format into its document elements.
@@ -33,13 +37,17 @@ def partition_csv(
         A file-like object using "rb" mode --> open(filename, "rb").
     include_metadata
         Determines whether or not metadata is included in the output.
+    include_path_in_metadata_filename
+        Determines whether or not metadata filename will contain full path
     """
     exactly_one(filename=filename, file=file)
 
     if filename:
         table = pd.read_csv(filename)
     else:
-        f = spooled_to_bytes_io_if_needed(cast(Union[BinaryIO, SpooledTemporaryFile], file))
+        f = spooled_to_bytes_io_if_needed(
+            cast(Union[BinaryIO, SpooledTemporaryFile], file),
+        )
         table = pd.read_csv(f)
 
     html_text = table.to_html(index=False, header=False, na_rep="")
@@ -48,7 +56,8 @@ def partition_csv(
     if include_metadata:
         metadata = ElementMetadata(
             text_as_html=html_text,
-            filename=metadata_filename or filename,
+            filename=metadata_filename,
+            include_path_in_metadata_filename=include_path_in_metadata_filename,
         )
     else:
         metadata = ElementMetadata()
