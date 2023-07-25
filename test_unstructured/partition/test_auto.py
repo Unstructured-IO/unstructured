63076ba9e131f579aa55480b03876a6bb638bca7
Matt Robinson <mrobinson@unstructured.io>
2023-07-25 15:59:45 -0400

    refactor: simplifies JSON detection and add tests (#975)
    * refactor json detection
    
    * version and changelog
    
    * fix mock in test

--- test_unstructured/partition/test_auto.py
+++ test_unstructured/partition/test_auto.py
@@ -220,6 +220,19 @@ def test_auto_partition_json_from_filename():
     assert json_data == json_elems
 
 
+def test_auto_partition_json_raises_with_unprocessable_json(tmpdir):
+    # NOTE(robinson) - This is unprocessable because it is not a list of dicts,
+    # per the Unstructured ISD format
+    text = '{"hi": "there"}'
+
+    filename = os.path.join(tmpdir, "unprocessable.json")
+    with open(filename, "w") as f:
+        f.write(text)
+
+    with pytest.raises(ValueError):
+        partition(filename=filename)
+
+
 @pytest.mark.xfail(
     reason="parsed as text not json, https://github.com/Unstructured-IO/unstructured/issues/492",
 )
@@ -525,7 +538,7 @@ def test_auto_partition_odt_from_file():
 @pytest.mark.parametrize(
     ("content_type", "routing_func", "expected"),
     [
-        ("application/json", "json", "application/json"),
+        ("text/csv", "csv", "text/csv"),
         ("text/html", "html", "text/html"),
         ("jdsfjdfsjkds", "pdf", None),
     ],
