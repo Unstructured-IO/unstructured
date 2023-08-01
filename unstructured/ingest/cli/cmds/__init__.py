3a836b600a77ba2762905911d7cb1459d2e46199
Ryan Nikolaidis <1208590+ryannikolaidis@users.noreply.github.com>
2023-08-01 10:50:33 -0700

    completely automate imports

--- unstructured/ingest/cli/cmds/__init__.py
+++ unstructured/ingest/cli/cmds/__init__.py
@@ -1,28 +1,19 @@
-# List of command modules (module names as strings)
-__all__ = [
-    "azure",
-    "biomed",
-    "confluence",
-    "discord",
-    "dropbox",
-    "elasticsearch",
-    "fsspec",
-    "gcs",
-    "github",
-    "gitlab",
-    "google_drive",
-    "local",
-    "onedrive",
-    "outlook",
-    "reddit",
-    "s3",
-    "slack",
-    "wikipedia",
+import os
+import importlib
+
+# List all the files in the current directory
+current_directory = os.path.dirname(__file__)
+all_files = os.listdir(current_directory)
+exclude_files = ["__init__.py", "utils.py"]
+
+# Filter for Python files and exclude __init__.py and other excluded files
+command_modules = [
+    file[:-3] for file in all_files if file.endswith(".py") and file not in exclude_files
 ]
 
-# Function to get the 'get_cmd'
-def get_command_function(command_name):
-    return getattr(__import__(f"{__name__}.{command_name}", fromlist=["get_cmd"]), "get_cmd")
-
-for command in __all__:
-    globals()[command] = get_command_function(command)
+# Dynamically import the modules and create the __all__ list
+__all__ = []
+for module_name in command_modules:
+    module = importlib.import_module("." + module_name, package=__name__)
+    __all__.append(module_name)
+    globals()[module_name] = module.get_cmd
--- unstructured/ingest/runner/__init__.py
+++ unstructured/ingest/runner/__init__.py
@@ -1,23 +1,11 @@
-# List of command modules (module names as strings)
+import os
+
+exlude_files = ["__init__.py", "utils.py"]
+# Get a list of all Python files in the current directory (excluding __init__.py)
 __all__ = [
-    "azure",
-    "biomed",
-    "confluence",
-    "discord",
-    "dropbox",
-    "elasticsearch",
-    "fsspec",
-    "gcs",
-    "github",
-    "gitlab",
-    "google_drive",
-    "local",
-    "onedrive",
-    "outlook",
-    "reddit",
-    "s3",
-    "slack",
-    "wikipedia",
+    filename[:-3]
+    for filename in os.listdir(os.path.dirname(__file__))
+    if filename.endswith(".py") and filename not in exlude_files
 ]
 
 # Loop through the command modules and generate import statements and __all__ list
