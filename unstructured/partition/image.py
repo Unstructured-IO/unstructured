0c8ca60728029bfc1fbd036c6b6ec845b95c1864
Shreya Nidadavolu <shreyanid9@gmail.com>
2023-08-01 11:47:35 -0700

    tidy

--- unstructured/partition/image.py
+++ unstructured/partition/image.py
@@ -18,32 +18,32 @@ def partition_image(
 ) -> List[Element]:
     """Parses an image into a list of interpreted elements.
 
-        Parameters
-        ----------
-        filename
-            A string defining the target filename path.
-        file
-            A file-like object as bytes --> open(filename, "rb").
-        include_page_breaks
-            If True, includes page breaks at the end of each page in the document.
-        infer_table_structure
-            Only applicable if `strategy=hi_res`.
-            If True, any Table elements that are extracted will also have a metadata field
-            named "text_as_html" where the table's text content is rendered into an html string.
-            I.e., rows and cells are preserved.
-            Whether True or False, the "text" field is always present in any Table element
-            and is the text content of the table (no structure).
-        ocr_languages
-            The languages to use for the Tesseract agent. To use a language, you'll first need
-            to install the appropriate Tesseract language pack.
-        strategy
-            The strategy to use for partitioning the image. Valid strategies are "hi_res" and
-            "ocr_only". When using the "hi_res" strategy, the function uses a layout detection
-            model if to identify document elements. When using the "ocr_only" strategy,
-            partition_image simply extracts the text from the document using OCR and processes it.
-            The default strategy is `hi_res`.
-        metadata_last_modified
-            The last modified date for the document.
+    Parameters
+    ----------
+    filename
+        A string defining the target filename path.
+    file
+        A file-like object as bytes --> open(filename, "rb").
+    include_page_breaks
+        If True, includes page breaks at the end of each page in the document.
+    infer_table_structure
+        Only applicable if `strategy=hi_res`.
+        If True, any Table elements that are extracted will also have a metadata field
+        named "text_as_html" where the table's text content is rendered into an html string.
+        I.e., rows and cells are preserved.
+        Whether True or False, the "text" field is always present in any Table element
+        and is the text content of the table (no structure).
+    ocr_languages
+        The languages to use for the Tesseract agent. To use a language, you'll first need
+        to install the appropriate Tesseract language pack.
+    strategy
+        The strategy to use for partitioning the image. Valid strategies are "hi_res" and
+        "ocr_only". When using the "hi_res" strategy, the function uses a layout detection
+        model if to identify document elements. When using the "ocr_only" strategy,
+        partition_image simply extracts the text from the document using OCR and processes it.
+        The default strategy is `hi_res`.
+    metadata_last_modified
+        The last modified date for the document.
 
 
     """
