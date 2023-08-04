ee947875588e0f520b6618d8cef476328a3a651b
Klaijan Sinteppadon <klaijan@unstructured.io>
2023-08-04 16:33:49 -0400

    revert to commit 8fb93b7 and change threshold from 0.5 to 0.1

--- unstructured/cleaners/core.py
+++ unstructured/cleaners/core.py
@@ -163,6 +163,7 @@ def new_line_grouper(
         clean_paragraphs.append(paragraph)
     return "\n\n".join(clean_paragraphs)
 
+
 def blank_line_grouper(
     text: str,
     paragraph_split: re.Pattern = DOUBLE_PARAGRAPH_PATTERN_RE,
@@ -181,13 +182,7 @@ def blank_line_grouper(
     Vestibulum auctor dapibus neque.\n\nNunc dignissim risus id metus.\n\n
 
     """
-    paragraphs = paragraph_split.split(text)
-    clean_paragraphs = []
-    for paragraph in paragraphs:
-        if not paragraph.strip():
-            continue
-        clean_paragraphs.append(re.sub(PARAGRAPH_PATTERN, " ", paragraph))
-    return "\n\n".join(clean_paragraphs)
+    return group_broken_paragraphs(text)
 
 
 def auto_paragraph_grouper(
@@ -216,12 +211,18 @@ def auto_paragraph_grouper(
             empty_line_count += 1
     ratio = empty_line_count / line_count
 
+    print(ratio)
+    print(threshold)
+
     # NOTE(klaijan) - for ratio < threshold, we pass to new-line grouper,
     # otherwise to blank-line grouper
     if ratio < threshold:
+        print("new line grouper")
         return new_line_grouper(text)
     else:
+        print("blank line grouper")
         return blank_line_grouper(text)
+
 
 # TODO(robinson) - There's likely a cleaner was to accomplish this and get all of the
 # unicode characters instead of just the quotes. Doing this for now since quotes are
