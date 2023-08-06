8b8b55601e87d1368eef4b513bb492bdd45d6a71
Ryan Nikolaidis <1208590+ryannikolaidis@users.noreply.github.com>
2023-08-06 15:13:33 -0700

    use -ne

--- test_unstructured_ingest/check-num-files-output.sh
+++ test_unstructured_ingest/check-num-files-output.sh
@@ -14,7 +14,7 @@ SCRIPT_DIR=$(dirname "$(realpath "$0")")
 OUTPUT_DIR=$SCRIPT_DIR/structured-output/$OUTPUT_FOLDER_NAME
 num_files_created="$(find "$OUTPUT_DIR" -type f -exec printf '.' \; | wc -c | xargs)"
 
-if [[ num_files_created != "$EXPECTED_NUM_FILES" ]]; then
+if [[ num_files_created -ne "$EXPECTED_NUM_FILES" ]]; then
    echo
    echo "ERROR: $num_files_created files created. $EXPECTED_NUM_FILES files should have been created."
    exit 1
