b1c53bc30a91d536cf0575a406792a93302f3f19
rvztz <revazqueztuz@gmail.com>
2023-08-07 18:03:27 -0600

    Ignoring meta.last_modified

--- test_unstructured_ingest/test-ingest-sharepoint.sh
+++ test_unstructured_ingest/test-ingest-sharepoint.sh
@@ -12,11 +12,11 @@ if [ -z "$SHAREPOINT_CLIENT_ID" ] || [ -z "$SHAREPOINT_CRED" ]; then
    echo "Skipping Sharepoint ingest test because the MS_CLIENT_ID or MS_CLIENT_CRED env var is not set."
    exit 0
 fi
-
+# excluding metadata.last_modified since this will always update as date processed because the Sharepoint connector creates documents on the fly
 PYTHONPATH=. ./unstructured/ingest/main.py \
     sharepoint \
     --download-dir "$DOWNLOAD_DIR" \
-    --metadata-exclude file_directory,metadata.data_source.date_processed \
+    --metadata-exclude file_directory,metadata.data_source.date_processed,meta.last_modified \
     --num-processes 2 \
     --partition-strategy hi_res \
     --preserve-downloads \
