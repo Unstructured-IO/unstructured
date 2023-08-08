71e5201227ea83d97f86e31f1694624983ba6001
Ryan Nikolaidis <1208590+ryannikolaidis@users.noreply.github.com>
2023-08-07 17:03:54 -0700

    actually set the session_handle

--- unstructured/ingest/doc_processor/generalized.py
+++ unstructured/ingest/doc_processor/generalized.py
@@ -33,6 +33,8 @@ def process_document(doc: "IngestDoc", **partition_kwargs) -> Optional[List[Dict
     """
     isd_elems_no_filename = None
     try:
+        # set the session handler on the doc
+        doc.session_handle = mp.current_process().session_handle
         # does the work necessary to load file into filesystem
         # in the future, get_file_handle() could also be supported
         doc.get_file()
