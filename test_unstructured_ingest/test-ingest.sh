fb575065a788dc3fe6bacacd670f83f758986975
Roman Isecke <roman@unstructured.io>
2023-08-09 14:52:08 -0400

    Add ingest test for Notion

--- test_unstructured_ingest/test-ingest.sh
+++ test_unstructured_ingest/test-ingest.sh
@@ -31,6 +31,7 @@ export OMP_THREAD_LIMIT=1
 ./test_unstructured_ingest/test-ingest-local-single-file.sh
 ./test_unstructured_ingest/test-ingest-local-single-file-with-encoding.sh
 ./test_unstructured_ingest/test-ingest-local-single-file-with-pdf-infer-table-structure.sh
+./test_unstructured_ingest/test-ingest-notion.sh
 # NOTE(yuming): The following test should be put after any tests with --preserve-downloads option
 ./test_unstructured_ingest/test-ingest-pdf-fast-reprocess.sh
 ./test_unstructured_ingest/test-ingest-sharepoint.sh
