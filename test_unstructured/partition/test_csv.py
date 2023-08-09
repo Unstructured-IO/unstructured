0d22777b832321596d5dc2e441a2c3aaa61f411d
christinestraub <christinemstraub@gmail.com>
2023-08-09 13:48:55 -0700

    feat: add functionality to switch `html` text parser based on whether the `html` text contains emoji

--- test_unstructured/partition/test_csv.py
+++ test_unstructured/partition/test_csv.py
@@ -1,6 +1,13 @@
 from tempfile import SpooledTemporaryFile
 
-from test_unstructured.partition.test_constants import EXPECTED_TABLE, EXPECTED_TEXT
+import pytest
+
+from test_unstructured.partition.test_constants import (
+    EXPECTED_TABLE,
+    EXPECTED_TABLE_WITH_EMOJI,
+    EXPECTED_TEXT,
+    EXPECTED_TEXT_WITH_EMOJI,
+)
 from unstructured.cleaners.core import clean_extra_whitespace
 from unstructured.documents.elements import Table
 from unstructured.partition.csv import partition_csv
@@ -8,13 +15,21 @@ from unstructured.partition.csv import partition_csv
 EXPECTED_FILETYPE = "text/csv"
 
 
-def test_partition_csv_from_filename(filename="example-docs/stanley-cups.csv"):
-    elements = partition_csv(filename=filename)
+@pytest.mark.parametrize(
+    ("filename", "expected_text", "expected_table"),
+    [
+        ("stanley-cups.csv", EXPECTED_TEXT, EXPECTED_TABLE),
+        ("stanley-cups-with-emoji.csv", EXPECTED_TEXT_WITH_EMOJI, EXPECTED_TABLE_WITH_EMOJI),
+    ],
+)
+def test_partition_csv_from_filename(filename, expected_text, expected_table):
+    f_path = f"example-docs/{filename}"
+    elements = partition_csv(filename=f_path)
 
-    assert clean_extra_whitespace(elements[0].text) == EXPECTED_TEXT
-    assert elements[0].metadata.text_as_html == EXPECTED_TABLE
+    assert clean_extra_whitespace(elements[0].text) == expected_text
+    assert elements[0].metadata.text_as_html == expected_table
     assert elements[0].metadata.filetype == EXPECTED_FILETYPE
-    assert elements[0].metadata.filename == "stanley-cups.csv"
+    assert elements[0].metadata.filename == filename
 
 
 def test_partition_csv_from_filename_with_metadata_filename(
@@ -26,13 +41,21 @@ def test_partition_csv_from_filename_with_metadata_filename(
     assert elements[0].metadata.filename == "test"
 
 
-def test_partition_csv_from_file(filename="example-docs/stanley-cups.csv"):
-    with open(filename, "rb") as f:
+@pytest.mark.parametrize(
+    ("filename", "expected_text", "expected_table"),
+    [
+        ("stanley-cups.csv", EXPECTED_TEXT, EXPECTED_TABLE),
+        ("stanley-cups-with-emoji.csv", EXPECTED_TEXT_WITH_EMOJI, EXPECTED_TABLE_WITH_EMOJI),
+    ],
+)
+def test_partition_csv_from_file(filename, expected_text, expected_table):
+    f_path = f"example-docs/{filename}"
+    with open(f_path, "rb") as f:
         elements = partition_csv(file=f)
 
-    assert clean_extra_whitespace(elements[0].text) == EXPECTED_TEXT
+    assert clean_extra_whitespace(elements[0].text) == expected_text
     assert isinstance(elements[0], Table)
-    assert elements[0].metadata.text_as_html == EXPECTED_TABLE
+    assert elements[0].metadata.text_as_html == expected_table
     assert elements[0].metadata.filetype == EXPECTED_FILETYPE
     assert elements[0].metadata.filename is None
 
