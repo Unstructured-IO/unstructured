3d88c281bd8be14335502bcbe975398bcc471d86
Newel H <37004249+newelh@users.noreply.github.com>
2023-08-18 12:50:51 -0400

    fix: check docx and docx.table imports

--- unstructured/partition/common.py
+++ unstructured/partition/common.py
@@ -25,7 +25,7 @@ from unstructured.logger import logger
 from unstructured.nlp.patterns import ENUMERATED_BULLETS_RE, UNICODE_BULLETS_RE
 from unstructured.utils import dependency_exists
 
-if dependency_exists("docx.table"):
+if dependency_exists("docx") and dependency_exists("docx.table"):
     from docx.table import Table as docxtable
 
 if TYPE_CHECKING:
