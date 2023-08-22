2e86c394790d7be92a4a162c1db0eb780fde383d
Roman Isecke <roman@unstructured.io>
2023-08-22 11:16:15 -0400

    Fix handlers on logger

--- unstructured/ingest/logger.py
+++ unstructured/ingest/logger.py
@@ -10,5 +10,8 @@ def make_default_logger(level: int = logging.NOTSET) -> logging.Logger:
     handler.name = "ingest_log_handler"
     formatter = logging.Formatter("%(asctime)s %(processName)-10s %(levelname)-8s %(message)s")
     handler.setFormatter(formatter)
+    # Only want to add the handler once
+    if "ingest_log_handler" not in [h.name for h in logger.handlers]:
+        logger.addHandler(handler)
     logger.setLevel(level)
     return logger
