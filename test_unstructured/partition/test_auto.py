c578b85699d379791464cebc7e00f225f8d94853
Matt Robinson <mrobinson@unstructured.io>
2023-08-25 00:14:48 -0400

    fix: respect `<pre>` tag order in `partition_html` (#1197)
    ### Summary
    
    Closes #1184. Updates `partition_html` to respect the ordering of
    `<pre>` tags in HTML documents.
    
    ### Testing
    
    The elements in the following example should be in the correct order.
    
    ```python
        from unstructured.partition.html import partition_html
    
        html_text = """
        <pre>The Big Brown Bear</pre>
        <div>The big brown bear is growling.</div>
        <pre>The big brown bear is sleeping.</pre>
        <div>The Big Blue Bear</div>
        """
        elements = partition_html(text=html_text)
        print("\n\n".join([str(el) for el in elements]))
    ```

--- test_unstructured/partition/test_auto.py
+++ test_unstructured/partition/test_auto.py
@@ -830,8 +830,8 @@ def test_auto_partition_html_pre_from_file(filename="example-docs/fake-html-pre.
 
     assert len(elements) > 0
     assert "PageBreak" not in [elem.category for elem in elements]
-    assert clean_extra_whitespace(elements[0].text) == "[107th Congress Public Law 56]"
-    assert isinstance(elements[0], Title)
+    assert clean_extra_whitespace(elements[0].text).startswith("[107th Congress Public Law 56]")
+    assert isinstance(elements[0], NarrativeText)
     assert elements[0].metadata.filetype == "text/html"
     assert elements[0].metadata.filename == "fake-html-pre.htm"
 
