da31901647bc8ebad1f9c299de7032671756e9bf
Coniferish <johnjennings702@gmail.com>
2023-08-30 15:05:49 -0500

    WIP: add test for partition_image

--- test_unstructured/partition/pdf-image/test_image.py
+++ test_unstructured/partition/pdf-image/test_image.py
@@ -393,15 +393,22 @@ def test_partition_msg_with_json(
         assert elements[i] == test_elements[i]
 
 
-def test_partition_image_with_ocr_has_coordinates_from_file(
-    mocker,
+def test_partition_image_with_ocr_has_coordinates_from_filename(
     filename="example-docs/english-and-korean.png",
 ):
-    mocked_last_modification_date = "2029-07-05T09:24:28"
-    mocker.patch(
-        "unstructured.partition.pdf.get_last_modified_date",
-        return_value=mocked_last_modification_date,
-    )
     elements = image.partition_image(filename=filename, strategy="ocr_only")
     int_coordinates = [(int(x), int(y)) for x, y in elements[0].metadata.coordinates.points]
     assert int_coordinates == [(14, 36), (14, 16), (381, 16), (381, 36)]
+
+
+def test_partition_image_with_ocr_has_valid_coordinates_from_filename(
+    filename="example-docs/layout-parser-paper-with-table.jpg",
+):
+    import math
+
+    elements = image.partition_image(filename=filename, strategy="ocr_only")
+    for element in elements:
+        box = element.metadata.coordinates.points
+        for point in box:
+            assert point[0] is not math.nan
+            assert point[1] is not math.nan
