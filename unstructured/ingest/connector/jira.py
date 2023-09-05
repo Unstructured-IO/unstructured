0d50ec28d8eca0a6c071d82998015251a18c804b
Ahmet Melek <ahmetmeleq@gmail.com>
2023-09-06 00:55:05 +0300

    ditch cached property for calculation within exists

--- unstructured/ingest/connector/jira.py
+++ unstructured/ingest/connector/jira.py
@@ -278,7 +278,7 @@ class JiraIngestDoc(IngestDocSessionHandleMixin, IngestDocCleanupMixin, BaseInge
     @requires_dependencies(dependencies=["atlassian"], extras="jira")
     def metadata_fields(self):
         return {
-            "exists": bool(self.issue),
+            "exists": bool(self.session_handle.service.issue(self.file_meta.issue_key)),
             "date_modified": str(self.parsed_fields["updated"]),
             "date_created": str(self.parsed_fields["created"]),
             "date_processed": str(datetime.datetime.now().time()),
