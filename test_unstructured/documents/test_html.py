5f0344d9c6f979a6382d484d0d89a9a1fa51e2e1
acameron103 <amanda@unstructured.io>
2023-09-07 17:28:39 -0700

    tidying

--- test_unstructured/documents/test_html.py
+++ test_unstructured/documents/test_html.py
@@ -115,8 +115,8 @@ def test_read_without_skipping_table(monkeypatch):
     document = HTMLDocument.from_string(doc).doc_after_cleaners(skip_table=False)
     assert document.pages[0].elements[0] == Table(
         text="<table>\n<tbody>\n<tr><td>"
-             "</td><td>Hi there! I am Matt!</td>"
-             "<td></td></tr>\n</tbody>\n</table>",
+        "</td><td>Hi there! I am Matt!</td>"
+        "<td></td></tr>\n</tbody>\n</table>",
     )
 
 
