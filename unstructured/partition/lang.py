2f535c2780601fd02c243fba36919ebe2806fdf2
Shreya Nidadavolu <shreyanid9@gmail.com>
2023-09-11 09:50:59 -0400

    docstrings

--- unstructured/partition/lang.py
+++ unstructured/partition/lang.py
@@ -1,14 +1,19 @@
 from typing import List
 
 
-# convert languages (list of strings) into tesseract ocr langcode format (with +)
 def prepare_languages_for_tesseract(languages: List[str] = ["eng"]):
-    # NOTE(Shreya): assumes language codes are in tesseract format (will be updated later)
+    """
+    Convert the languages param (list of strings) into tesseract ocr langcode format (uses +) string
+    """
+    # NOTE(Shreya): assumes language codes are already in tesseract format (will be updated later)
 
     return "+".join(languages)
 
 
 def convert_old_ocr_languages_to_languages(ocr_languages: str):
-    # assumption: ocr_languages is in tesseract plus sign format
+    """
+    Convert ocr_languages parameter to list of langcode strings.
+    Assumption: ocr_languages is in tesseract plus sign format
+    """
 
     return ocr_languages.split("+")
