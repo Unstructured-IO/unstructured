8243ffa9cad3598054d988d31280f612b99ef746
Roman Isecke <roman@unstructured.io>
2023-09-11 08:51:52 -0400

    cleanup tmp directories for text content

--- test_unstructured_ingest/check-diff-expected-output.sh
+++ test_unstructured_ingest/check-diff-expected-output.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Description: Compare the structured output files to the expected output files and exit with an error
-#              if they are different. If the environment variable OVERWRITE_FIXTURES is not "false", 
+#              if they are different. If the environment variable OVERWRITE_FIXTURES is not "false",
 #              then this script will instead copy the output files to the expected output directory.
 #
 # Arguments:
@@ -14,37 +14,59 @@ set +e
 
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 OVERWRITE_FIXTURES=${OVERWRITE_FIXTURES:-false}
+TMP_DIRECTORY_CLEANUP=${TMP_DIRECTORY_CLEANUP:-true}
 OUTPUT_FOLDER_NAME=$1
 OUTPUT_DIR=$SCRIPT_DIR/structured-output/$OUTPUT_FOLDER_NAME
 OUTPUT_DIR_TEXT=$SCRIPT_DIR/text-output/$OUTPUT_FOLDER_NAME
 EXPECTED_OUTPUT_DIR=$SCRIPT_DIR/expected-structured-output/$OUTPUT_FOLDER_NAME
 EXPECTED_OUTPUT_DIR_TEXT=$SCRIPT_DIR/expected-text-output/$OUTPUT_FOLDER_NAME
 
-# to update ingest test fixtures, run scripts/ingest-test-fixtures-update.sh on x86_64
-if [ "$OVERWRITE_FIXTURES" != "false" ]; then
-    # remove folder if it exists
-    if [ -d "$EXPECTED_OUTPUT_DIR" ]; then
-        rm -rf "$EXPECTED_OUTPUT_DIR"
+function cleanup() {
+  if [ "$TMP_DIRECTORY_CLEANUP" == "true" ]; then
+    if [ -d "$EXPECTED_OUTPUT_DIR_TEXT" ]; then
+      echo "cleaning up tmp directory: $EXPECTED_OUTPUT_DIR_TEXT"
+      rm -rf "$EXPECTED_OUTPUT_DIR_TEXT"
     fi
-    mkdir -p "$EXPECTED_OUTPUT_DIR"
-    cp -rf "$OUTPUT_DIR" "$SCRIPT_DIR/expected-structured-output"
-elif ! diff -ru "$EXPECTED_OUTPUT_DIR" "$OUTPUT_DIR" ; then
-    "$SCRIPT_DIR"/json-to-clean-text-folder.sh "$EXPECTED_OUTPUT_DIR" "$EXPECTED_OUTPUT_DIR_TEXT"
-    "$SCRIPT_DIR"/json-to-clean-text-folder.sh "$OUTPUT_DIR" "$OUTPUT_DIR_TEXT"
-    diff -ru "$EXPECTED_OUTPUT_DIR_TEXT" "$OUTPUT_DIR_TEXT"> outputdiff.txt
-    cat outputdiff.txt
-    diffstat -c outputdiff.txt
-    echo
-    echo "There are differences from the previously checked-in structured outputs."
-    echo
-    echo "If these differences are acceptable, overwrite by the fixtures by setting the env var:"
-    echo
-    echo "  export OVERWRITE_FIXTURES=true"
-    echo
-    echo "and then rerun this script."
-    echo
-    echo "NOTE: You'll likely just want to run scripts/ingest-test-fixtures-update.sh on x86_64 hardware"
-    echo "to update fixtures for CI."
-    echo
-    exit 1
-fi
+    if [ -d "$OUTPUT_DIR_TEXT" ]; then
+      echo "cleaning up tmp directory: $OUTPUT_DIR_TEXT"
+      rm -rf "$OUTPUT_DIR_TEXT"
+    fi
+  else
+    echo "skipping tmp directory cleanup"
+  fi
+
+}
+
+trap cleanup EXIT
+
+"$SCRIPT_DIR"/json-to-clean-text-folder.sh "$EXPECTED_OUTPUT_DIR" "$EXPECTED_OUTPUT_DIR_TEXT"
+"$SCRIPT_DIR"/json-to-clean-text-folder.sh "$OUTPUT_DIR" "$OUTPUT_DIR_TEXT"
+diff -ru "$EXPECTED_OUTPUT_DIR_TEXT" "$OUTPUT_DIR_TEXT"> outputdiff.txt
+# to update ingest test fixtures, run scripts/ingest-test-fixtures-update.sh on x86_64
+#if [ "$OVERWRITE_FIXTURES" != "false" ]; then
+#    # remove folder if it exists
+#    if [ -d "$EXPECTED_OUTPUT_DIR" ]; then
+#        rm -rf "$EXPECTED_OUTPUT_DIR"
+#    fi
+#    mkdir -p "$EXPECTED_OUTPUT_DIR"
+#    cp -rf "$OUTPUT_DIR" "$SCRIPT_DIR/expected-structured-output"
+#elif ! diff -ru "$EXPECTED_OUTPUT_DIR" "$OUTPUT_DIR" ; then
+#    "$SCRIPT_DIR"/json-to-clean-text-folder.sh "$EXPECTED_OUTPUT_DIR" "$EXPECTED_OUTPUT_DIR_TEXT"
+#    "$SCRIPT_DIR"/json-to-clean-text-folder.sh "$OUTPUT_DIR" "$OUTPUT_DIR_TEXT"
+#    diff -ru "$EXPECTED_OUTPUT_DIR_TEXT" "$OUTPUT_DIR_TEXT"> outputdiff.txt
+#    cat outputdiff.txt
+#    diffstat -c outputdiff.txt
+#    echo
+#    echo "There are differences from the previously checked-in structured outputs."
+#    echo
+#    echo "If these differences are acceptable, overwrite by the fixtures by setting the env var:"
+#    echo
+#    echo "  export OVERWRITE_FIXTURES=true"
+#    echo
+#    echo "and then rerun this script."
+#    echo
+#    echo "NOTE: You'll likely just want to run scripts/ingest-test-fixtures-update.sh on x86_64 hardware"
+#    echo "to update fixtures for CI."
+#    echo
+#    exit 1
+#fi
