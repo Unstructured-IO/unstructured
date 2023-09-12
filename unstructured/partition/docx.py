a27fb64a4ccaf6f8c37d7dc50baa57be8c2457f7
Steve Canny <stcanny@gmail.com>
2023-09-11 23:32:34 -0700

    rfctr: extract ._iter_paragraph_elements()
    This completes the recomposition of paragraph-partitioning. Remove now
    unused `_paragraph_to_element()` and `_text_to_element()`

--- unstructured/partition/docx.py
+++ unstructured/partition/docx.py
@@ -129,43 +129,6 @@ def convert_and_partition_docx(
     return elements
 
 
-# NOTE(robinson) - documentation on built in styles can be found at the link below
-# ref: https://python-docx.readthedocs.io/en/latest/user/
-#   styles-understanding.html#paragraph-styles-in-default-template
-STYLE_TO_ELEMENT_MAPPING = {
-    "Caption": Text,  # TODO(robinson) - add caption element type
-    "Heading 1": Title,
-    "Heading 2": Title,
-    "Heading 3": Title,
-    "Heading 4": Title,
-    "Heading 5": Title,
-    "Heading 6": Title,
-    "Heading 7": Title,
-    "Heading 8": Title,
-    "Heading 9": Title,
-    "Intense Quote": Text,  # TODO(robinson) - add quote element type
-    "List": ListItem,
-    "List 2": ListItem,
-    "List 3": ListItem,
-    "List Bullet": ListItem,
-    "List Bullet 2": ListItem,
-    "List Bullet 3": ListItem,
-    "List Continue": ListItem,
-    "List Continue 2": ListItem,
-    "List Continue 3": ListItem,
-    "List Number": ListItem,
-    "List Number 2": ListItem,
-    "List Number 3": ListItem,
-    "List Paragraph": ListItem,
-    "Macro Text": Text,
-    "No Spacing": Text,
-    "Quote": Text,  # TODO(robinson) - add quote element type
-    "Subtitle": Title,
-    "TOCHeading": Title,
-    "Title": Title,
-}
-
-
 @process_metadata()
 @add_metadata_with_filetype(FileType.DOCX)
 @add_chunking_strategy()
@@ -294,15 +257,7 @@ class _DocxPartitioner:
                 table_index += 1
             elif element_item.tag.endswith("p"):
                 paragraph = Paragraph(element_item, self._document)
-                (emphasized_text_contents, emphasized_text_tags) = self._paragraph_emphasis(
-                    paragraph
-                )
-                para_element: Optional[Text] = _paragraph_to_element(
-                    paragraph, self._is_list_item(paragraph)
-                )
-                if para_element is not None:
-                    para_element.metadata = self._paragraph_metadata(paragraph)
-                    yield para_element
+                yield from self._iter_paragraph_elements(paragraph)
             elif element_item.tag.endswith("sectPr"):
                 if len(headers_and_footers) > section:
                     footers = headers_and_footers[section][1]
@@ -365,6 +320,44 @@ class _DocxPartitioner:
             return True
 
         return "<w:numPr>" in paragraph._p.xml
+
+    def _iter_paragraph_elements(self, paragraph: Paragraph) -> Iterator[Element]:
+        """Generate zero-or-one document element for `paragraph`.
+
+        In Word, an empty paragraph is commonly used for inter-paragraph spacing. An empty paragraph
+        does not contribute to the document-element stream and will not cause an element to be
+        emitted.
+        """
+        text = paragraph.text
+
+        # -- blank paragraphs are commonly used for spacing between paragraphs and
+        # -- do not contribute to the document-element stream.
+        if not text.strip():
+            return
+
+        metadata = self._paragraph_metadata(paragraph)
+
+        # -- a list gets some special treatment --
+        if self._is_list_item(paragraph):
+            clean_text = clean_bullets(text).strip()
+            if clean_text:
+                yield ListItem(text=clean_text, metadata=metadata)
+            return
+
+        # -- determine element-type from an explicit Word paragraph-style if possible --
+        TextSubCls = self._style_based_element_type(paragraph)
+        if TextSubCls:
+            yield TextSubCls(text=text, metadata=metadata)
+            return
+
+        # -- try to recognize the element type by parsing its text --
+        TextSubCls = self._parse_paragraph_text_for_element_type(paragraph)
+        if TextSubCls:
+            yield TextSubCls(text=text, metadata=metadata)
+            return
+
+        # -- if all that fails we give it the default `Text` element-type --
+        yield Text(text, metadata=metadata)
 
     def _iter_paragraph_emphasis(self, paragraph: Paragraph) -> Iterator[Dict[str, str]]:
         """Generate e.g. {"text": "MUST", "tag": "b"} for each emphasis in `paragraph`."""
@@ -489,53 +482,6 @@ class _DocxPartitioner:
         # NOTE(robinson) - The "Normal" style name will return None since it's not
         # in the mapping. Unknown style names will also return None.
         return STYLE_TO_ELEMENT_MAPPING.get(style_name)
-
-
-def _paragraph_to_element(
-    paragraph: Paragraph,
-    is_list: bool = False,
-) -> Optional[Text]:
-    """Converts a docx Paragraph object into the appropriate unstructured document element.
-    If the paragraph style is "Normal" or unknown, we try to predict the element type from the
-    raw text."""
-    text = paragraph.text
-    # -- paragraph.style can be None in rare cases, so can style.name. That's going to mean default
-    # -- style which is equivalent to "Normal" for our purposes.
-    style_name = (paragraph.style and paragraph.style.name) or "Normal"
-
-    if len(text.strip()) == 0:
-        return None
-
-    element_class = STYLE_TO_ELEMENT_MAPPING.get(style_name)
-
-    # NOTE(robinson) - The "Normal" style name will return None since it's in the mapping.
-    # Unknown style names will also return None
-    if is_list:
-        return _text_to_element(text, is_list)
-    elif element_class is None:
-        return _text_to_element(text)
-    else:
-        return element_class(text)
-
-
-def _text_to_element(text: str, is_list: bool = False) -> Optional[Text]:
-    """Converts raw text into an unstructured Text element."""
-    if is_bulleted_text(text) or is_list:
-        clean_text = clean_bullets(text).strip()
-        return ListItem(text=clean_bullets(text)) if clean_text else None
-
-    elif is_us_city_state_zip(text):
-        return Address(text=text)
-    elif is_email_address(text):
-        return EmailAddress(text=text)
-    if len(text) < 2:
-        return None
-    elif is_possible_narrative_text(text):
-        return NarrativeText(text)
-    elif is_possible_title(text):
-        return Title(text)
-    else:
-        return Text(text)
 
 
 def _join_paragraphs(paragraphs: List[Paragraph]) -> Optional[str]:
