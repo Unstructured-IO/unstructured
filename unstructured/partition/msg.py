9b1494dfa769b9ab8b8ca74d192bb2d4bdfca786
acameron103 <amanda@unstructured.io>
2023-09-13 13:52:24 -0700

    fixing msg

--- unstructured/partition/msg.py
+++ unstructured/partition/msg.py
@@ -79,7 +79,7 @@ def partition_msg(
     elif text is None:
         pass
     elif "<html>" in text or "</div>" in text:
-        elements = partition_html(text=text, source_format="msg")
+        elements = partition_html(text=text)
     else:
         elements = partition_text(
             text=text,
