df4c0bd933e0e7e8cf058761d2d896e6ef979bf9
Steve Canny <stcanny@gmail.com>
2023-09-15 18:00:34 -0700

    rfctr: replace partition_pptx implementation ...
    ... with a call to the main classmethod of _PptxPartitioner.

--- unstructured/partition/pptx.py
+++ unstructured/partition/pptx.py
@@ -70,84 +70,19 @@ def partition_pptx(
         metadata.
     metadata_last_modified
         The last modified date for the document.
-
-
     include_slide_notes
         If True, includes the slide notes as element
     """
-
-    # Verify that only one of the arguments was provided
-    exactly_one(filename=filename, file=file)
-    last_modification_date = None
-    if filename is not None:
-        if not filename.startswith("/tmp"):
-            last_modification_date = get_last_modified_date(filename)
-
-        presentation = pptx.Presentation(filename)
-    else:
-        assert file is not None
-        last_modification_date = get_last_modified_date_from_file(file)
-        if isinstance(file, SpooledTemporaryFile):
-            file.seek(0)
-            file = io.BytesIO(file.read())
-        presentation = pptx.Presentation(file)
-
-    elements: List[Element] = []
-    metadata = ElementMetadata(filename=metadata_filename or filename)
-    num_slides = len(presentation.slides)
-    for i, slide in enumerate(presentation.slides):
-        metadata = ElementMetadata.from_dict(metadata.to_dict())
-        metadata.last_modified = metadata_last_modified or last_modification_date
-        metadata.page_number = i + 1
-        if include_slide_notes and slide.has_notes_slide is True:
-            notes_slide = slide.notes_slide
-            if notes_slide.notes_text_frame is not None:
-                notes_text_frame = notes_slide.notes_text_frame
-                notes_text = notes_text_frame.text
-                if notes_text.strip() != "":
-                    elements.append(NarrativeText(text=notes_text, metadata=metadata))
-
-        for shape in _order_shapes(slide.shapes):
-            if shape.has_table:
-                assert isinstance(shape, GraphicFrame)
-                table = shape.table
-                html_table = convert_ms_office_table_to_text(table, as_html=True)
-                text_table = convert_ms_office_table_to_text(table, as_html=False).strip()
-                if text_table:
-                    metadata = ElementMetadata(
-                        filename=metadata_filename or filename,
-                        text_as_html=html_table,
-                        page_number=metadata.page_number,
-                        last_modified=metadata_last_modified or last_modification_date,
-                    )
-                    elements.append(Table(text=text_table, metadata=metadata))
-                continue
-            if not shape.has_text_frame:
-                continue
-            assert isinstance(shape, Shape)
-            # NOTE(robinson) - avoid processing shapes that are not on the actual slide
-            # NOTE - skip check if no top or left position (shape displayed top left)
-            if (shape.top and shape.left) and (shape.top < 0 or shape.left < 0):
-                continue
-            for paragraph in shape.text_frame.paragraphs:
-                text = paragraph.text
-                if text.strip() == "":
-                    continue
-                if _is_bulleted_paragraph(paragraph):
-                    elements.append(ListItem(text=text, metadata=metadata))
-                elif is_email_address(text):
-                    elements.append(EmailAddress(text=text))
-                elif is_possible_narrative_text(text):
-                    elements.append(NarrativeText(text=text, metadata=metadata))
-                elif is_possible_title(text):
-                    elements.append(Title(text=text, metadata=metadata))
-                else:
-                    elements.append(Text(text=text, metadata=metadata))
-
-        if include_page_breaks and i < num_slides - 1:
-            elements.append(PageBreak(text=""))
-
-    return elements
+    return list(
+        _PptxPartitioner.iter_presentation_elements(
+            file,
+            filename,
+            include_page_breaks,
+            include_slide_notes,
+            metadata_filename,
+            metadata_last_modified,
+        )
+    )
 
 
 class _PptxPartitioner:  # pyright: ignore[reportUnusedClass]
