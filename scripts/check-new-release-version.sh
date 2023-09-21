60ed953489512fbea4f7685da49e5b0d3fc529de
Ryan Nikolaidis <1208590+ryannikolaidis@users.noreply.github.com>
2023-09-20 19:17:30 -0700

    fix version check

--- scripts/check-new-release-version.sh
+++ scripts/check-new-release-version.sh
@@ -12,7 +12,7 @@ function is_non_dev_version {
 function get_main_branch_version {
     local VERSION
     git fetch origin main
-    VERSION=$(git show origin/main:unstructured/__version__.py | grep -o -m 1 -E "(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)")
+    VERSION=$(git show origin/main:unstructured/__version__.py | grep -o -m 1 -E "(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-[a-zA-Z0-9.-]+)?")
     echo "$VERSION"
 }
 
