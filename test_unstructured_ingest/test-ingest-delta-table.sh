582ccff2ee013b9eabdffaf7b7aa8877eeb15314
Roman Isecke <roman@unstructured.io>
2023-09-20 22:04:46 -0400

    fix delta table ingest test

--- test_unstructured_ingest/test-ingest-delta-table.sh
+++ test_unstructured_ingest/test-ingest-delta-table.sh
@@ -9,8 +9,6 @@ OUTPUT_DIR=$SCRIPT_DIR/structured-output/$OUTPUT_FOLDER_NAME
 DOWNLOAD_DIR=$SCRIPT_DIR/download/$OUTPUT_FOLDER_NAME
 DESTINATION_TABLE=$SCRIPT_DIR/delta-table-dest
 
-mkdir -p "$DESTINATION_TABLE"
-
 if [ -z "$AWS_ACCESS_KEY_ID" ] && [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
    echo "Skipping Delta Table ingest test because either AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY env var was not set."
    exit 0
@@ -28,7 +26,7 @@ trap cleanup EXIT
 
 PYTHONPATH=. ./unstructured/ingest/main.py \
     delta-table \
-    --metadata-exclude coordinates,filename,file_directory,metadata.data_source.date_processed,metadata.last_modified,metadata.detection_class_prob,metadata.parent_id,metadata.category_depth \
+    --metadata-exclude coordinates,filename,file_directory,metadata.data_source.date_processed,metadata.last_modified,metadata.date_created,metadata.detection_class_prob,metadata.parent_id,metadata.category_depth \
     --download-dir "$DOWNLOAD_DIR" \
     --table-uri s3://utic-dev-tech-fixtures/sample-delta-lake-data/deltatable/ \
     --output-dir "$OUTPUT_DIR" \
@@ -41,4 +39,4 @@ PYTHONPATH=. ./unstructured/ingest/main.py \
 
 "$SCRIPT_DIR"/check-diff-expected-output.sh $OUTPUT_FOLDER_NAME
 
-python "$SCRIPT_DIR"/python/test-ingest-delta-table-output.py
+python "$SCRIPT_DIR"/python/test-ingest-delta-table-output.py --table-uri "$DESTINATION_TABLE"
