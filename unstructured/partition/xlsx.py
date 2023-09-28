a948837c3e38819cfbc22c230d6ae8dad4b4f8a0
Klaijan Sinteppadon <klaijan@unstructured.io>
2023-09-28 17:58:50 -0400

    find subtables

--- unstructured/partition/xlsx.py
+++ unstructured/partition/xlsx.py
@@ -48,19 +48,24 @@ def partition_xlsx(
     exactly_one(filename=filename, file=file)
     last_modification_date = None
     if filename:
-        sheets = pd.read_excel(filename, sheet_name=None)
+        sheets = pd.read_excel(filename, sheet_name=None, header=None)
         last_modification_date = get_last_modified_date(filename)
 
     elif file:
         f = spooled_to_bytes_io_if_needed(
             cast(Union[BinaryIO, SpooledTemporaryFile], file),
         )
-        sheets = pd.read_excel(f, sheet_name=None)
+        sheets = pd.read_excel(f, sheet_name=None, header=None)
         last_modification_date = get_last_modified_date_from_file(file)
+
+    # connected_components: List[dict] = []
+    # for sheet in sheets:
+    #     connected_components.append(_find_connected_components(sheet))
 
     elements: List[Element] = []
     page_number = 0
     for sheet_name, table in sheets.items():
+        breakpoint()
         page_number += 1
         html_text = table.to_html(index=False, header=include_header, na_rep="")
         text = soupparser_fromstring(html_text).text_content()
@@ -80,3 +85,46 @@ def partition_xlsx(
         elements.append(table)
 
     return elements
+
+
+def _find_connected_components(sheet_name, sheet):
+    max_row, max_col = sheet.shape
+    visited = set()
+    connected_components = []
+
+    def dfs(row, col, component):
+        if (
+            row < 0
+            or row >= sheet.shape[0]
+            or col < 0
+            or col >= sheet.shape[1]
+            or (row, col) in visited
+        ):
+            return
+        visited.add((row, col))
+
+        if not pd.isna(sheet.iat[row, col]):
+            component.append((row, col))
+
+            # Explore neighboring cells
+            dfs(row - 1, col, component)  # Above
+            dfs(row + 1, col, component)  # Below
+            dfs(row, col - 1, component)  # Left
+            dfs(row, col + 1, component)  # Right
+
+    for row in range(max_row):
+        for col in range(max_col):
+            if (row, col) not in visited:
+                if not pd.isna(sheet.iat[row, col]):
+                    component = []
+                    dfs(row, col, component)
+                    connected_components.append(component)
+                    
+    min_x, min_y, max_x, max_y = _find_min_max_coords(connected_components)
+    return connected_components, (min_x, min_y, max_x, max_y)
+
+def _find_min_max_coords(connected_components):
+    pass
+
+def _make_rectangular():
+    pass
