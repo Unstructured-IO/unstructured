96d2cce84283540ad405dedffeca7b15170bdbc0
Trevor Bossert <alanboss@gmail.com>
2023-09-28 13:04:49 -0700

    Move Scarf to setup.py, this allows using pip interactive mode with -e
    This also follows what I have seen as the recommend way to define a file package like this.
    
    Also bumps minor versions from pip compile

--- setup.py
+++ setup.py
@@ -106,7 +106,12 @@ setup(
     entry_points={
         "console_scripts": ["unstructured-ingest=unstructured.ingest.main:main"],
     },
-    install_requires=load_requirements(),
+    install_requires=[
+        # (Trevor): This is a simple hello world package that is used to track
+        # download count for this package using scarf.
+        'repo @ https://packages.unstructured.io/scarf.tgz',
+        load_requirements()
+    ],
     extras_require={
         # Document specific extra requirements
         "all-docs": all_doc_reqs,
