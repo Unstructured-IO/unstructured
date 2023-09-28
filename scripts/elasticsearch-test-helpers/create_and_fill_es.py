a99eebb597b512d1fd3cfd3b84cd10b178bf3f5e
Yao You <yao@unstructured.io>
2023-09-28 15:29:21 -0500

    wait on all green

--- scripts/elasticsearch-test-helpers/create_and_fill_es.py
+++ scripts/elasticsearch-test-helpers/create_and_fill_es.py
@@ -15,7 +15,7 @@ print(es.info())
 df = pd.read_csv(DATA_PATH).dropna().reset_index()
 
 print("Creating an Elasticsearch index for testing elasticsearch ingest.")
-es.indices.create(index=INDEX_NAME, mappings=MAPPINGS, timeout="60")
+es.indices.create(index=INDEX_NAME, mappings=MAPPINGS)
 
 print("Loading data into the index.")
 bulk_data = []
