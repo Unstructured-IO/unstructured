786ab886e8db9c45962f199b4b62dc89f5b1e801
Benjamin Torres <benjamin@unstructured.io>
2023-09-27 19:43:40 -0600

    fix: corrected image source

--- unstructured/partition/pdf.py
+++ unstructured/partition/pdf.py
@@ -371,7 +371,7 @@ def _partition_pdf_or_image_local(
         # block with NLP rules. Otherwise, the assumptions in
         # unstructured.partition.common::layout_list_to_list_items often result in weird chunking.
         infer_list_items=False,
-        data_origin="pdf",
+        data_origin="image" if is_image else "pdf",
         **kwargs,
     )
 
