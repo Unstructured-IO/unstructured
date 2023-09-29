b3d0e681cb2ece678e9c95a8705bf0b8d2135e77
Benjamin Torres <benjamin@unstructured.io>
2023-09-29 10:57:21 -0600

    test: pins detectron2_onnx

---
+++ test_unstructured/staging/.test_huggingface.py
@@ -0,0 +1,68 @@
+import pytest
+
+from unstructured.documents.elements import Text
+from unstructured.staging import huggingface
+
+
+class MockTokenizer:
+    model_max_length = 20
+
+    def tokenize(self, text):
+        return text.split(" ")
+
+
+def test_stage_for_transformers():
+    elements = [Text(text="hello " * 20), Text(text="there " * 20)]
+    tokenizer = MockTokenizer()
+
+    chunks = huggingface.stage_for_transformers(elements, tokenizer, buffer=10)
+
+    hello_chunk = ("hello " * 10).strip()
+    there_chunk = ("there " * 10).strip()
+    assert chunks == [hello_chunk, hello_chunk, "\n\n" + there_chunk, there_chunk]
+
+
+def test_chunk_by_attention_window():
+    text = "hello " * 20 + "there " * 20
+    tokenizer = MockTokenizer()
+    chunks = huggingface.chunk_by_attention_window(text, tokenizer, buffer=10)
+
+    hello_chunk = ("hello " * 10).strip()
+    there_chunk = ("there " * 10).strip()
+    assert chunks == [hello_chunk, hello_chunk, there_chunk, there_chunk]
+
+
+def test_chunk_by_attention_window_no_buffer():
+    text = "hello " * 20 + "there " * 20
+    tokenizer = MockTokenizer()
+    chunks = huggingface.chunk_by_attention_window(text, tokenizer, buffer=0)
+
+    hello_chunk = ("hello " * 20).strip()
+    there_chunk = ("there " * 20).strip()
+    assert chunks == [hello_chunk, there_chunk]
+
+
+def test_chunk_by_attention_window_raises_with_negative_buffer():
+    text = "hello " * 20 + "there " * 20
+    tokenizer = MockTokenizer()
+    with pytest.raises(ValueError):
+        huggingface.chunk_by_attention_window(text, tokenizer, buffer=-10)
+
+
+def test_chunk_by_attention_window_raises_if_buffer_too_big():
+    text = "hello " * 20 + "there " * 20
+    tokenizer = MockTokenizer()
+    with pytest.raises(ValueError):
+        # NOTE(robinson) - The buffer exceeds the max input size of 20
+        huggingface.chunk_by_attention_window(text, tokenizer, buffer=40)
+
+
+def test_chunk_by_attention_window_raises_if_chunk_exceeds_window():
+    text = "hello " * 100 + "."
+    tokenizer = MockTokenizer()
+    with pytest.raises(ValueError):
+
+        def split_function(text):
+            return text.split(".")
+
+        huggingface.chunk_by_attention_window(text, tokenizer, split_function=split_function)
