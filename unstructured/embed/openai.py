08fa43ebe35c27f65892dba925850ad7075a1370
Ryan Nikolaidis <1208590+ryannikolaidis@users.noreply.github.com>
2023-09-28 22:18:27 -0700

    fix duplicate embeddings

--- unstructured/embed/openai.py
+++ unstructured/embed/openai.py
@@ -39,16 +39,9 @@ class OpenAIEmbeddingEncoder(BaseEmbeddingEncoder):
         elements_w_embedding = []
 
         for i, element in enumerate(elements):
-            original_method = element.to_dict
-
-            def new_to_dict(self):
-                d = original_method()
-                d["embeddings"] = self.embeddings
-                return d
-
             element.embeddings = embeddings[i]
             elements_w_embedding.append(element)
-            element.to_dict = types.MethodType(new_to_dict, element)
+
         return elements
 
     @EmbeddingEncoderConnectionError.wrap
