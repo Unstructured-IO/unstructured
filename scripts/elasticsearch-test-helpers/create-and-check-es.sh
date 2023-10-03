2e9b2a4f0149212cd808236e6ae59ccdf4626944
Roman Isecke <roman@unstructured.io>
2023-10-03 08:02:01 -0400

    debugging CI

--- scripts/elasticsearch-test-helpers/create-and-check-es.sh
+++ scripts/elasticsearch-test-helpers/create-and-check-es.sh
@@ -5,9 +5,9 @@ set -e
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 # Create the Elasticsearch cluster
-docker-compose --version
-docker-compose -f "$SCRIPT_DIR"/docker-compose.yaml up --wait
-docker-compose -f "$SCRIPT_DIR"/docker-compose.yaml ps
+docker compose version
+docker compose -f "$SCRIPT_DIR"/docker-compose.yaml up --wait
+docker compose -f "$SCRIPT_DIR"/docker-compose.yaml ps
 
 
 echo "Cluster is live."
