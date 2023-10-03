f4aa793ac53465da1d78dcb311336c2ad7593bc4
Klaijan <klaijan@unstructured.io>
2023-10-03 11:56:08 -0400

    Update create_and_fill_es.py

--- scripts/elasticsearch-test-helpers/create_and_fill_es.py
+++ scripts/elasticsearch-test-helpers/create_and_fill_es.py
@@ -17,7 +17,7 @@ print(es.info())
 df = pd.read_csv(DATA_PATH).dropna().reset_index()
 
 print("Creating an Elasticsearch index for testing elasticsearch ingest.")
-response = es.options(max_retries=10).indices.create(index=INDEX_NAME, mappings=MAPPINGS)
+response = es.options(max_retries=5).indices.create(index=INDEX_NAME, mappings=MAPPINGS)
 if response.meta.status != 200:
     raise RuntimeError("failed to create index")
 
