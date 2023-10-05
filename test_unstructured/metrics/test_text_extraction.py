9496cbd4b9287fbeb464c9a1c45d98e73c3f3855
Klaijan Sinteppadon <klaijan@unstructured.io>
2023-10-05 17:13:20 -0400

    add assert statement

--- test_unstructured/metrics/test_text_extraction.py
+++ test_unstructured/metrics/test_text_extraction.py
@@ -5,19 +5,21 @@ from unstructured.partition.auto import partition
 
 
 @pytest.mark.parametrize(
-    ("filename", "similarity"),
+    ("filename", "similarity", "percentage"),
     [
-        ("fake-text.txt", 0.82),
-        ("eml/fake-email.eml", 0.03),
-        ("README.md", 0.0),
+        ("fake-text.txt", 0.82, 0.18),
+        ("eml/fake-email.eml", 0.03, 0.97),
+        ("README.md", 0.0, 1.00),
     ],
 )
-def test_calculate_edit_distance(filename, similarity):
+def test_calculate_edit_distance(filename, similarity, percentage):
     with open("example-docs/fake-text.txt") as f:
         source_cct = f.read()
 
     elements = partition(filename=f"example-docs/{filename}")
     output_cct = "\n\n".join([str(el) for el in elements])
 
-    score = calculate_edit_distance(output_cct, source_cct)
+    score = calculate_edit_distance(output_cct, source_cct, return_as="score")
+    distance = calculate_edit_distance(output_cct, source_cct, return_as="percentage")
     assert round(score, 2) == similarity
+    assert round(distance, 2) == percentage
