8ede60851d027195d29dc31710440eed4ab525ee
Klaijan Sinteppadon <klaijan@unstructured.io>
2023-10-05 21:09:11 -0400

    linting

--- test_unstructured/metrics/test_text_extraction.py
+++ test_unstructured/metrics/test_text_extraction.py
@@ -17,13 +17,33 @@ def test_calculate_edit_distance():
     source_cct_dup_word = "I like pizza pizza. I like bagels."
 
     assert round(calculate_edit_distance(source_cct, source_cct, return_as="score"), 2) == 1.0
-    assert round(calculate_edit_distance(source_cct_word_space, source_cct, return_as="score"), 2) == 0.75
-    assert round(calculate_edit_distance(source_cct_spaces, source_cct, return_as="score"), 2) == 0.39
-    assert round(calculate_edit_distance(source_cct_no_space, source_cct, return_as="score"), 2) == 0.64
-    assert round(calculate_edit_distance(source_cct_one_sentence, source_cct, return_as="score"), 2) == 0.0
-    assert round(calculate_edit_distance(source_cct_missing_word, source_cct, return_as="score"), 2) == 0.57
-    assert round(calculate_edit_distance(source_cct_addn_char, source_cct, return_as="score"), 2) == 0.89
-    assert round(calculate_edit_distance(source_cct_dup_word, source_cct, return_as="score"), 2) == 0.79
+    assert (
+        round(calculate_edit_distance(source_cct_word_space, source_cct, return_as="score"), 2)
+        == 0.75
+    )
+    assert (
+        round(calculate_edit_distance(source_cct_spaces, source_cct, return_as="score"), 2) == 0.39
+    )
+    assert (
+        round(calculate_edit_distance(source_cct_no_space, source_cct, return_as="score"), 2)
+        == 0.64
+    )
+    assert (
+        round(calculate_edit_distance(source_cct_one_sentence, source_cct, return_as="score"), 2)
+        == 0.0
+    )
+    assert (
+        round(calculate_edit_distance(source_cct_missing_word, source_cct, return_as="score"), 2)
+        == 0.57
+    )
+    assert (
+        round(calculate_edit_distance(source_cct_addn_char, source_cct, return_as="score"), 2)
+        == 0.89
+    )
+    assert (
+        round(calculate_edit_distance(source_cct_dup_word, source_cct, return_as="score"), 2)
+        == 0.79
+    )
 
 
 @pytest.mark.parametrize(
