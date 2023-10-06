2a4bed04bf84e7f454f2609941cf837219abd1a3
Klaijan Sinteppadon <klaijan@unstructured.io>
2023-10-05 21:08:52 -0400

    add test case

--- test_unstructured/metrics/test_text_extraction.py
+++ test_unstructured/metrics/test_text_extraction.py
@@ -1,7 +1,29 @@
+import re
+
 import pytest
 
 from unstructured.metrics.text_extraction import calculate_edit_distance
 from unstructured.partition.auto import partition
+
+
+def test_calculate_edit_distance():
+    source_cct = "I like pizza. I like bagels."
+    source_cct_word_space = "I like p i z z a . I like bagles."
+    source_cct_spaces = re.sub(r"\s+", " ", " ".join(source_cct))
+    source_cct_no_space = source_cct.replace(" ", "")
+    source_cct_one_sentence = "I like pizza."
+    source_cct_missing_word = "I like pizza. I like ."
+    source_cct_addn_char = "I like pizza. I like beagles."
+    source_cct_dup_word = "I like pizza pizza. I like bagels."
+
+    assert round(calculate_edit_distance(source_cct, source_cct, return_as="score"), 2) == 1.0
+    assert round(calculate_edit_distance(source_cct_word_space, source_cct, return_as="score"), 2) == 0.75
+    assert round(calculate_edit_distance(source_cct_spaces, source_cct, return_as="score"), 2) == 0.39
+    assert round(calculate_edit_distance(source_cct_no_space, source_cct, return_as="score"), 2) == 0.64
+    assert round(calculate_edit_distance(source_cct_one_sentence, source_cct, return_as="score"), 2) == 0.0
+    assert round(calculate_edit_distance(source_cct_missing_word, source_cct, return_as="score"), 2) == 0.57
+    assert round(calculate_edit_distance(source_cct_addn_char, source_cct, return_as="score"), 2) == 0.89
+    assert round(calculate_edit_distance(source_cct_dup_word, source_cct, return_as="score"), 2) == 0.79
 
 
 @pytest.mark.parametrize(
@@ -12,7 +34,7 @@ from unstructured.partition.auto import partition
         ("README.md", 0.0, 719),
     ],
 )
-def test_calculate_edit_distance(filename, expected_score, expected_distance):
+def test_calculate_edit_distance_with_filename(filename, expected_score, expected_distance):
     with open("example-docs/fake-text.txt") as f:
         source_cct = f.read()
 
