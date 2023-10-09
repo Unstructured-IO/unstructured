01e29df31f99ce3b53f56ae3799b00dd4421d20a
coniferish <johnjennings702@gmail.com>
2023-10-09 16:13:04 -0500

    update error message

--- unstructured/partition/pdf.py
+++ unstructured/partition/pdf.py
@@ -214,7 +214,9 @@ def partition_pdf_or_image(
     # function.
 
     if not isinstance(languages, list):
-        raise TypeError("The language parameter must be a list of language codes as strings.")
+        raise TypeError(
+            "The language parameter must be a list of language codes as strings, ex. ['eng']",
+        )
 
     if ocr_languages is not None:
         if languages != ["eng"]:
