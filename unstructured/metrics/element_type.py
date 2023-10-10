652dcc8deadaf4f39a278a08deee078422363627
Klaijan Sinteppadon <klaijan@unstructured.io>
2023-10-10 16:39:38 -0400

    edit element count to tuple

--- unstructured/metrics/element_type.py
+++ unstructured/metrics/element_type.py
@@ -1,19 +1,22 @@
-from typing import Dict, List, Union
+from typing import Dict, List, Optional, Tuple, Union
+
+from unstructured.documents.elements import TYPE_TO_TEXT_ELEMENT_MAP
 
 
 def get_element_type_frequency(
     elements: List,
-) -> Union[Dict[str, Dict[str, int]], Dict]:
-    frequency: Dict = {}
+) -> Union[Dict[str, Tuple[Optional[str], int]], Dict]:
+    frequency: Dict = {key: {} for key in TYPE_TO_TEXT_ELEMENT_MAP}
     if len(elements) == 0:
         return frequency
     for element in elements:
         category = element.category
         category_depth = element.metadata.category_depth
-        if category not in frequency:
-            frequency[category] = {}
+
         if str(category_depth) not in frequency[category]:
             frequency[category][str(category_depth)] = 1
         else:
             frequency[category][str(category_depth)] += 1
+    for key in frequency:
+        frequency[key] = list(frequency[key].items())
     return frequency
