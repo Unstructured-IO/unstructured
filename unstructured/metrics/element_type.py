a5cccb694415615a9885bfd8466af0728925ede9
Klaijan Sinteppadon <klaijan@unstructured.io>
2023-10-10 19:15:26 -0400

    fix

--- unstructured/metrics/element_type.py
+++ unstructured/metrics/element_type.py
@@ -1,25 +1,21 @@
 from typing import Dict, List, Optional, Tuple, Union
 
-from unstructured.documents.elements import TYPE_TO_TEXT_ELEMENT_MAP
-
 
 def get_element_type_frequency(
     elements: List,
-) -> Union[Dict[str, Tuple[Optional[str], int]], Dict]:
+) -> Union[Dict[Tuple[str, Optional[int]], int], Dict]:
     """
     Calculate the frequency of Element Types from a list of elements.
     """
-    frequency: Dict = {key: {} for key in TYPE_TO_TEXT_ELEMENT_MAP}
+    frequency: Dict = {}
     if len(elements) == 0:
         return frequency
     for element in elements:
         category = element.category
         category_depth = element.metadata.category_depth
-
-        if str(category_depth) not in frequency[category]:
-            frequency[category][str(category_depth)] = 1
+        key = (category, category_depth)
+        if key not in frequency:
+            frequency[key] = 1
         else:
-            frequency[category][str(category_depth)] += 1
-    for key in frequency:
-        frequency[key] = list(frequency[key].items())
+            frequency[key] += 1
     return frequency
