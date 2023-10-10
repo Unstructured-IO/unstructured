eb6ebb645ae058299b43f4c55a8ef62f3616e0c2
Jason Scheirer <jason@unstructured.io>
2023-10-10 15:35:32 -0700

    Add 3.11 to matrix, but more changes

--- setup.py
+++ setup.py
@@ -96,6 +96,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     author="Unstructured Technologies",
--- test_unstructured_ingest/test-ingest.sh
+++ test_unstructured_ingest/test-ingest.sh
@@ -72,7 +72,7 @@ for test in "${all_tests[@]}"; do
   CURRENT_TEST="$test"
   # IF: python_version is not 3.10 (wildcarded to match any subminor version) AND the current test is not in full_python_matrix_tests
   # Note: to test we expand the full_python_matrix_tests array to a string and then regex match the current test
-  if [[ "$python_version" != "Python 3.10"* ]] && [[ ! "${full_python_matrix_tests[*]}" =~ $test ]] ; then
+  if [[ "$python_version" != "Python 3.1[0-2]"* ]] && [[ ! "${full_python_matrix_tests[*]}" =~ $test ]] ; then
     echo "--------- SKIPPING SCRIPT $test ---------"
     continue
   fi
