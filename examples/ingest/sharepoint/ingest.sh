f7f752b36a285b60c96a75fc655e5e9fd276d2c5
Ahmet Melek <ahmetmeleq@gmail.com>
2023-10-13 00:41:27 +0100

    update comments

--- examples/ingest/sharepoint/ingest.sh
+++ examples/ingest/sharepoint/ingest.sh
@@ -24,9 +24,9 @@ PYTHONPATH=. ./unstructured/ingest/main.py \
     --client-id "<Microsoft Sharepoint app client-id>" \
     --client-cred "<Microsoft Sharepoint app client-secret>" \
     --site "<e.g https://contoso.sharepoint.com or https://contoso.admin.sharepoint.com to process all sites within tenant>" \
-    --permissions-application-id "<Microsoft Graph API application id>" \
-    --permissions-client-cred "<Microsoft Graph API application credentials>" \
-    --permissions-tenant "<e.g https://contoso.onmicrosoft.com to process permission info within tenant>" \
+    --permissions-application-id "<Microsoft Graph API application id to process per-file access permissions>" \
+    --permissions-client-cred "<Microsoft Graph API application credentials to process per-file access permissions>" \
+    --permissions-tenant "<e.g https://contoso.onmicrosoft.com to process per-file access permissions>" \
     --files-only "Flag to process only files within the site(s)" \
     --output-dir sharepoint-ingest-output \
     --num-processes 2 \
