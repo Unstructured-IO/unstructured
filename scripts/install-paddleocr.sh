4ad016b751b16cb60b984c889e71897bef5131a5
yuming <305248291@qq.com>
2023-10-12 20:13:17 -0400

    revert paddle disable handler

--- scripts/install-paddleocr.sh
+++ scripts/install-paddleocr.sh
@@ -7,9 +7,3 @@ else
 		python3 -m pip install paddlepaddle;
 fi
 python3 -m pip install unstructured.paddleocr
-
-# Note(yuming): Disable signal handlers at C++ level upon failing
-# ref: https://www.paddlepaddle.org.cn/documentation/docs/en/api/paddle/disable_signal_handler_en.html#disable-signal-handler
-# this is mainly for the paddle instance used by layourparser
-# ref: https://github.com/Layout-Parser/layout-parser/blob/main/src/layoutparser/models/paddledetection/layoutmodel.py#L31
-python3 -c "import paddle; paddle.disable_signal_handler()"
