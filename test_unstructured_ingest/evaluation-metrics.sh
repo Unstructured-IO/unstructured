aecc41f4e369d42b63614245a75fc75a1908c9f5
Klaijan Sinteppadon <klaijan@unstructured.io>
2023-10-16 18:16:18 -0400

    set default region

--- test_unstructured_ingest/evaluation-metrics.sh
+++ test_unstructured_ingest/evaluation-metrics.sh
@@ -16,7 +16,7 @@ BUCKET_NAME=utic-dev-tech-fixtures
 FOLDER_NAME=small-cct
 CCT_DIR=$SCRIPT_DIR/gold-standard/$FOLDER_NAME
 mkdir -p "$CCT_DIR"
-aws s3 cp "s3://$BUCKET_NAME/$FOLDER_NAME" "$CCT_DIR" --recursive --no-sign-request --debug
+aws s3 cp "s3://$BUCKET_NAME/$FOLDER_NAME" "$CCT_DIR" --recursive --no-sign-request --debug --region us-east-2
 
 PYTHONPATH=. ./unstructured/ingest/evaluate.py \
     --output_dir "$OUTPUT_DIR" \
