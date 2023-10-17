78de5fb4fa01996388e3ccf9479a78145a00470c
Klaijan Sinteppadon <klaijan@unstructured.io>
2023-10-17 17:44:52 -0400

    add cleanup function

--- test_unstructured_ingest/evaluation-metrics.sh
+++ test_unstructured_ingest/evaluation-metrics.sh
@@ -13,10 +13,16 @@ BUCKET_NAME=utic-dev-tech-fixtures
 FOLDER_NAME=small-cct
 CCT_DIR=$SCRIPT_DIR/gold-standard/$FOLDER_NAME
 mkdir -p "$CCT_DIR"
-aws s3 cp "s3://$BUCKET_NAME/$FOLDER_NAME" "$CCT_DIR" --recursive --no-sign-request --debug --region us-east-2
+aws s3 cp "s3://$BUCKET_NAME/$FOLDER_NAME" "$CCT_DIR" --recursive --no-sign-request --region us-east-2
+
+# shellcheck disable=SC1091
+source "$SCRIPT_DIR"/cleanup.sh
+function cleanup() {
+  cleanup_dir "$OUTPUT_DIR"
+  cleanup_dir "$CCT_DIR"
+}
+trap cleanup EXIT
 
 PYTHONPATH=. ./unstructured/ingest/evaluate.py \
     --output_dir "$OUTPUT_DIR" \
     --source_dir "$CCT_DIR" 
-
-# don't forget to clean on exit
