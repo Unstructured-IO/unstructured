bae12cb0b695510912b1123cd5554401fb3adeb2
Roman Isecke <roman@unstructured.io>
2023-10-17 14:33:51 -0400

    Add all other fsspec writers

--- unstructured/ingest/runner/writers/azure_cognitive_search.py
+++ unstructured/ingest/runner/writers/azure_cognitive_search.py
@@ -1,9 +1,12 @@
+from unstructured.ingest.interfaces import BaseDestinationConnector
+
+
 def azure_cognitive_search_writer(
     endpoint: str,
     key: str,
     index: str,
     **kwargs,
-):
+) -> BaseDestinationConnector:
     from unstructured.ingest.connector.azure_cognitive_search import (
         AzureCognitiveSearchDestinationConnector,
         AzureCognitiveSearchWriteConfig,
