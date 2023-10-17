029474fc285c86b9883ad9135544d79c366174e3
Roman Isecke <roman@unstructured.io>
2023-10-17 14:33:51 -0400

    Add all other fsspec writers

---
+++ unstructured/ingest/runner/writers/box.py
@@ -0,0 +1,28 @@
+import typing as t
+
+from unstructured.ingest.interfaces import BaseDestinationConnector
+
+
+def box_writer(
+    remote_url: str,
+    box_app_config: t.Optional[str],
+    verbose: bool = False,
+) -> BaseDestinationConnector:
+    import boxsdk
+
+    from unstructured.ingest.connector.box import (
+        BoxDestinationConnector,
+        SimpleBoxConfig,
+    )
+    from unstructured.ingest.connector.fsspec import FsspecWriteConfig
+
+    access_kwargs: t.Dict[str, t.Any] = {"box_app_config": box_app_config}
+    if verbose:
+        access_kwargs["client_type"] = boxsdk.LoggingClient
+    return BoxDestinationConnector(
+        write_config=FsspecWriteConfig(),
+        connector_config=SimpleBoxConfig(
+            remote_url=remote_url,
+            access_kwargs=access_kwargs,
+        ),
+    )
