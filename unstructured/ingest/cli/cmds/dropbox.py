353d1f93ef45547cdd38171f8c4c9a42c348d0d8
Roman Isecke <roman@unstructured.io>
2023-10-18 09:37:04 -0400

    Add dropbox dest connector

--- unstructured/ingest/cli/cmds/dropbox.py
+++ unstructured/ingest/cli/cmds/dropbox.py
@@ -9,6 +9,8 @@ from unstructured.ingest.cli.interfaces import (
 )
 from unstructured.ingest.interfaces import BaseConfig
 
+CMD_NAME = "dropbox"
+
 
 @dataclass
 class DropboxCliConfig(BaseConfig, CliMixin):
@@ -27,5 +29,12 @@ class DropboxCliConfig(BaseConfig, CliMixin):
 
 
 def get_base_src_cmd() -> BaseSrcCmd:
-    cmd_cls = BaseSrcCmd(cmd_name="dropbox", cli_config=DropboxCliConfig, is_fsspec=True)
+    cmd_cls = BaseSrcCmd(cmd_name=CMD_NAME, cli_config=DropboxCliConfig, is_fsspec=True)
+    return cmd_cls
+
+
+def get_base_dest_cmd():
+    from unstructured.ingest.cli.base.dest import BaseDestCmd
+
+    cmd_cls = BaseDestCmd(cmd_name=CMD_NAME, cli_config=DropboxCliConfig, is_fsspec=True)
     return cmd_cls
