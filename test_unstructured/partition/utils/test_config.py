b33c264f25a951db253633a2f0c1a5cba2add90a
yuming <305248291@qq.com>
2023-10-19 15:05:40 -0400

    update default image crop pad

--- test_unstructured/partition/utils/test_config.py
+++ test_unstructured/partition/utils/test_config.py
@@ -1,7 +1,7 @@
 def test_default_config():
     from unstructured.partition.utils.config import env_config
 
-    assert env_config.IMAGE_CROP_PAD == 12
+    assert env_config.IMAGE_CROP_PAD == 0
 
 
 def test_env_override(monkeypatch):
--- unstructured/partition/utils/config.py
+++ unstructured/partition/utils/config.py
@@ -31,7 +31,7 @@ class ENVConfig:
     @property
     def IMAGE_CROP_PAD(self) -> int:
         """extra image content to add around an identified element region; measured in pixels"""
-        return self._get_int("IMAGE_CROP_PAD", 12)
+        return self._get_int("IMAGE_CROP_PAD", 0)
 
     @property
     def TESSERACT_TEXT_HEIGHT_QUANTILE(self) -> float:
