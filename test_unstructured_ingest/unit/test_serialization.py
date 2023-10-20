34e3ed04944ecfb04b46e7c6950e15f971ca3852
Roman Isecke <roman@unstructured.io>
2023-10-20 09:20:48 -0400

    Add unit test to validate the lack of meta on serialized content if it doesn't exist

--- test_unstructured_ingest/unit/test_serialization.py
+++ test_unstructured_ingest/unit/test_serialization.py
@@ -31,3 +31,18 @@ def test_registry_from_dict():
     serialized_dict: dict = json.loads(serialized_json)
     deserialized_doc = create_ingest_doc_from_dict(serialized_dict)
     assert doc == deserialized_doc
+
+
+def test_source_metadata_serialization():
+    doc = LocalIngestDoc(
+        path="test_unstructured_ingest/example-docs/layout-parser-paper.pdf",
+        connector_config=SimpleLocalConfig(input_path="test_unstructured_ingest/example-docs/"),
+        processor_config=ProcessorConfig(),
+        read_config=ReadConfig(),
+    )
+    serialized_json = doc.to_dict()
+    assert not serialized_json["_source_metadata"]
+
+    doc.update_source_metadata()
+    serialized_json_w_meta = doc.to_dict()
+    assert serialized_json_w_meta["_source_metadata"]
