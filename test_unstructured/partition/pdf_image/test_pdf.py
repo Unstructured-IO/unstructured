546e61866da855d8623d156f2e6c3d3cc403e023
christinestraub <christinemstraub@gmail.com>
2023-10-24 14:30:36 -0700

    test: fix unit test errors

--- test_unstructured/partition/pdf_image/test_pdf.py
+++ test_unstructured/partition/pdf_image/test_pdf.py
@@ -131,7 +131,7 @@ def test_partition_pdf_local_raises_with_no_filename():
     ("strategy", "expected", "origin"),
     # fast: can't capture the "intentionally left blank page" page
     # others: will ignore the actual blank page
-    [("fast", {1, 4}, "pdfminer"), ("hi_res", {1, 3, 4}, "pdf"), ("ocr_only", {1, 3, 4}, "OCR")],
+    [("fast", {1, 4}, "pdfminer"), ("hi_res", {1, 3, 4}, "pdf"), ("ocr_only", {1, 3, 4}, "pdf")],
 )
 def test_partition_pdf(
     file_mode,
@@ -1067,7 +1067,11 @@ def test_partition_model_name_default_to_None():
         ),
         (
             "ocr_only",
-            "unstructured_pytesseract.run_and_get_multiple_output",
+            "unstructured_pytesseract.image_to_data",
+        ),
+        (
+            "ocr_only",
+            "unstructured_pytesseract.image_to_string",
         ),
     ],
 )
