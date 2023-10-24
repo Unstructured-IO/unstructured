b9702e633495a026a0d42803fef7dc97fc054761
rvztz <revazqueztuz@gmail.com>
2023-10-24 16:51:42 -0600

    Updates cli interface

--- unstructured/ingest/cli/cmds/hubspot.py
+++ unstructured/ingest/cli/cmds/hubspot.py
@@ -3,16 +3,11 @@ import typing as t
 from dataclasses import dataclass
 
 import click
-
-from unstructured.ingest.cli.common import (
-    log_options,
-)
 from unstructured.ingest.cli.interfaces import CliMixin, DelimitedString
-from unstructured.ingest.cli.utils import Group, add_options, conform_click_options, extract_configs
+from unstructured.ingest.cli.base.src import BaseSrcCmd
 from unstructured.ingest.connector.hubspot import HubSpotObjectTypes
 from unstructured.ingest.interfaces import BaseConfig
-from unstructured.ingest.logger import ingest_log_streaming_init, logger
-from unstructured.ingest.runner import HubSpotRunner
+from unstructured.ingest.logger import logger
 
 OBJECT_TYPES = {t.value for t in HubSpotObjectTypes}
 
@@ -37,7 +32,7 @@ class HubSpotCliConfig(BaseConfig, CliMixin):
     custom_properties: t.Optional[t.Dict[str, t.List[str]]] = None
 
     @staticmethod
-    def add_cli_options(cmd: click.Command) -> None:
+    def get_cli_options() -> t.List[click.Option]:
         options = [
             click.Option(
                 ["--api-token"],
@@ -72,30 +67,11 @@ class HubSpotCliConfig(BaseConfig, CliMixin):
                         it will be omitted.",
             ),
         ]
-        cmd.params.extend(options)
+        return options
 
-
-@click.group(name="hubspot", invoke_without_command=True, cls=Group)
-@click.pass_context
-def hubspot_source(ctx: click.Context, **options):
-    if ctx.invoked_subcommand:
-        return
-    conform_click_options(options)
-    verbose = options.get("verbose", False)
-    ingest_log_streaming_init(logging.DEBUG if verbose else logging.INFO)
-    log_options(options, verbose=verbose)
-    try:
-        configs = extract_configs(options, validate=([HubSpotCliConfig]))
-        runner = HubSpotRunner(
-            **configs,  # type: ignore
-        )
-        runner.run(**options)
-    except Exception as e:
-        logger.error(e, exc_info=True)
-        raise click.ClickException(str(e)) from e
-
-
-def get_source_cmd() -> click.Group:
-    cmd = hubspot_source
-    add_options(cmd, extras=[HubSpotCliConfig])
-    return cmd
+def get_base_src_cmd() -> BaseSrcCmd:
+    cmd_cls = BaseSrcCmd(
+        cmd_name="hubspot",
+        cli_config=HubSpotCliConfig,
+    )
+    return cmd_cls
