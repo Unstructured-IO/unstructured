37e841310afa0745c826d52e1d934a71795452a6
Roman Isecke <136338424+rbiseck3@users.noreply.github.com>
2023-10-24 08:57:33 -0400

    refactor ingest CLI for better code reuse (#1846)
    ### Description
    Much of the current CLI code is copy-paste across subcommands. To
    alleviate this, most of the duplicate code was moved into base classes
    for src and destination connector commands. This also allows for code
    reuse when a destination command is called and it no longer has to jump
    through hoops to dynamically recreate what _would_ have been called by a
    source command.
    
    The reason everything can't live in a single BaseCmd class is due to the
    need for a dynamic map to the source command. This runs into a circular
    dependency issue if it was all in one class. By splitting it into a
    `BaseSrcCmd` and a `BaseDestCmd` class, this helps avoid that issue.

--- unstructured/ingest/cli/cmds/__init__.py
+++ unstructured/ingest/cli/cmds/__init__.py
@@ -1,67 +1,97 @@
+from __future__ import annotations
+
+import collections
 import typing as t
 
-import click
+from unstructured.ingest.cli.base.src import BaseSrcCmd
 
-from .airtable import get_source_cmd as airtable_src
-from .azure import get_source_cmd as azure_src
-from .azure_cognitive_search import get_dest_cmd as azure_cognitive_search_dest
-from .biomed import get_source_cmd as biomed_src
-from .box import get_source_cmd as box_src
-from .confluence import get_source_cmd as confluence_src
-from .delta_table import get_dest_cmd as delta_table_dest
-from .delta_table import get_source_cmd as delta_table_src
-from .discord import get_source_cmd as discord_src
-from .dropbox import get_source_cmd as dropbox_src
-from .elasticsearch import get_source_cmd as elasticsearch_src
-from .fsspec import get_source_cmd as fsspec_src
-from .gcs import get_source_cmd as gcs_src
-from .github import get_source_cmd as github_src
-from .gitlab import get_source_cmd as gitlab_src
-from .google_drive import get_source_cmd as google_drive_src
-from .jira import get_source_cmd as jira_src
-from .local import get_source_cmd as local_src
-from .notion import get_source_cmd as notion_src
-from .onedrive import get_source_cmd as onedrive_src
-from .outlook import get_source_cmd as outlook_src
-from .reddit import get_source_cmd as reddit_src
-from .s3 import get_dest_cmd as s3_dest
-from .s3 import get_source_cmd as s3_src
-from .salesforce import get_source_cmd as salesforce_src
-from .sharepoint import get_source_cmd as sharepoint_src
-from .slack import get_source_cmd as slack_src
-from .wikipedia import get_source_cmd as wikipedia_src
+from .airtable import get_base_src_cmd as airtable_base_src_cmd
+from .azure import get_base_src_cmd as azure_base_src_cmd
+from .azure_cognitive_search import get_base_dest_cmd as azure_cognitive_search_base_dest_cmd
+from .biomed import get_base_src_cmd as biomed_base_src_cmd
+from .box import get_base_src_cmd as box_base_src_cmd
+from .confluence import get_base_src_cmd as confluence_base_src_cmd
+from .delta_table import get_base_dest_cmd as delta_table_dest_cmd
+from .delta_table import get_base_src_cmd as delta_table_base_src_cmd
+from .discord import get_base_src_cmd as discord_base_src_cmd
+from .dropbox import get_base_src_cmd as dropbox_base_src_cmd
+from .elasticsearch import get_base_src_cmd as elasticsearch_base_src_cmd
+from .fsspec import get_base_src_cmd as fsspec_base_src_cmd
+from .gcs import get_base_src_cmd as gcs_base_src_cmd
+from .github import get_base_src_cmd as github_base_src_cmd
+from .gitlab import get_base_src_cmd as gitlab_base_src_cmd
+from .google_drive import get_base_src_cmd as google_drive_base_src_cmd
+from .jira import get_base_src_cmd as jira_base_src_cmd
+from .local import get_base_src_cmd as local_base_src_cmd
+from .notion import get_base_src_cmd as notion_base_src_cmd
+from .onedrive import get_base_src_cmd as onedrive_base_src_cmd
+from .outlook import get_base_src_cmd as outlook_base_src_cmd
+from .reddit import get_base_src_cmd as reddit_base_src_cmd
+from .s3 import get_base_dest_cmd as s3_base_dest_cmd
+from .s3 import get_base_src_cmd as s3_base_src_cmd
+from .salesforce import get_base_src_cmd as salesforce_base_src_cmd
+from .sharepoint import get_base_src_cmd as sharepoint_base_src_cmd
+from .slack import get_base_src_cmd as slack_base_src_cmd
+from .wikipedia import get_base_src_cmd as wikipedia_base_src_cmd
 
-src: t.List[click.Group] = [
-    airtable_src(),
-    azure_src(),
-    biomed_src(),
-    box_src(),
-    confluence_src(),
-    delta_table_src(),
-    discord_src(),
-    dropbox_src(),
-    elasticsearch_src(),
-    fsspec_src(),
-    gcs_src(),
-    github_src(),
-    gitlab_src(),
-    google_drive_src(),
-    jira_src(),
-    local_src(),
-    notion_src(),
-    onedrive_src(),
-    outlook_src(),
-    reddit_src(),
-    salesforce_src(),
-    sharepoint_src(),
-    slack_src(),
-    s3_src(),
-    wikipedia_src(),
+if t.TYPE_CHECKING:
+    from unstructured.ingest.cli.base.dest import BaseDestCmd
+
+base_src_cmd_fns: t.List[t.Callable[[], BaseSrcCmd]] = [
+    airtable_base_src_cmd,
+    azure_base_src_cmd,
+    biomed_base_src_cmd,
+    box_base_src_cmd,
+    confluence_base_src_cmd,
+    delta_table_base_src_cmd,
+    discord_base_src_cmd,
+    dropbox_base_src_cmd,
+    elasticsearch_base_src_cmd,
+    fsspec_base_src_cmd,
+    gcs_base_src_cmd,
+    github_base_src_cmd,
+    gitlab_base_src_cmd,
+    google_drive_base_src_cmd,
+    jira_base_src_cmd,
+    local_base_src_cmd,
+    notion_base_src_cmd,
+    onedrive_base_src_cmd,
+    outlook_base_src_cmd,
+    reddit_base_src_cmd,
+    salesforce_base_src_cmd,
+    sharepoint_base_src_cmd,
+    slack_base_src_cmd,
+    s3_base_src_cmd,
+    wikipedia_base_src_cmd,
 ]
 
-dest: t.List[click.Command] = [azure_cognitive_search_dest(), s3_dest(), delta_table_dest()]
+# Make sure there are not overlapping names
+src_cmd_names = [b().cmd_name for b in base_src_cmd_fns]
+src_duplicates = [item for item, count in collections.Counter(src_cmd_names).items() if count > 1]
+if src_duplicates:
+    raise ValueError(
+        "multiple base src commands defined with the same names: {}".format(
+            ", ".join(src_duplicates),
+        ),
+    )
+
+base_dest_cmd_fns: t.List[t.Callable[[], "BaseDestCmd"]] = [
+    s3_base_dest_cmd,
+    azure_cognitive_search_base_dest_cmd,
+    delta_table_dest_cmd,
+]
+
+# Make sure there are not overlapping names
+dest_cmd_names = [b().cmd_name for b in base_dest_cmd_fns]
+dest_duplicates = [item for item, count in collections.Counter(dest_cmd_names).items() if count > 1]
+if dest_duplicates:
+    raise ValueError(
+        "multiple base dest commands defined with the same names: {}".format(
+            ", ".join(dest_duplicates),
+        ),
+    )
 
 __all__ = [
-    "src",
-    "dest",
+    "base_src_cmd_fns",
+    "base_dest_cmd_fns",
 ]
--- unstructured/ingest/cli/cmds/delta_table.py
+++ unstructured/ingest/cli/cmds/delta_table.py
@@ -1,25 +1,15 @@
-import logging
 import typing as t
 from dataclasses import dataclass
 
 import click
 
-from unstructured.ingest.cli.common import (
-    log_options,
-)
+from unstructured.ingest.cli.base.src import BaseSrcCmd
 from unstructured.ingest.cli.interfaces import (
     CliMixin,
 )
-from unstructured.ingest.cli.utils import (
-    Group,
-    add_options,
-    conform_click_options,
-    extract_configs,
-    orchestrate_runner,
-)
 from unstructured.ingest.interfaces import BaseConfig
-from unstructured.ingest.logger import ingest_log_streaming_init, logger
-from unstructured.ingest.runner import DeltaTableRunner
+
+CMD_NAME = "delta-table"
 
 
 @dataclass
@@ -60,27 +50,6 @@ class DeltaTableCliConfig(BaseConfig, CliMixin):
         return options
 
 
-@click.group(name="delta-table", invoke_without_command=True, cls=Group)
-@click.pass_context
-def delta_table_source(ctx: click.Context, **options):
-    if ctx.invoked_subcommand:
-        return
-
-    conform_click_options(options)
-    verbose = options.get("verbose", False)
-    ingest_log_streaming_init(logging.DEBUG if verbose else logging.INFO)
-    log_options(options, verbose=verbose)
-    try:
-        configs = extract_configs(options, validate=[DeltaTableCliConfig])
-        runner = DeltaTableRunner(
-            **configs,  # type: ignore
-        )
-        runner.run(**options)
-    except Exception as e:
-        logger.error(e, exc_info=True)
-        raise click.ClickException(str(e)) from e
-
-
 @dataclass
 class DeltaTableCliWriteConfig(BaseConfig, CliMixin):
     write_column: str
@@ -108,42 +77,17 @@ class DeltaTableCliWriteConfig(BaseConfig, CliMixin):
         return options
 
 
-@click.command(name="delta-table")
-@click.pass_context
-def delta_table_dest(ctx: click.Context, **options):
-    if not ctx.parent:
-        raise click.ClickException("destination command called without a parent")
-    if not ctx.parent.info_name:
-        raise click.ClickException("parent command missing info name")
-    source_cmd = ctx.parent.info_name.replace("-", "_")
-    parent_options: dict = ctx.parent.params if ctx.parent else {}
-    conform_click_options(options)
-    conform_click_options(parent_options)
-    verbose = parent_options.get("verbose", False)
-    ingest_log_streaming_init(logging.DEBUG if verbose else logging.INFO)
-    log_options(parent_options, verbose=verbose)
-    log_options(options, verbose=verbose)
-    try:
-        orchestrate_runner(
-            source_cmd=source_cmd,
-            writer_type="delta_table",
-            parent_options=parent_options,
-            options=options,
-            validate=[DeltaTableCliWriteConfig],
-        )
-    except Exception as e:
-        logger.error(e, exc_info=True)
-        raise click.ClickException(str(e)) from e
+def get_base_src_cmd() -> BaseSrcCmd:
+    cmd_cls = BaseSrcCmd(cmd_name=CMD_NAME, cli_config=DeltaTableCliConfig)
+    return cmd_cls
 
 
-def get_dest_cmd() -> click.Command:
-    cmd = delta_table_dest
-    DeltaTableCliConfig.add_cli_options(cmd)
-    DeltaTableCliWriteConfig.add_cli_options(cmd)
-    return cmd
+def get_base_dest_cmd():
+    from unstructured.ingest.cli.base.dest import BaseDestCmd
 
-
-def get_source_cmd() -> click.Group:
-    cmd = delta_table_source
-    add_options(cmd, extras=[DeltaTableCliConfig])
-    return cmd
+    cmd_cls = BaseDestCmd(
+        cmd_name=CMD_NAME,
+        cli_config=DeltaTableCliConfig,
+        additional_cli_options=[DeltaTableCliWriteConfig],
+    )
+    return cmd_cls
