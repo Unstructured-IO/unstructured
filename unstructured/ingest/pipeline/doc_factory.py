4753b8fbf36b37a6bfb4a4de66b12dc5778f0029
Roman Isecke <roman@unstructured.io>
2023-10-25 10:37:10 -0400

    Fix registry dict method to not change passed in dict

--- unstructured/ingest/pipeline/doc_factory.py
+++ unstructured/ingest/pipeline/doc_factory.py
@@ -1,13 +1,12 @@
 import typing as t
 from dataclasses import dataclass
 
-from unstructured.ingest.interfaces import IngestDocJsonMixin
 from unstructured.ingest.pipeline.interfaces import DocFactoryNode
 
 
 @dataclass
 class DocFactory(DocFactoryNode):
     def run(self, *args, **kwargs) -> t.Iterable[dict]:
-        docs: t.List[IngestDocJsonMixin] = self.source_doc_connector.get_ingest_docs()
+        docs = self.source_doc_connector.get_ingest_docs()
         json_docs = [doc.to_dict() for doc in docs]
         return json_docs
