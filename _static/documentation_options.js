984545933782e8f16d4832a9d0c87794038fe61f
qued <qued@users.noreply.github.com>
2023-10-26 17:25:51 +0000

    deploy: 808b4ced7aecfdda0edf988f22176f59c3bc6d5a

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.10.26',
+    VERSION: '0.10.27',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
