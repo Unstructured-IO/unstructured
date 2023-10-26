2e4dda21b9677b6a9ae4da9d78b15902ec67990d
Ahmet Melek <ahmetmeleq@gmail.com>
2023-10-26 13:26:13 +0100

    update example

--- examples/ingest/pinecone/ingest.sh
+++ examples/ingest/pinecone/ingest.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
 
-# Processes all the files from abfs://container1/ in azureunstructured1 account,
-# using the `unstructured` library.
+# Processes all the files from s3://utic-dev-tech-fixtures/small-pdf-set/,
+# embeds the processed documents, and writes to results to a Pinecone index.
 
-# Structured outputs are stored in azure-ingest-output/
+# Structured outputs are stored in s3-small-batch-output-to-pinecone/
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"/../../.. || exit 1
 
 # We remove chunking params until the chunk/embed ordering fix is merged
+
+# As an example we're using the s3 source connector,
+# however ingesting from any supported source connector is possible.
 PYTHONPATH=. ./unstructured/ingest/main.py \
         s3 \
-         --remote-url s3://utic-dev-tech-fixtures/small-pdf-set/ \
+         --remote-url "<s3 URL to ingest from, ie: s3://utic-dev-tech-fixtures/small-pdf-set/" \
          --anonymous \
          --output-dir s3-small-batch-output-to-pinecone \
          --num-processes 2 \
          --verbose \
          --strategy fast \
-         --embedding-api-key "$OPENAI_API_KEY" \
+         --embedding-api-key "<OpenAI API Key to embed ingested elements>" \
         pinecone \
-        --api-key "$PINECONE_API_KEY" \
-        --index-name ingest-test \
-        --environment "gcp-starter"
+        --api-key "<Pinecone API Key to write into a Pinecone index>" \
+        --index-name "<Pinecone index name, ie: ingest-test>" \
+        --environment "<Pinecone environment name, ie: gcp-starter>"
