11701768a09284d90a757955ba00ee0ffbb1ebe2
Benjamin Torres <benjamin@unstructured.io>
2023-10-27 11:41:59 -0600

    refactor: improvement on redability

--- unstructured/partition/utils/processing_elements.py
+++ unstructured/partition/utils/processing_elements.py
@@ -12,10 +12,11 @@ def clean_pdfminer_inner_elements(document: DocumentLayout) -> Tuple[DocumentLay
     for page in document.pages:
         tables = [e for e in page.elements if e.type == "Table"]
         for i, element in enumerate(page.elements):
-            if element.source == Source.PDFMINER:
-                element_inside_table = [element.bbox.is_in(t.bbox, error_margin=15) for t in tables]
-                if sum(element_inside_table) == 1:
-                    page.elements[i] = None
+            if not element.source == Source.PDFMINER:
+                continue
+            element_inside_table = [element.bbox.is_in(t.bbox, error_margin=15) for t in tables]
+            if sum(element_inside_table) == 1:
+                page.elements[i] = None
         page.elements = [e for e in page.elements if e]
 
     return document
