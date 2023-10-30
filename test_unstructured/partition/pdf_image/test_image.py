4beca6584440841b8ff10a89406c21cae91190a7
christinestraub <christinemstraub@gmail.com>
2023-10-30 12:13:19 -0700

    test: update test function

--- test_unstructured/partition/pdf_image/test_image.py
+++ test_unstructured/partition/pdf_image/test_image.py
@@ -260,7 +260,7 @@ def test_partition_image_default_strategy_hi_res():
     assert isinstance(elements[idx].metadata.detection_class_prob, float)
     if UNSTRUCTURED_INCLUDE_DEBUG_METADATA:
         # A bug in partition_groups_from_regions in unstructured-inference losses some sources
-        assert {element.metadata.detection_origin for element in elements} == {"yolox", None}
+        assert {element.metadata.detection_origin for element in elements} == {"yolox", "ocr_tesseract"}
 
 
 def test_partition_image_metadata_date(
