cdbe8dbd380700db555c8eb59b486f3a17213b4f
Alan Bertl <alan@unstructured.io>
2023-10-30 11:29:59 -0500

    linting

--- test_unstructured/partition/pdf_image/test_chipper.py
+++ test_unstructured/partition/pdf_image/test_chipper.py
@@ -18,12 +18,12 @@ def chipper_children(chipper_results):
     return [el for el in chipper_results if el.metadata.parent_id is not None]
 
 
-@pytest.mark.chipper
+@pytest.mark.chipper()
 def test_chipper_has_hierarchy(chipper_children):
     assert chipper_children
 
 
-@pytest.mark.chipper
+@pytest.mark.chipper()
 def test_chipper_not_losing_parents(chipper_results, chipper_children):
     assert all(
         [el for el in chipper_results if el.id == child.metadata.parent_id]
