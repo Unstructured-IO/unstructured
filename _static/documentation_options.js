35e81f10fa76aa58f77b24644a0b37e7ed196513
cragwolfe <cragwolfe@users.noreply.github.com>
2023-10-31 06:03:14 +0000

    deploy: ecbc4546e3858d1347a7a74b932c5236b4d12ed9

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.10.27',
+    VERSION: '0.10.28',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
