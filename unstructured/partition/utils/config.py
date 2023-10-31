ca3ca80dad0841106447345b1e930a756638cbf6
christinestraub <christinemstraub@gmail.com>
2023-10-31 11:17:45 -0700

    feat: add `TABLE_IMAGE_CROP_PAD` to env config

--- unstructured/partition/utils/config.py
+++ unstructured/partition/utils/config.py
@@ -36,6 +36,15 @@ class ENVConfig:
         return self._get_int("IMAGE_CROP_PAD", 0)
 
     @property
+    def TABLE_IMAGE_CROP_PAD(self) -> int:
+        """extra image content to add around an identified table region; measured in pixels
+
+        The padding adds image data around an identified table bounding box for downstream table
+        structure detection model use as input
+        """
+        return self._get_int("TABLE_IMAGE_CROP_PAD", 12)
+
+    @property
     def TESSERACT_TEXT_HEIGHT_QUANTILE(self) -> float:
         """the quantile to check for text height"""
         return self._get_float("TESSERACT_TEXT_HEIGHT_QUANTILE", 0.5)
