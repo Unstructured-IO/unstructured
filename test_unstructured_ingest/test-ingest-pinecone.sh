cf7731584d3074e44ed670956415828b54a85d29
Ahmet Melek <ahmetmeleq@gmail.com>
2023-11-03 16:09:06 +0000

    do not exclude metadata

--- test_unstructured_ingest/test-ingest-pinecone.sh
+++ test_unstructured_ingest/test-ingest-pinecone.sh
@@ -89,7 +89,6 @@ fi
 PYTHONPATH=. ./unstructured/ingest/main.py \
   local \
   --num-processes "$max_processes" \
-  --metadata-exclude coordinates,filename,file_directory,metadata.data_source.date_processed,metadata.last_modified,metadata.detection_class_prob,metadata.parent_id,metadata.category_depth \
   --output-dir "$OUTPUT_DIR" \
   --strategy fast \
   --verbose \
