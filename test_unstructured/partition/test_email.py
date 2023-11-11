e15db5825e6fd7c6c410ad266be0885ba0d9b585
Steve Canny <stcanny@gmail.com>
2023-11-11 10:53:52 -0800

    rfctr: remove test-induced breakage
    Adding interface methods to objects strictly for use in tests is
    test-induced breakage. Interface methods should be restricted to those
    required by clients because once added we need to support them
    indefinitely.
    
    Remove `.get_last_modified()` method used only by tests replacing it
    with a unit-test utility function `parse_optional_datetime()`.

--- test_unstructured/partition/test_email.py
+++ test_unstructured/partition/test_email.py
@@ -5,7 +5,11 @@ import pathlib
 
 import pytest
 
-from test_unstructured.unit_utils import assert_round_trips_through_JSON, example_doc_path
+from test_unstructured.unit_utils import (
+    assert_round_trips_through_JSON,
+    example_doc_path,
+    parse_optional_datetime,
+)
 from unstructured.chunking.title import chunk_by_title
 from unstructured.documents.elements import (
     ElementMetadata,
@@ -323,7 +327,7 @@ def test_partition_email_from_filename_has_metadata():
         ).to_dict()
     )
     expected_dt = datetime.datetime.fromisoformat("2022-12-16T17:04:16-05:00")
-    assert elements[0].metadata.last_modified_datetime == expected_dt
+    assert parse_optional_datetime(elements[0].metadata.last_modified) == expected_dt
     for element in elements:
         assert element.metadata.filename == "fake-email.eml"
 
@@ -399,7 +403,7 @@ def test_partition_email_still_works_with_no_content():
 def test_partition_email_from_filename_exclude_metadata():
     filename = os.path.join(EXAMPLE_DOCS_DIRECTORY, "fake-email-header.eml")
     elements = partition_email(filename=filename, include_metadata=False)
-    assert elements[0].metadata.last_modified_datetime is None
+    assert parse_optional_datetime(elements[0].metadata.last_modified) is None
     assert elements[0].metadata.filetype is None
     assert elements[0].metadata.page_name is None
     assert elements[0].metadata.filename is None
@@ -413,7 +417,7 @@ def test_partition_email_from_text_file_exclude_metadata():
             content_source="text/plain",
             include_metadata=False,
         )
-    assert elements[0].metadata.last_modified_datetime is None
+    assert parse_optional_datetime(elements[0].metadata.last_modified) is None
     assert elements[0].metadata.filetype is None
     assert elements[0].metadata.page_name is None
     assert elements[0].metadata.filename is None
@@ -423,7 +427,7 @@ def test_partition_email_from_file_exclude_metadata():
     filename = os.path.join(EXAMPLE_DOCS_DIRECTORY, "fake-email.eml")
     with open(filename) as f:
         elements = partition_email(file=f, include_metadata=False)
-    assert elements[0].metadata.last_modified_datetime is None
+    assert parse_optional_datetime(elements[0].metadata.last_modified) is None
     assert elements[0].metadata.filetype is None
     assert elements[0].metadata.page_name is None
     assert elements[0].metadata.filename is None
