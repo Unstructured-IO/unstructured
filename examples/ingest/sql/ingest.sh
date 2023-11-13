7839f76e73fc85b84e18488c722c3252f1ca54c9
rvztz <revazqueztuz@gmail.com>
2023-11-13 04:17:13 -0600

    fixes typo on ingest example for sql

--- examples/ingest/sql/ingest.sh
+++ examples/ingest/sql/ingest.sh
@@ -2,7 +2,7 @@
 
 # Uploads the structured output of the files within the given S3 path.
 
-# Structured outputs are stored in azure-ingest-output/
+# Structured outputs are stored in a PostgreSQL instance/
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"/../../.. || exit 1
