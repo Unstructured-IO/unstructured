7f1ce9aaa080c69e7fafeea25c1ca7f41e83400e
christinestraub <christinemstraub@gmail.com>
2023-11-14 11:46:36 -0800

    refactor: add `_process_uncategorized_text_elements()`

--- unstructured/partition/pdf.py
+++ unstructured/partition/pdf.py
@@ -280,7 +280,7 @@ def partition_pdf_or_image(
         # NOTE(robinson): Catches a UserWarning that occurs when detectron is called
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
-            _layout_elements = _partition_pdf_or_image_local(
+            elements = _partition_pdf_or_image_local(
                 filename=filename,
                 file=spooled_to_bytes_io_if_needed(file),
                 is_image=is_image,
@@ -292,14 +292,7 @@ def partition_pdf_or_image(
                 image_output_dir_path=image_output_dir_path,
                 **kwargs,
             )
-            layout_elements = []
-            for el in _layout_elements:
-                if hasattr(el, "category") and el.category == ElementType.UNCATEGORIZED_TEXT:
-                    new_el = element_from_text(cast(Text, el).text)
-                    new_el.metadata = el.metadata
-                else:
-                    new_el = el
-                layout_elements.append(new_el)
+            out_elements = _process_uncategorized_text_elements(elements)
 
     elif strategy == PDFStrategy.FAST:
         return extracted_elements
@@ -307,7 +300,7 @@ def partition_pdf_or_image(
     elif strategy == PDFStrategy.OCR_ONLY:
         # NOTE(robinson): Catches file conversion warnings when running with PDFs
         with warnings.catch_warnings():
-            _layout_elements = _partition_pdf_or_image_with_ocr(
+            elements = _partition_pdf_or_image_with_ocr(
                 filename=filename,
                 file=file,
                 include_page_breaks=include_page_breaks,
@@ -316,17 +309,26 @@ def partition_pdf_or_image(
                 metadata_last_modified=metadata_last_modified or last_modification_date,
                 **kwargs,
             )
+            out_elements = _process_uncategorized_text_elements(elements)
 
-            layout_elements = []
-            for el in _layout_elements:
-                if hasattr(el, "category") and el.category == ElementType.UNCATEGORIZED_TEXT:
-                    new_el = element_from_text(cast(Text, el).text)
-                    new_el.metadata = el.metadata
-                else:
-                    new_el = el
-                layout_elements.append(new_el)
+    return out_elements
 
-    return layout_elements
+
+def _process_uncategorized_text_elements(elements: List[Element]):
+    """Processes a list of elements, creating a new list where elements with the
+    category `UncategorizedText` are replaced with corresponding
+    elements created from their text content."""
+
+    out_elements = []
+    for el in elements:
+        if hasattr(el, "category") and el.category == ElementType.UNCATEGORIZED_TEXT:
+            new_el = element_from_text(cast(Text, el).text)
+            new_el.metadata = el.metadata
+        else:
+            new_el = el
+        out_elements.append(new_el)
+
+    return out_elements
 
 
 @requires_dependencies("unstructured_inference")
