e9799c319387c6992ea1edd263e4114262eb39c6
cragwolfe <crag@unstructured.io>
2023-11-15 12:23:33 -0800

    refactor evaluation-ingest-cp.sh

--- test_unstructured_ingest/evaluation-ingest-cp.sh
+++ test_unstructured_ingest/evaluation-ingest-cp.sh
@@ -1,40 +1,22 @@
 #!/usr/bin/env bash
 
-set -e
+set -eu
 
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 cd "$SCRIPT_DIR"/.. || exit 1
 
-OUTPUT_DIR=$1
-OUTPUT_FOLDER_NAME=$2
+EXPECTED_OUTPUTS_DIR=$1
+CONNECTOR_TYPE=$2
 
-function walk_dir() {
-  local directory=$1
-  local -a walkdir=()
+EVAL_OUTPUT_ROOT=${EVAL_OUTPUT_ROOT:-$SCRIPT_DIR}
+EVAL_OUTPUT_DIR=$EVAL_OUTPUT_ROOT/structured-output-eval/$CONNECTOR_TYPE
 
-  while IFS= read -r -d '' file; do
-    walkdir+=("$file")
-  done < <(find "$directory" -type f -name "*.json" -print0 | sed -z "s|${directory}/||")
+mkdir -p "$EVAL_OUTPUT_DIR"
 
-  echo "${walkdir[@]}"
-}
-
-structured_outputs=()
-while IFS= read -r -d $'\0' line; do
-    structured_outputs+=("$line")
-done < <(walk_dir "$OUTPUT_DIR")
-
-OUTPUT_ROOT=${OUTPUT_ROOT:-$SCRIPT_DIR}
-CP_DIR=$OUTPUT_ROOT/structured-output-eval/$OUTPUT_FOLDER_NAME
-mkdir -p "$CP_DIR"
-
-selected_outputs=$(cat "$SCRIPT_DIR/metrics/metrics-json-manifest.txt")
-
-# If structured output file in this connector's outputs match the
-# selected outputs in the txt file, copy to the destination
-for file in "${structured_outputs[@]}"; do
-  if [[ "${selected_outputs[*]}" =~ $(basename "$file") ]] ; then
-    echo "--- Copying $OUTPUT_DIR/$file to $CP_DIR/$file ---"
-    cp -n "$OUTPUT_DIR/$file" "$CP_DIR"
+while IFS= read -r json_filename
+do
+  if find "$EXPECTED_OUTPUTS_DIR" -name "$json_filename" -print -quit | grep -q . ; then
+      echo "evaluation: copying $json_filename to $EVAL_OUTPUT_DIR"
+      find "$EXPECTED_OUTPUTS_DIR" -name "$json_filename" -exec cp {} "$EVAL_OUTPUT_DIR" \;
   fi
-done
+done < "$SCRIPT_DIR/metrics/metrics-json-manifest.txt"
