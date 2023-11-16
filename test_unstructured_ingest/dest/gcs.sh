ead2a7f1eb2ceaf52f9b124144df836a70b4ebe8
Roman Isecke <136338424+rbiseck3@users.noreply.github.com>
2023-11-16 12:13:46 -0800

    drop cloud cli deps (#2088)
    ### Description
    To not require additional dependencies on cloud-related CLIs (i.e.
    gcloud and az), using python and the existing dependencies already used
    to run out code to interact with those providers for overhead work
    associated with destination ingest tests.

--- test_unstructured_ingest/dest/gcs.sh
+++ test_unstructured_ingest/dest/gcs.sh
@@ -10,7 +10,10 @@ OUTPUT_ROOT=${OUTPUT_ROOT:-$SCRIPT_DIR}
 OUTPUT_DIR=$OUTPUT_ROOT/structured-output/$OUTPUT_FOLDER_NAME
 WORK_DIR=$OUTPUT_ROOT/workdir/$OUTPUT_FOLDER_NAME
 max_processes=${MAX_PROCESSES:=$(python3 -c "import os; print(os.cpu_count())")}
-DESTINATION_GCS="gs://utic-test-ingest-fixtures-output/$(uuidgen)"
+BUCKET="utic-test-ingest-fixtures-output"
+DIRECTORY=$(uuidgen)
+DIRECTORY="test"
+DESTINATION_GCS="gs://$BUCKET/$DIRECTORY"
 CI=${CI:-"false"}
 
 if [ -z "$GCP_INGEST_SERVICE_KEY" ]; then
@@ -31,10 +34,10 @@ function cleanup() {
     cleanup_dir "$DOWNLOAD_DIR"
   fi
 
-  if gcloud storage ls "$DESTINATION_GCS"; then
-    echo "deleting $DESTINATION_GCS"
-    gcloud storage rm --recursive "$DESTINATION_GCS"
-  fi
+  python "$SCRIPT_DIR"/python/test-gcs-output.py down \
+  --service-account-file "$GCP_INGEST_SERVICE_KEY_FILE" \
+  --bucket "$BUCKET" \
+  --blob-path "$DIRECTORY"
 
 }
 
@@ -55,9 +58,8 @@ PYTHONPATH=${PYTHONPATH:-.} "$RUN_SCRIPT" \
     --remote-url "$DESTINATION_GCS"
 
 # Simply check the number of files uploaded
-expected_num_files=1
-num_files_in_gcs=$(gcloud storage ls "$DESTINATION_GCS"/example-docs/ | wc -l )
-if [ "$num_files_in_gcs" -ne "$expected_num_files" ]; then
-    echo "Expected $expected_num_files files to be uploaded to gcs, but found $num_files_in_gcs files."
-    exit 1
-fi
+python "$SCRIPT_DIR"/python/test-gcs-output.py check \
+--expected-files 1 \
+--service-account-file "$GCP_INGEST_SERVICE_KEY_FILE" \
+--bucket "$BUCKET" \
+--blob-path "$DIRECTORY"
