ae4bd9724f18eac1d6ddfa7dd41a4b1086f84b9e
Steve Canny <stcanny@gmail.com>
2023-11-17 20:05:41 -0800

    fix(html): text_as_html has whitespace padding
    The current implementation uses `tabulate` to form the `.text_as_html`
    metadata for an `HTMLTable` element. Because it is build for interactive
    use, `tabulate` inserts padding such that columns align visually.
    
    This whitespace is undesireable for downstream purposes where
    compactness is most important.
    
    Add a simple HTML table generator that conforms to our needs and use it
    instead of `tabulate`.

--- unstructured/utils.py
+++ unstructured/utils.py
@@ -1,4 +1,5 @@
 import functools
+import html
 import importlib
 import json
 import os
@@ -17,6 +18,7 @@ from typing import (
     Iterator,
     List,
     Optional,
+    Sequence,
     Tuple,
     TypeVar,
     Union,
@@ -35,6 +37,36 @@ DATE_FORMATS = ("%Y-%m-%d", "%Y-%m-%dT%H:%M:%S", "%Y-%m-%d+%H:%M:%S", "%Y-%m-%dT
 
 _T = TypeVar("_T")
 _P = ParamSpec("_P")
+
+
+def htmlify_matrix_of_cell_texts(matrix: Sequence[Sequence[str]]) -> str:
+    """Form an HTML table from `M`.
+
+    Character overhead is minimized:
+    - No whitespace padding is added for human readability
+    - No newlines ("\n") are added
+    - No `<thead>`, `<tbody>`, or `<tfoot>` elements are used; we can't tell where those might be
+      semantically appropriate anyway so at best they would consume unnecessary space and at worst
+      would be misleading.
+    """
+
+    def iter_trs(rows_of_cell_strs: Sequence[Sequence[str]]) -> Iterator[str]:
+        for row_cell_strs in rows_of_cell_strs:
+            # -- suppress emission of rows with no cells --
+            if not row_cell_strs:
+                continue
+            yield f"<tr>{''.join(iter_tds(row_cell_strs))}</tr>"
+
+    def iter_tds(row_cell_strs: Sequence[str]) -> Iterator[str]:
+        for s in row_cell_strs:
+            # -- take care of things like '<' and '>' in the text --
+            s = html.escape(s)
+            # -- substitute <br/> elements for line-feeds in the text --
+            s = "<br/>".join(s.split("\n"))
+            # -- strip leading and trailing whitespace, wrap it up and go --
+            yield f"<td>{s.strip()}</td>"
+
+    return f"<table>{''.join(iter_trs(matrix))}</table>" if matrix else ""
 
 
 class lazyproperty(Generic[_T]):
