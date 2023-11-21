433c3889dcb00ab70c1fcb6044ff050211adbeb6
Klaijan <klaijan@unstructured.io>
2023-11-21 15:04:30 -0500

    ci: reorganize eval output folders and add azure to matrix test (#2093)
    **Summary**
    The CI workflow for evaluation previously saved the metric outputs to
    the `metrics/` folder. Currently structured in subfolders e.g.
    `metrics/text-extraction` `metrics/element-type` for the folder clean up
    purpose.
    
    Additionally, Azure connector is also added to
    `full_python_matrix_tests` in this PR.
    
    ---------
    
    Co-authored-by: ryannikolaidis <1208590+ryannikolaidis@users.noreply.github.com>
    Co-authored-by: Klaijan <Klaijan@users.noreply.github.com>

--- test_unstructured_ingest/evaluation-metrics.sh
+++ test_unstructured_ingest/evaluation-metrics.sh
@@ -28,12 +28,11 @@ SOURCE_DIR=$OUTPUT_ROOT/gold-standard/$FOLDER_NAME
 mkdir -p "$SOURCE_DIR"
 aws s3 cp "s3://$BUCKET_NAME/$FOLDER_NAME" "$SOURCE_DIR" --recursive --no-sign-request --region us-east-2
 
-EXPORT_DIR=$OUTPUT_ROOT/metrics-tmp
+EXPORT_DIR=$OUTPUT_ROOT/metrics-tmp/$EVAL_NAME
 
 # shellcheck disable=SC1091
 source "$SCRIPT_DIR"/cleanup.sh
 function cleanup() {
-  cleanup_dir "$OUTPUT_DIR"
   cleanup_dir "$SOURCE_DIR"
 }
 trap cleanup EXIT
@@ -67,4 +66,4 @@ PYTHONPATH=. ./unstructured/ingest/evaluate.py \
     $METRIC_STRATEGY "${output_args[@]}" "${source_args[@]}" \
     --export_dir "$EXPORT_DIR"
 
-"$SCRIPT_DIR"/check-diff-evaluation-metrics.sh
+"$SCRIPT_DIR"/check-diff-evaluation-metrics.sh "$EVAL_NAME"
