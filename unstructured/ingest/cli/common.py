4b43469727d795ce8e1f8fbb07508b774b126ef0
Roman Isecke <roman@unstructured.io>
2023-11-28 09:43:49 -0500

    Create a more robust way to hide sensitive data when logging fields

--- unstructured/ingest/cli/common.py
+++ unstructured/ingest/cli/common.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from typing import Optional
 
@@ -58,22 +59,34 @@ def run_init_checks(
         )
 
 
-def log_options(options: dict, verbose=False):
-    ingest_log_streaming_init(logging.DEBUG if verbose else logging.INFO)
+def hide_sensitive_fields(data: dict) -> dict:
     sensitive_fields = [
         "account_name",
-        "account_key",
-        "api_key",
-        "token",
         "client_id",
-        "client_cred",
     ]
-    options_to_log = options.copy()
-    options_to_log.update(
-        {
-            k: "*******"
-            for k, v in options_to_log.items()
-            if k in sensitive_fields and v is not None
-        },
-    )
+    sensitive_triggers = ["key", "cred", "token"]
+    new_data = data.copy()
+    for k, v in new_data.items():
+        if (
+            any([s in k.lower() for s in sensitive_triggers])  # noqa: C419
+            or k.lower() in sensitive_fields
+        ):
+            new_data[k] = "*******"
+        if isinstance(v, dict):
+            new_data[k] = hide_sensitive_fields(v)
+        if isinstance(v, str):
+            try:
+                json_data = json.loads(v)
+                if isinstance(json_data, dict):
+                    updated_data = hide_sensitive_fields(json_data)
+                    new_data[k] = json.dumps(updated_data)
+            except json.JSONDecodeError:
+                pass
+
+    return new_data
+
+
+def log_options(options: dict, verbose=False):
+    ingest_log_streaming_init(logging.DEBUG if verbose else logging.INFO)
+    options_to_log = hide_sensitive_fields(options)
     logger.debug(f"options: {options_to_log}")
