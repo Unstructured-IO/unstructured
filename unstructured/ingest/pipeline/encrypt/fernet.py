50dd007dabdb6b65e77b302d919a0d474dbab6a0
Roman Isecke <roman@unstructured.io>
2023-11-29 14:45:54 -0500

    use the encoding set when encoding/decoding the bytes used by fernet

--- unstructured/ingest/pipeline/encrypt/fernet.py
+++ unstructured/ingest/pipeline/encrypt/fernet.py
@@ -7,6 +7,11 @@ from unstructured.ingest.pipeline.encrypt.interfaces import Encryption
 
 @dataclass
 class FernetEncryption(Encryption):
+    """
+    To make it easier to work with strings rather than bytes, each method is
+    responsible for encoding/decoding the bytes that the fernet library expects.
+    """
+
     key: str
     encoding: str = "UTF-8"
 
@@ -14,7 +19,7 @@ class FernetEncryption(Encryption):
         self.fernet = Fernet(self.key)
 
     def encrypt(self, s: str) -> str:
-        return self.fernet.encrypt(s.encode()).decode()
+        return self.fernet.encrypt(s.encode(encoding=self.encoding)).decode(encoding=self.encoding)
 
     def decrypt(self, s: str) -> str:
-        return self.fernet.decrypt(s.encode()).decode()
+        return self.fernet.decrypt(s.encode(encoding=self.encoding)).decode(encoding=self.encoding)
