0a93ab8019563e75ff57886374b54049ae21c9d4
Roman Isecke <roman@unstructured.io>
2023-11-29 16:30:28 -0500

    support dataclass from 3.8

--- unstructured/ingest/enhanced_dataclass/dataclasses.py
+++ unstructured/ingest/enhanced_dataclass/dataclasses.py
@@ -4,43 +4,29 @@ from unstructured.ingest.enhanced_dataclass.core import _recursive_repr
 
 
 class EnhancedField(Field):
-    __slots__ = (
-        "name",
-        "type",
-        "default",
-        "default_factory",
-        "repr",
-        "hash",
-        "init",
-        "compare",
-        "metadata",
-        "kw_only",
-        "sensitive",
-        "_field_type",  # Private: not to be used by user code.
-    )
-
     def __init__(self, *args, sensitive=False):
         super().__init__(*args)
         self.sensitive = sensitive
 
     @_recursive_repr
     def __repr__(self):
-        return (
-            "Field("
-            f"name={self.name!r},"
-            f"type={self.type!r},"
-            f"default={self.default!r},"
-            f"default_factory={self.default_factory!r},"
-            f"init={self.init!r},"
-            f"repr={self.repr!r},"
-            f"hash={self.hash!r},"
-            f"compare={self.compare!r},"
-            f"metadata={self.metadata!r},"
-            f"kw_only={self.kw_only!r},"
-            f"sensitive={self.sensitive!r},"
-            f"_field_type={self._field_type}"
-            ")"
-        )
+        # Support for kw_only added in 3.10, to support as low as 3.8, need to dynamically map
+        fields_array = [
+            f"name={self.name!r}",
+            f"type={self.type!r}",
+            f"default={self.default!r}",
+            f"default_factory={self.default_factory!r}",
+            f"init={self.init!r}",
+            f"repr={self.repr!r}",
+            f"hash={self.hash!r}",
+            f"compare={self.compare!r}",
+            f"metadata={self.metadata!r}",
+            f"sensitive={self.sensitive!r}",
+            f"_field_type={self._field_type}",
+        ]
+        if kw_only := getattr(self, "kw_only"):
+            fields_array.append(f"kw_only={kw_only!r}")
+        return "Field({})".format(",".join(fields_array))
 
 
 def enhanced_field(
@@ -57,6 +43,8 @@ def enhanced_field(
 ):
     if default is not MISSING and default_factory is not MISSING:
         raise ValueError("cannot specify both default and default_factory")
-    return EnhancedField(
-        default, default_factory, init, repr, hash, compare, metadata, kw_only, sensitive=sensitive
-    )
+    args = [default, default_factory, init, repr, hash, compare, metadata]
+    # Support for kw_only added in 3.10, to support as low as 3.8, need to dynamically map
+    if "kw_only" in EnhancedField.__slots__:
+        args.append(kw_only)
+    return EnhancedField(*args, sensitive=sensitive)
