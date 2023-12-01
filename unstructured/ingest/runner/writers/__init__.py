8a9ef0347c1e31b66c3bc83264b16b5b90ebcf25
Roman Isecke <roman@unstructured.io>
2023-12-01 11:12:49 -0500

    migrate azure cognitive search

--- unstructured/ingest/runner/writers/__init__.py
+++ unstructured/ingest/runner/writers/__init__.py
@@ -1,11 +1,12 @@
 import typing as t
 
 from .azure import AzureWriter
+from .azure_cognitive_search import AzureCognitiveSearchWriter
 from .base_writer import Writer
 
 writer_map: t.Dict[str, t.Type[Writer]] = {
     "azure": AzureWriter,
-    # "azure_cognitive_search": azure_cognitive_search_writer,
+    "azure_cognitive_search": AzureCognitiveSearchWriter,
     # "box": box_writer,
     # "delta_table": delta_table_writer,
     # "dropbox": dropbox_writer,
--- unstructured/ingest/runner/writers/azure_cognitive_search.py
+++ unstructured/ingest/runner/writers/azure_cognitive_search.py
@@ -1,24 +1,27 @@
+import typing as t
+from dataclasses import dataclass
+
 from unstructured.ingest.interfaces import BaseDestinationConnector
+from unstructured.ingest.runner.writers.base_writer import Writer
 
-
-def azure_cognitive_search_writer(
-    endpoint: str,
-    key: str,
-    index: str,
-    **kwargs,
-) -> BaseDestinationConnector:
+if t.TYPE_CHECKING:
     from unstructured.ingest.connector.azure_cognitive_search import (
-        AzureCognitiveSearchDestinationConnector,
         AzureCognitiveSearchWriteConfig,
         SimpleAzureCognitiveSearchStorageConfig,
     )
 
-    return AzureCognitiveSearchDestinationConnector(
-        write_config=AzureCognitiveSearchWriteConfig(
-            index=index,
-        ),
-        connector_config=SimpleAzureCognitiveSearchStorageConfig(
-            endpoint=endpoint,
-            key=key,
-        ),
-    )
+
+@dataclass
+class AzureCognitiveSearchWriter(Writer):
+    connector_config: t.Optional["SimpleAzureCognitiveSearchStorageConfig"] = None
+    write_config: t.Optional["AzureCognitiveSearchWriteConfig"] = None
+
+    def get_connector(self, overwrite: bool = False, **kwargs) -> BaseDestinationConnector:
+        from unstructured.ingest.connector.azure_cognitive_search import (
+            AzureCognitiveSearchDestinationConnector,
+        )
+
+        return AzureCognitiveSearchDestinationConnector(
+            write_config=self.write_config,
+            connector_config=self.connector_config,
+        )
