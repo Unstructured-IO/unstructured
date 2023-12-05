1f9a44b775924ca697cb378bb14763d91f02cfc2
rvztz <revazqueztuz@gmail.com>
2023-12-04 19:18:37 -0600

    Standarized conform_dict method

--- unstructured/ingest/connector/sql/connector.py
+++ unstructured/ingest/connector/sql/connector.py
@@ -10,6 +10,7 @@ from unstructured.ingest.interfaces import (
     WriteConfig,
 )
 from unstructured.ingest.logger import logger
+from unstructured.ingest.utils.conform import ConformDict
 from unstructured.utils import requires_dependencies
 
 from .schema import (
@@ -119,7 +120,6 @@ class SqlDestinationConnector(BaseDestinationConnector):
         """
         Updates the element dictionary to conform to the sql schema
         """
-        from datetime import datetime
 
         data["id"] = str(uuid.uuid4())
         data["metadata_id"] = None
@@ -140,64 +140,8 @@ class SqlDestinationConnector(BaseDestinationConnector):
                 data["metadata"]["coordinates"]["id"] = coordinates_id
                 data["coordinates_id"] = coordinates_id
 
-        # Dict as string formatting
-        if record_locator := data.get("metadata", {}).get("data_source", {}).get("record_locator"):
-            # Explicit casting otherwise fails schema type checking
-            data["metadata"]["data_source"]["record_locator"] = str(json.dumps(record_locator))
-
-        # Array of items as string formatting
-        if (embeddings := data.get("embeddings")) and (
-            self.connector_config.db_name != "postgresql"
-        ):
-            data["embeddings"] = str(json.dumps(embeddings))
-
-        if points := data.get("metadata", {}).get("coordinates", {}).get("points"):
-            data["metadata"]["coordinates"]["points"] = str(json.dumps(points))
-
-        if links := data.get("metadata", {}).get("links", {}):
-            data["metadata"]["links"] = str(json.dumps(links))
-
-        if permissions_data := (
-            data.get("metadata", {}).get("data_source", {}).get("permissions_data")
-        ):
-            data["metadata"]["data_source"]["permissions_data"] = json.dumps(permissions_data)
-
-        if link_texts := data.get("metadata", {}).get("link_texts", {}):
-            data["metadata"]["link_texts"] = str(json.dumps(link_texts))
-
-        if sent_from := data.get("metadata", {}).get("sent_from", {}):
-            data["metadata"]["sent_from"] = str(json.dumps(sent_from))
-
-        if sent_to := data.get("metadata", {}).get("sent_to", {}):
-            data["metadata"]["sent_to"] = str(json.dumps(sent_to))
-
-        if emphasized_text_contents := data.get("metadata", {}).get("emphasized_text_contents", {}):
-            data["metadata"]["emphasized_text_contents"] = str(json.dumps(emphasized_text_contents))
-
-        # Datetime formatting
-        if date_created := data.get("metadata", {}).get("data_source", {}).get("date_created"):
-            data["metadata"]["data_source"]["date_created"] = datetime.fromisoformat(date_created)
-
-        if date_modified := data.get("metadata", {}).get("data_source", {}).get("date_modified"):
-            data["metadata"]["data_source"]["date_modified"] = datetime.fromisoformat(date_modified)
-
-        if date_processed := data.get("metadata", {}).get("data_source", {}).get("date_processed"):
-            data["metadata"]["data_source"]["date_processed"] = datetime.fromisoformat(
-                date_processed
-            )
-
-        if last_modified := data.get("metadata", {}).get("last_modified", {}):
-            data["metadata"]["last_modified"] = datetime.fromisoformat(last_modified)
-
-        # String casting
-        if version := data.get("metadata", {}).get("data_source", {}).get("version"):
-            data["metadata"]["data_source"]["version"] = str(version)
-
-        if page_number := data.get("metadata", {}).get("page_number"):
-            data["metadata"]["page_number"] = str(page_number)
-
-        if regex_metadata := data.get("metadata", {}).get("regex_metadata"):
-            data["metadata"]["regex_metadata"] = str(json.dumps(regex_metadata))
+        conform_instance = ConformDict()
+        data = conform_instance.conform(data)
 
         data_source = data.get("metadata", {}).pop("data_source", None)
         coordinates = data.get("metadata", {}).pop("coordinates", None)
--- unstructured/ingest/connector/weaviate.py
+++ unstructured/ingest/connector/weaviate.py
@@ -10,6 +10,7 @@ from unstructured.ingest.interfaces import (
     WriteConfig,
 )
 from unstructured.ingest.logger import logger
+from unstructured.ingest.utils.conform import ConformDict
 from unstructured.utils import requires_dependencies
 
 if t.TYPE_CHECKING:
@@ -92,57 +93,8 @@ class WeaviateDestinationConnector(BaseDestinationConnector):
         """
         Updates the element dictionary to conform to the Weaviate schema
         """
-        from dateutil import parser
-
-        # Dict as string formatting
-        if record_locator := data.get("metadata", {}).get("data_source", {}).get("record_locator"):
-            # Explicit casting otherwise fails schema type checking
-            data["metadata"]["data_source"]["record_locator"] = str(json.dumps(record_locator))
-
-        # Array of items as string formatting
-        if points := data.get("metadata", {}).get("coordinates", {}).get("points"):
-            data["metadata"]["coordinates"]["points"] = str(json.dumps(points))
-
-        if links := data.get("metadata", {}).get("links", {}):
-            data["metadata"]["links"] = str(json.dumps(links))
-
-        if permissions_data := (
-            data.get("metadata", {}).get("data_source", {}).get("permissions_data")
-        ):
-            data["metadata"]["data_source"]["permissions_data"] = json.dumps(permissions_data)
-
-        # Datetime formatting
-        if date_created := data.get("metadata", {}).get("data_source", {}).get("date_created"):
-            data["metadata"]["data_source"]["date_created"] = parser.parse(date_created).strftime(
-                "%Y-%m-%dT%H:%M:%S.%fZ",
-            )
-
-        if date_modified := data.get("metadata", {}).get("data_source", {}).get("date_modified"):
-            data["metadata"]["data_source"]["date_modified"] = parser.parse(date_modified).strftime(
-                "%Y-%m-%dT%H:%M:%S.%fZ",
-            )
-
-        if date_processed := data.get("metadata", {}).get("data_source", {}).get("date_processed"):
-            data["metadata"]["data_source"]["date_processed"] = parser.parse(
-                date_processed
-            ).strftime(
-                "%Y-%m-%dT%H:%M:%S.%fZ",
-            )
-
-        if last_modified := data.get("metadata", {}).get("last_modified", {}):
-            data["metadata"]["last_modified"] = parser.parse(last_modified).strftime(
-                "%Y-%m-%dT%H:%M:%S.%fZ",
-            )
-
-        # String casting
-        if version := data.get("metadata", {}).get("data_source", {}).get("version"):
-            data["metadata"]["data_source"]["version"] = str(version)
-
-        if page_number := data.get("metadata", {}).get("page_number"):
-            data["metadata"]["page_number"] = str(page_number)
-
-        if regex_metadata := data.get("metadata", {}).get("regex_metadata"):
-            data["metadata"]["regex_metadata"] = str(json.dumps(regex_metadata))
+        conform_instance = ConformDict()
+        return conform_instance.conform(data)
 
     def write_dict(self, *args, json_list: t.List[t.Dict[str, t.Any]], **kwargs) -> None:
         logger.info(
