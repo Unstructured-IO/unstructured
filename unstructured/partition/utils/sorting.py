b09b8e889bb6286a4b7070c1657cd8331588ffda
christinestraub <christinemstraub@gmail.com>
2023-12-06 11:21:44 -0800

    feat: add utility function to sort bboxes using XY-cut algorithm

--- unstructured/partition/utils/sorting.py
+++ unstructured/partition/utils/sorting.py
@@ -1,5 +1,5 @@
 import os
-from typing import List, Tuple
+from typing import TYPE_CHECKING, Any, List, Tuple
 
 import numpy as np
 
@@ -7,6 +7,9 @@ from unstructured.documents.elements import CoordinatesMetadata, Element
 from unstructured.logger import trace_logger
 from unstructured.partition.utils.constants import SORT_MODE_BASIC, SORT_MODE_XY_CUT
 from unstructured.partition.utils.xycut import recursive_xy_cut, recursive_xy_cut_swapped
+
+if TYPE_CHECKING:
+    from unstructured_inference.inference.elements import TextRegion
 
 
 def coordinates_to_bbox(coordinates: CoordinatesMetadata) -> Tuple[int, int, int, int]:
@@ -67,6 +70,24 @@ def coord_has_valid_points(coordinates: CoordinatesMetadata) -> bool:
             return False
         try:
             if point[0] < 0 or point[1] < 0:
+                return False
+        except TypeError:
+            return False
+    return True
+
+
+def bbox_is_valid(bbox: Any) -> bool:
+    """
+    Verifies all 4 values in a bounding box exist and are positive.
+    """
+
+    if not bbox:
+        return False
+    if len(bbox) != 4:
+        return False
+    for v in bbox:
+        try:
+            if v < 0:
                 return False
         except TypeError:
             return False
@@ -163,3 +184,36 @@ def sort_page_elements(
         sorted_page_elements = page_elements
 
     return sorted_page_elements
+
+
+def sort_bboxes_by_xy_cut(
+    bboxes,
+    shrink_factor: float = 0.9,
+    xy_cut_primary_direction: str = "x",
+):
+    """Sort bounding boxes using XY-cut algorithm."""
+
+    shrink_factor = float(
+        os.environ.get("UNSTRUCTURED_XY_CUT_BBOX_SHRINK_FACTOR", shrink_factor),
+    )
+
+    xy_cut_primary_direction = os.environ.get(
+        "UNSTRUCTURED_XY_CUT_PRIMARY_DIRECTION",
+        xy_cut_primary_direction,
+    )
+
+    shrunken_bboxes = []
+    for bbox in bboxes:
+        shrunken_bbox = shrink_bbox(bbox, shrink_factor)
+        shrunken_bboxes.append(shrunken_bbox)
+
+    res: List[int] = []
+    xy_cut_sorting_func = (
+        recursive_xy_cut_swapped if xy_cut_primary_direction == "x" else recursive_xy_cut
+    )
+    xy_cut_sorting_func(
+        np.asarray(shrunken_bboxes).astype(int),
+        np.arange(len(shrunken_bboxes)),
+        res,
+    )
+    return res
