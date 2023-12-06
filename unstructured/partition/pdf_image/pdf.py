48c061071a1b6f933e3b3d37c6d772c93791c660
coniferish <johnjennings702@gmail.com>
2023-12-06 10:28:20 -0600

    remove redundant init of coordinate_system

--- unstructured/partition/pdf_image/pdf.py
+++ unstructured/partition/pdf_image/pdf.py
@@ -695,7 +695,6 @@ def _process_pdfminer_pages(
                     page_elements.append(element)
         list_item = 0
         updated_page_elements = []  # type: ignore
-        coordinate_system = PixelSpace(width=width, height=height)
         for page_element in page_elements:
             if isinstance(page_element, ListItem):
                 list_item += 1
