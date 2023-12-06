bb2bf1bb0495f3fee4960ea88596dc5b093e25ae
Roman Isecke <roman@unstructured.io>
2023-12-06 15:45:10 -0500

    refactor slack

--- unstructured/ingest/runner/slack.py
+++ unstructured/ingest/runner/slack.py
@@ -6,20 +6,21 @@ from unstructured.ingest.logger import ingest_log_streaming_init, logger
 from unstructured.ingest.runner.base_runner import Runner
 from unstructured.ingest.runner.utils import update_download_dir_hash
 
+if t.TYPE_CHECKING:
+    from unstructured.ingest.connector.slack import SimpleSlackConfig
+
 
 class SlackRunner(Runner):
+    connector_config: "SimpleSlackConfig"
+
     def run(
         self,
-        channels: t.List[str],
-        token: str,
-        start_date: t.Optional[str] = None,
-        end_date: t.Optional[str] = None,
         **kwargs,
     ):
         ingest_log_streaming_init(logging.DEBUG if self.processor_config.verbose else logging.INFO)
 
         hashed_dir_name = hashlib.sha256(
-            ",".join(channels).encode("utf-8"),
+            ",".join(self.connector_config.channels).encode("utf-8"),
         )
 
         self.read_config.download_dir = update_download_dir_hash(
@@ -30,17 +31,11 @@ class SlackRunner(Runner):
         )
 
         from unstructured.ingest.connector.slack import (
-            SimpleSlackConfig,
             SlackSourceConnector,
         )
 
         source_doc_connector = SlackSourceConnector(  # type: ignore
-            connector_config=SimpleSlackConfig(
-                channels=channels,
-                token=token,
-                oldest=start_date,
-                latest=end_date,
-            ),
+            connector_config=self.connector_config,
             read_config=self.read_config,
             processor_config=self.processor_config,
         )
