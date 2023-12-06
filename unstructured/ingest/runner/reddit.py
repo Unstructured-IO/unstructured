302e08c50fe6b1fab7069c793aef5485a8e6b154
Roman Isecke <roman@unstructured.io>
2023-12-06 15:11:02 -0500

    refactor reddit

--- unstructured/ingest/runner/reddit.py
+++ unstructured/ingest/runner/reddit.py
@@ -6,22 +6,21 @@ from unstructured.ingest.logger import ingest_log_streaming_init, logger
 from unstructured.ingest.runner.base_runner import Runner
 from unstructured.ingest.runner.utils import update_download_dir_hash
 
+if t.TYPE_CHECKING:
+    from unstructured.ingest.connector.reddit import SimpleRedditConfig
+
 
 class RedditRunner(Runner):
+    connector_config: "SimpleRedditConfig"
+
     def run(
         self,
-        subreddit_name: str,
-        user_agent: str,
-        num_posts: int,
-        client_id: t.Optional[str] = None,
-        client_secret: t.Optional[str] = None,
-        search_query: t.Optional[str] = None,
         **kwargs,
     ):
         ingest_log_streaming_init(logging.DEBUG if self.processor_config.verbose else logging.INFO)
 
         hashed_dir_name = hashlib.sha256(
-            subreddit_name.encode("utf-8"),
+            self.connector_config.subreddit_name.encode("utf-8"),
         )
 
         self.read_config.download_dir = update_download_dir_hash(
@@ -33,18 +32,10 @@ class RedditRunner(Runner):
 
         from unstructured.ingest.connector.reddit import (
             RedditSourceConnector,
-            SimpleRedditConfig,
         )
 
         source_doc_connector = RedditSourceConnector(  # type: ignore
-            connector_config=SimpleRedditConfig(
-                subreddit_name=subreddit_name,
-                client_id=client_id,
-                client_secret=client_secret,
-                user_agent=user_agent,
-                search_query=search_query,
-                num_posts=num_posts,
-            ),
+            connector_config=self.connector_config,
             read_config=self.read_config,
             processor_config=self.processor_config,
         )
