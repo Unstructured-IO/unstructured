cae510b60d78647d15db0382fb33aaad3d765462
Roman Isecke <roman@unstructured.io>
2023-12-11 08:19:42 -0500

    update box runner with new approach

--- unstructured/ingest/runner/fsspec/box.py
+++ unstructured/ingest/runner/fsspec/box.py
@@ -1,7 +1,7 @@
-import logging
 import typing as t
 
-from unstructured.ingest.logger import ingest_log_streaming_init, logger
+from unstructured.ingest.interfaces import BaseSourceConnector
+from unstructured.ingest.logger import logger
 from unstructured.ingest.runner.base_runner import Runner
 from unstructured.ingest.runner.utils import update_download_dir_remote_url
 
@@ -12,12 +12,7 @@ if t.TYPE_CHECKING:
 class BoxRunner(Runner):
     connector_config: t.Optional["SimpleBoxConfig"] = None
 
-    def run(
-        self,
-        **kwargs,
-    ):
-        ingest_log_streaming_init(logging.DEBUG if self.processor_config.verbose else logging.INFO)
-
+    def update_read_config(self):
         self.read_config.download_dir = update_download_dir_remote_url(
             connector_name="box",
             read_config=self.read_config,
@@ -25,12 +20,7 @@ class BoxRunner(Runner):
             logger=logger,
         )
 
+    def get_source_connector_cls(self) -> t.Type[BaseSourceConnector]:
         from unstructured.ingest.connector.fsspec.box import BoxSourceConnector
 
-        source_doc_connector = BoxSourceConnector(  # type: ignore
-            read_config=self.read_config,
-            connector_config=self.connector_config,
-            processor_config=self.processor_config,
-        )
-
-        self.process_documents(source_doc_connector=source_doc_connector)
+        return BoxSourceConnector
