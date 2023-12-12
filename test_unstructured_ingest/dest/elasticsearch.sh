1a558af8192ff210a3458ec4e4a2e367b0999ce0
Ahmet Melek <ahmetmeleq@gmail.com>
2023-12-12 17:32:17 +0000

    shellcheck

--- test_unstructured_ingest/dest/elasticsearch.sh
+++ test_unstructured_ingest/dest/elasticsearch.sh
@@ -52,8 +52,8 @@ PYTHONPATH=. ./unstructured/ingest/main.py \
   elasticsearch \
   --hosts http://localhost:9200 \
   --index-name ingest-test-destination \
-  --username $ELASTICSEARCH_USER \
-	--password $ELASTICSEARCH_PASSWORD \
+  --username "$ELASTICSEARCH_USER" \
+	--password "$ELASTICSEARCH_PASSWORD" \
   --batch-size-bytes 15000000 \
   --num-processes "$max_processes"
 
