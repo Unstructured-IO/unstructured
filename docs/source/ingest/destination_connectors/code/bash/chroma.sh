597cc43ff014c3bbb990801fd9c410791d8f4217
potter-potter <david.potter@gmail.com>
2023-12-13 08:50:22 -0800

    shell format

--- docs/source/ingest/destination_connectors/code/bash/chroma.sh
+++ docs/source/ingest/destination_connectors/code/bash/chroma.sh
@@ -1,15 +1,17 @@
+#!/usr/bin/env bash
+
 unstructured-ingest \
-    local \
-    --input-path example-docs/book-war-and-peace-1225p.txt \
-    --output-dir local-to-pinecone \
-    --strategy fast \
-    --chunk-elements \
-    --embedding-provider <an unstructured embedding provider, ie. langchain-huggingface> \
-    --num-processes 2 \
-    --verbose \
-    --work-dir "<directory for intermediate outputs to be saved>" \
-    chroma \
-    --host "localhost" \
-    --port 8000 \
-    --collection-name "collection_name" \
-    --batch-size 80
+        local \
+        --input-path example-docs/book-war-and-peace-1p.txt \
+        --output-dir local-to-pinecone \
+        --strategy fast \
+        --chunk-elements \
+        --embedding-provider "<unstructured embedding provider, ie. langchain-huggingface>" \
+        --num-processes 2 \
+        --verbose \
+        --work-dir "<directory for intermediate outputs to be saved>" \
+        chroma \
+        --host "localhost" \
+        --port 8000 \
+        --collection-name "collection name" \
+        --batch-size 80 
