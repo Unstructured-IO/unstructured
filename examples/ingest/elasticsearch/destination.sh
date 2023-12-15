52b4ac9ba4dff1e73634923a0c98335a7b8326a7
Ahmet Melek <ahmetmeleq@gmail.com>
2023-12-15 22:06:49 +0000

    fix example

--- examples/ingest/elasticsearch/destination.sh
+++ examples/ingest/elasticsearch/destination.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-# Processes all the files from s3://utic-dev-tech-fixtures/small-pdf-set/,
-# embeds the processed documents, and writes to results to a Pinecone index.
+# Processes a the file from local, chunks, embeds, and writes the results to an Elasticsearch index.
 
-# Structured outputs are stored in s3-small-batch-output-to-pinecone/
+# Structured outputs are stored in local-to-elasticsearch/
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 cd "$SCRIPT_DIR"/../../.. || exit 1
 
-# As an example we're using the s3 source connector,
+# As an example we're using the local connector,
 # however ingesting from any supported source connector is possible.
 # shellcheck disable=2094
 PYTHONPATH=. ./unstructured/ingest/main.py \
 	local \
 	--input-path example-docs/book-war-and-peace-1225p.txt \
-	--output-dir local-to-pinecone \
+	--output-dir local-to-elasticsearch \
 	--strategy fast \
 	--chunk-elements \
 	--embedding-provider "<an unstructured embedding provider, ie. langchain-huggingface>" \
