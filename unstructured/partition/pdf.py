8036b6a7bf64a98c7548bfaaf5fea88d9f8a5c3c
coniferish <johnjennings702@gmail.com>
2023-12-18 10:40:32 -0600

    add back changes to pdf.py

--- unstructured/partition/pdf.py
+++ unstructured/partition/pdf.py
@@ -139,6 +139,7 @@ def partition_pdf(
     extract_images_in_pdf: bool = False,
     extract_element_types: Optional[List[str]] = None,
     image_output_dir_path: Optional[str] = None,
+    hi_res_model_name: Optional[str] = None,
     **kwargs,
 ) -> List[Element]:
     """Parses a pdf document into a list of interpreted elements.
@@ -174,6 +175,8 @@ def partition_pdf(
     image_output_dir_path
         If extract_images_in_pdf=True and strategy=hi_res, any detected images or tables
         will be saved in the given path
+    hi_res_model_name
+        The layout detection model used when partitioning strategy is set to `hi_res`.
     """
 
     exactly_one(filename=filename, file=file)
@@ -191,6 +194,7 @@ def partition_pdf(
         extract_images_in_pdf=extract_images_in_pdf,
         extract_element_types=extract_element_types,
         image_output_dir_path=image_output_dir_path,
+        hi_res_model_name=hi_res_model_name,
         **kwargs,
     )
 
@@ -236,13 +240,14 @@ def _partition_pdf_or_image_local(
     include_page_breaks: bool = False,
     languages: Optional[List[str]] = None,
     ocr_mode: str = OCRMode.FULL_PAGE.value,
-    model_name: Optional[str] = None,
+    model_name: Optional[str] = None,  # to be deprecated in favor of `hi_res_model_name`
     metadata_last_modified: Optional[str] = None,
     pdf_text_extractable: bool = False,
     extract_images_in_pdf: bool = False,
     extract_element_types: Optional[List[str]] = None,
     image_output_dir_path: Optional[str] = None,
     pdf_image_dpi: Optional[int] = None,
+    hi_res_model_name: Optional[str] = None,
     **kwargs,
 ) -> List[Element]:
     """Partition using package installed locally"""
@@ -265,10 +270,12 @@ def _partition_pdf_or_image_local(
 
     ocr_languages = prepare_languages_for_tesseract(languages)
 
-    model_name = model_name or default_hi_res_model(infer_table_structure)
+    hi_res_model_name = (
+        hi_res_model_name or model_name or default_hi_res_model(infer_table_structure)
+    )
     if pdf_image_dpi is None:
-        pdf_image_dpi = 300 if model_name == "chipper" else 200
-    if (pdf_image_dpi < 300) and (model_name == "chipper"):
+        pdf_image_dpi = 300 if hi_res_model_name == "chipper" else 200
+    if (pdf_image_dpi < 300) and (hi_res_model_name == "chipper"):
         logger.warning(
             "The Chipper model performs better when images are rendered with DPI >= 300 "
             f"(currently {pdf_image_dpi}).",
@@ -278,7 +285,7 @@ def _partition_pdf_or_image_local(
         inferred_document_layout = process_file_with_model(
             filename,
             is_image=is_image,
-            model_name=model_name,
+            model_name=hi_res_model_name,
             pdf_image_dpi=pdf_image_dpi,
         )
 
@@ -291,7 +298,7 @@ def _partition_pdf_or_image_local(
         else:
             merged_document_layout = inferred_document_layout
 
-        if model_name.startswith("chipper"):
+        if hi_res_model_name.startswith("chipper"):
             # NOTE(alan): We shouldn't do OCR with chipper
             final_document_layout = merged_document_layout
         else:
@@ -308,7 +315,7 @@ def _partition_pdf_or_image_local(
         inferred_document_layout = process_data_with_model(
             file,
             is_image=is_image,
-            model_name=model_name,
+            model_name=hi_res_model_name,
             pdf_image_dpi=pdf_image_dpi,
         )
         if hasattr(file, "seek"):
@@ -322,7 +329,7 @@ def _partition_pdf_or_image_local(
         else:
             merged_document_layout = inferred_document_layout
 
-        if model_name.startswith("chipper"):
+        if hi_res_model_name.startswith("chipper"):
             # NOTE(alan): We shouldn't do OCR with chipper
             final_document_layout = merged_document_layout
         else:
@@ -339,7 +346,7 @@ def _partition_pdf_or_image_local(
             )
 
     # NOTE(alan): starting with v2, chipper sorts the elements itself.
-    if model_name == "chipper":
+    if hi_res_model_name == "chipper":
         kwargs["sort_mode"] = SORT_MODE_DONT
 
     final_document_layout = clean_pdfminer_inner_elements(final_document_layout)
@@ -409,7 +416,7 @@ def _partition_pdf_or_image_local(
             ).strip()
             # NOTE(alan): with chipper there are parent elements with no text we don't want to
             # filter those out and leave the children orphaned.
-            if el.text or isinstance(el, PageBreak) or model_name.startswith("chipper"):
+            if el.text or isinstance(el, PageBreak) or hi_res_model_name.startswith("chipper"):
                 out_elements.append(cast(Element, el))
 
     return out_elements
@@ -428,6 +435,7 @@ def partition_pdf_or_image(
     extract_images_in_pdf: bool = False,
     extract_element_types: Optional[List[str]] = None,
     image_output_dir_path: Optional[str] = None,
+    hi_res_model_name: Optional[str] = None,
     **kwargs,
 ) -> List[Element]:
     """Parses a pdf or image document into a list of interpreted elements."""
@@ -489,6 +497,7 @@ def partition_pdf_or_image(
                 extract_images_in_pdf=extract_images_in_pdf,
                 extract_element_types=extract_element_types,
                 image_output_dir_path=image_output_dir_path,
+                hi_res_model_name=hi_res_model_name,
                 **kwargs,
             )
             out_elements = _process_uncategorized_text_elements(elements)
