bd8a74d686446250adb0a95333efec5a3fdd6fc0
cragwolfe <crag@unstructured.io>
2023-12-18 23:48:21 -0800

    chore: shell scripts default indent of 2 instead of 4 (#2287)
    Given the tendency for shell scripts to easily enter into a few levels
    of indentation and long line lengths, update the default to 2 spaces.

--- examples/ingest/slack/ingest.sh
+++ examples/ingest/slack/ingest.sh
@@ -15,10 +15,10 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 cd "$SCRIPT_DIR"/../../.. || exit 1
 
 PYTHONPATH=. ./unstructured/ingest/main.py \
-	slack \
-	--channels 12345678 \
-	--token 12345678 \
-	--download-dir slack-ingest-download \
-	--output-dir slack-ingest-output \
-	--start-date 2023-04-01T01:00:00-08:00 \
-	--end-date 2023-04-02
+  slack \
+  --channels 12345678 \
+  --token 12345678 \
+  --download-dir slack-ingest-download \
+  --output-dir slack-ingest-output \
+  --start-date 2023-04-01T01:00:00-08:00 \
+  --end-date 2023-04-02
--- test_unstructured_ingest/src/local-single-file-with-encoding.sh
+++ test_unstructured_ingest/src/local-single-file-with-encoding.sh
@@ -15,22 +15,22 @@ max_processes=${MAX_PROCESSES:=$(python3 -c "import os; print(os.cpu_count())")}
 source "$SCRIPT_DIR"/cleanup.sh
 # shellcheck disable=SC2317
 function cleanup() {
-	cleanup_dir "$OUTPUT_DIR"
-	cleanup_dir "$WORK_DIR"
+  cleanup_dir "$OUTPUT_DIR"
+  cleanup_dir "$WORK_DIR"
 }
 trap cleanup EXIT
 
 RUN_SCRIPT=${RUN_SCRIPT:-./unstructured/ingest/main.py}
 PYTHONPATH=${PYTHONPATH:-.} "$RUN_SCRIPT" \
-	local \
-	--num-processes "$max_processes" \
-	--metadata-exclude filename,file_directory,metadata.data_source.date_created,metadata.data_source.date_modified,metadata.data_source.date_processed,metadata.last_modified,metadata.detection_class_prob,metadata.parent_id,metadata.category_depth \
-	--output-dir "$OUTPUT_DIR" \
-	--encoding cp1252 \
-	--verbose \
-	--reprocess \
-	--input-path example-docs/fake-html-cp1252.html \
-	--work-dir "$WORK_DIR"
+  local \
+  --num-processes "$max_processes" \
+  --metadata-exclude filename,file_directory,metadata.data_source.date_created,metadata.data_source.date_modified,metadata.data_source.date_processed,metadata.last_modified,metadata.detection_class_prob,metadata.parent_id,metadata.category_depth \
+  --output-dir "$OUTPUT_DIR" \
+  --encoding cp1252 \
+  --verbose \
+  --reprocess \
+  --input-path example-docs/fake-html-cp1252.html \
+  --work-dir "$WORK_DIR"
 
 set +e
 "$SCRIPT_DIR"/check-diff-expected-output.sh $OUTPUT_FOLDER_NAME
@@ -38,8 +38,8 @@ EXIT_CODE=$?
 set -e
 
 if [ "$EXIT_CODE" -ne 0 ]; then
-	echo "The last script run exited with a non-zero exit code: $EXIT_CODE."
-	# Handle the error or exit
+  echo "The last script run exited with a non-zero exit code: $EXIT_CODE."
+  # Handle the error or exit
 fi
 
 "$SCRIPT_DIR"/evaluation-ingest-cp.sh "$OUTPUT_DIR" "$OUTPUT_FOLDER_NAME"
