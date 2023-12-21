8645bab02f329159a2af2233a96b991171832067
potter-potter <david.potter@gmail.com>
2023-12-20 17:57:12 -0800

    opensearch ingest working

--- unstructured/ingest/connector/opensearch.py
+++ unstructured/ingest/connector/opensearch.py
@@ -1,23 +1,29 @@
 import hashlib
+import json
 import typing as t
+import uuid
 from dataclasses import dataclass, field
+from itertools import chain
 from pathlib import Path
 
 from dataclasses_json.core import Json
 
 from unstructured.ingest.enhanced_dataclass import enhanced_field
-from unstructured.ingest.error import SourceConnectionError
+from unstructured.ingest.error import DestinationConnectionError, SourceConnectionError
 from unstructured.ingest.interfaces import (
     AccessConfig,
     BaseConnectorConfig,
+    BaseDestinationConnector,
     BaseIngestDocBatch,
     BaseSingleIngestDoc,
     BaseSourceConnector,
     IngestDocCleanupMixin,
     SourceConnectorCleanupMixin,
     SourceMetadata,
+    WriteConfig,
 )
 from unstructured.ingest.logger import logger
+from unstructured.ingest.utils.data_prep import generator_batching_wbytes
 from unstructured.staging.base import flatten_dict
 from unstructured.utils import requires_dependencies
 
@@ -43,7 +49,7 @@ class OpenSearchAccessConfig(AccessConfig):
         d = super().to_dict(**kwargs)
         # Update auth related fields to conform to what the SDK expects based on the
         # supported methods:
-        # https://www.elastic.co/guide/en/elasticsearch/client/python-api/current/connecting.html
+        # https://www.elastic.co/guide/en/opensearch/client/python-api/current/connecting.html
         if not self.ca_certs:
             # ES library already sets a default for this, don't want to
             # introduce data by setting it to None
@@ -67,20 +73,20 @@ class OpenSearchAccessConfig(AccessConfig):
 @dataclass
 class SimpleOpenSearchConfig(BaseConnectorConfig):
     """Connector config where:
-    url is the url to access the elasticsearch server,
+    url is the url to access the opensearch server,
     index_name is the name of the index to reach to,
     """
 
     index_name: str
     batch_size: int = 100
-    fields: t.List[str] = field(default=list)
+    fields: t.List[str] = field(default_factory=list)
     access_config: OpenSearchAccessConfig = None
 
 
 @dataclass
 class OpenSearchDocumentMeta:
     """Metadata specifying:
-    name of the elasticsearch index that is being reached to,
+    name of the opensearch index that is being reached to,
     and the id of document that is being reached to,
     """
 
@@ -174,7 +180,7 @@ class OpenSearchIngestDocBatch(BaseIngestDocBatch):
     connector_config: SimpleOpenSearchConfig
     ingest_docs: t.List[OpenSearchIngestDoc] = field(default_factory=list)
     list_of_ids: t.List[str] = field(default_factory=list)
-    registry_name: str = "opensearch"
+    registry_name: str = "opensearch_batch"
 
     def __post_init__(self):
         # Until python3.8 is deprecated, this is a limitation of dataclass inheritance
@@ -191,7 +197,7 @@ class OpenSearchIngestDocBatch(BaseIngestDocBatch):
         from opensearchpy import OpenSearch
         from opensearchpy.helpers import scan
 
-        client = OpenSearch(**self.connector_config.access_config.to_dict(apply_name_overload=False))
+        es = OpenSearch(**self.connector_config.access_config.to_dict(apply_name_overload=False))
         scan_query = {
             "_source": self.connector_config.fields,
             "version": True,
@@ -199,7 +205,7 @@ class OpenSearchIngestDocBatch(BaseIngestDocBatch):
         }
 
         result = scan(
-            client,
+            es,
             query=scan_query,
             scroll="1m",
             index=self.connector_config.index_name,
@@ -224,7 +230,8 @@ class OpenSearchIngestDocBatch(BaseIngestDocBatch):
             doc_body = doc["_source"]
             filename = ingest_doc.filename
             flattened_dict = flatten_dict(dictionary=doc_body)
-            concatenated_values = "\n".join(flattened_dict.values())
+            str_values = [str(value) for value in flattened_dict.values()]
+            concatenated_values = "\n".join(str_values)
 
             filename.parent.mkdir(parents=True, exist_ok=True)
             with open(filename, "w", encoding="utf8") as f:
@@ -299,3 +306,90 @@ class OpenSearchSourceConnector(SourceConnectorCleanupMixin, BaseSourceConnector
             )
             for batched_ids in id_batches
         ]
+
+
+@dataclass
+class OpenSearchWriteConfig(WriteConfig):
+    batch_size_bytes: int
+    num_processes: int
+
+
+@dataclass
+class OpenSearchDestinationConnector(BaseDestinationConnector):
+    write_config: OpenSearchWriteConfig
+    connector_config: SimpleOpenSearchConfig
+    _client: t.Optional["OpenSearch"] = field(init=False, default=None)
+
+    @DestinationConnectionError.wrap
+    @requires_dependencies(["opensearch"], extras="opensearch")
+    def generate_client(self) -> "OpenSearch":
+        from opensearchpy import OpenSearch
+
+        return OpenSearch(
+            **self.connector_config.access_config.to_dict(apply_name_overload=False)
+        )
+
+    @property
+    def client(self):
+        if self._client is None:
+            self._client = self.generate_client()
+        return self._client
+
+    def initialize(self):
+        _ = self.client
+
+    @DestinationConnectionError.wrap
+    def check_connection(self):
+        try:
+            assert self.client.ping()
+        except Exception as e:
+            logger.error(f"failed to validate connection: {e}", exc_info=True)
+            raise DestinationConnectionError(f"failed to validate connection: {e}")
+
+    @requires_dependencies(["opensearch"], extras="opensearch")
+    def write_dict(self, element_dicts: t.List[t.Dict[str, t.Any]]) -> None:
+        logger.info(
+            f"writing document batches to destination"
+            f" index named {self.connector_config.index_name}"
+            f" at {self.connector_config.access_config.hosts}"
+            f" with batch size (in bytes) {self.write_config.batch_size_bytes}"
+            f" with {self.write_config.num_processes} (number of) processes"
+        )
+        from opensearchpy.helpers import parallel_bulk
+
+        for batch in generator_batching_wbytes(
+            element_dicts, batch_size_limit_bytes=self.write_config.batch_size_bytes
+        ):
+            for success, info in parallel_bulk(
+                self.client, batch, thread_count=self.write_config.num_processes
+            ):
+                if not success:
+                    logger.error(
+                        "upload failed for a batch in opensearch destination connector:", info
+                    )
+
+    def conform_dict(self, element_dict):
+        return {
+            "_index": self.connector_config.index_name,
+            "_id": str(uuid.uuid4()),
+            "_source": {
+                "element_id": element_dict.pop("element_id", None),
+                "embeddings": element_dict.pop("embeddings", None),
+                "text": element_dict.pop("text", None),
+                "metadata": flatten_dict(
+                    element_dict.pop("metadata", None),
+                    separator="-",
+                ),
+            },
+        }
+
+    def write(self, docs: t.List[BaseSingleIngestDoc]) -> None:
+        def generate_element_dicts(doc):
+            with open(doc._output_filename) as json_file:
+                element_dicts_one_doc = (
+                    self.conform_dict(element_dict) for element_dict in json.load(json_file)
+                )
+                yield from element_dicts_one_doc
+
+        # We chain to unite the generators into one generator
+        self.write_dict(chain(*(generate_element_dicts(doc) for doc in docs)))
--- unstructured/ingest/connector/registry.py
+++ unstructured/ingest/connector/registry.py
@@ -27,7 +27,7 @@ from unstructured.ingest.connector.notion.connector import (
     NotionPageIngestDoc,
 )
 from unstructured.ingest.connector.onedrive import OneDriveIngestDoc
-from unstructured.ingest.connector.opensearch import OpenSearchIngestDoc
+from unstructured.ingest.connector.opensearch import (OpenSearchIngestDoc,OpenSearchIngestDocBatch)
 from unstructured.ingest.connector.outlook import OutlookIngestDoc
 from unstructured.ingest.connector.reddit import RedditIngestDoc
 from unstructured.ingest.connector.salesforce import SalesforceIngestDoc
@@ -63,6 +63,7 @@ INGEST_DOC_NAME_TO_CLASS: Dict[str, Type[EnhancedDataClassJsonMixin]] = {
     "notion_page": NotionPageIngestDoc,
     "onedrive": OneDriveIngestDoc,
     "opensearch": OpenSearchIngestDoc,
+    "opensearch_batch": OpenSearchIngestDocBatch,
     "outlook": OutlookIngestDoc,
     "reddit": RedditIngestDoc,
     "s3": S3IngestDoc,
