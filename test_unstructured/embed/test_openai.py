93443221a4bb60c9115130a9f82df69c8e9378e5
Roman Isecke <roman@unstructured.io>
2023-12-26 16:53:28 -0500

    fix unit tests

--- test_unstructured/embed/test_openai.py
+++ test_unstructured/embed/test_openai.py
@@ -7,8 +7,8 @@ def test_embed_documents_does_not_break_element_to_dict(mocker):
     mock_client = mocker.MagicMock()
     mock_client.embed_documents.return_value = [1, 2]
 
-    # Mock get_openai_client to return our mock_client
-    mocker.patch.object(OpenAIEmbeddingEncoder, "get_openai_client", return_value=mock_client)
+    # Mock create_client to return our mock_client
+    mocker.patch.object(OpenAIEmbeddingEncoder, "create_client", return_value=mock_client)
 
     encoder = OpenAIEmbeddingEncoder(config=OpenAiEmbeddingConfig(api_key="api_key"))
     elements = encoder.embed_documents(
