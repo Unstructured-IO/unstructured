cf7943b429e72be737cf5fa251122fff237d4144
coniferish <johnjennings702@gmail.com>
2023-12-29 11:25:39 -0600

    extract _get_iso639_language_object to its own function

--- unstructured/partition/lang.py
+++ unstructured/partition/lang.py
@@ -13,7 +13,7 @@ from unstructured.partition.utils.constants import (
 
 # pytesseract.get_languages(config="") only shows user installed language packs,
 # so manually include the list of all currently supported Tesseract languages
-PYTESSERACT_LANGS = [
+PYTESSERACT_LANG_CODES = [
     "afr",
     "amh",
     "ara",
@@ -152,7 +152,7 @@ def prepare_languages_for_tesseract(languages: Optional[List[str]] = ["eng"]):
     converted_languages = list(
         filter(
             lambda x: x is not None and x != "",
-            [convert_language_code_to_tesseract_code(lang) for lang in languages],
+            [_convert_language_code_to_pytesseract_lang_code(lang) for lang in languages],
         ),
     )
     # Remove duplicates from the list but keep the original order
@@ -233,64 +233,72 @@ def convert_old_ocr_languages_to_languages(ocr_languages: str):
     return ocr_languages.split(TESSERACT_LANGUAGES_SPLITTER)
 
 
-def convert_language_code_to_tesseract_code(lang: str) -> str:
+def _convert_language_code_to_pytesseract_lang_code(lang: str) -> str:
     """
-    Convert a language code to its tesseract formatted and recognized langcode(s), if supported.
+    Convert a single language code to its tesseract formatted and recognized
+    langcode(s), if supported.
     """
     # if language is already tesseract langcode, return it immediately
     # this will catch the tesseract special cases equ and osd
     # NOTE(shreya): this may catch some cases of choosing between tesseract code variants for a lang
-    if lang in PYTESSERACT_LANGS:  # 3-letter lang codes
+    if lang in PYTESSERACT_LANG_CODES:
         return lang
 
-    # get iso639 language object
+    lang_iso639 = _get_iso639_language_object(lang)
+
+    # tesseract uses 3 digit codes (639-3, 639-2b, etc) as prefixes, with suffixes for orthography
+    # use first 3 letters of tesseract codes for matching to standard codes
+    pytesseract_langs_3 = {lang[:3] for lang in PYTESSERACT_LANG_CODES}
+
+    if lang_iso639:
+        # try to match ISO 639-3 code
+        if lang_iso639.part3 in pytesseract_langs_3:
+            matched_langcodes = _get_all_tesseract_langcodes_with_prefix(lang_iso639.part3)
+            return TESSERACT_LANGUAGES_SPLITTER.join(matched_langcodes)
+
+        # try to match ISO 639-2b
+        elif lang_iso639.part2b in pytesseract_langs_3:
+            matched_langcodes = _get_all_tesseract_langcodes_with_prefix(lang_iso639.part2b)
+            return TESSERACT_LANGUAGES_SPLITTER.join(matched_langcodes)
+
+        # try to match ISO 639-2t
+        elif lang_iso639.part2t in pytesseract_langs_3:
+            matched_langcodes = _get_all_tesseract_langcodes_with_prefix(lang_iso639.part2t)
+            return TESSERACT_LANGUAGES_SPLITTER.join(matched_langcodes)
+
+        else:
+            logger.warning(f"{lang} is not a language supported by Tesseract.")
+            return ""
+
+
+def _get_iso639_language_object(lang: str) -> Union[iso639.Language, str]:
     try:
         # match other lang codes (ex. 2-letter codes)
         lang_iso639 = iso639.Language.match(lang.lower())
     except iso639.LanguageNotFoundError:
         logger.warning(f"{lang} is not a valid standard language code.")
-        return ""
-
-    # tesseract uses 3 digit codes (639-3, 639-2b, etc) as prefixes, with suffixes for orthography
-    # use first 3 letters of tesseract codes for matching to standard codes
-    pytesseract_langs_3 = {lang[:3] for lang in PYTESSERACT_LANGS}
-
-    # try to match ISO 639-3 code
-    if lang_iso639.part3 in pytesseract_langs_3:
-        matched_langcodes = _get_all_tesseract_langcodes_with_prefix(lang_iso639.part3)
-        return TESSERACT_LANGUAGES_SPLITTER.join(matched_langcodes)
-
-    # try to match ISO 639-2b
-    elif lang_iso639.part2b in pytesseract_langs_3:
-        matched_langcodes = _get_all_tesseract_langcodes_with_prefix(lang_iso639.part2b)
-        return TESSERACT_LANGUAGES_SPLITTER.join(matched_langcodes)
-
-    # try to match ISO 639-2t
-    elif lang_iso639.part2t in pytesseract_langs_3:
-        matched_langcodes = _get_all_tesseract_langcodes_with_prefix(lang_iso639.part2t)
-        return TESSERACT_LANGUAGES_SPLITTER.join(matched_langcodes)
-
-    else:
-        logger.warning(f"{lang} is not a language supported by Tesseract.")
-        return ""
+        return None
+    return lang_iso639
 
 
 def _get_all_tesseract_langcodes_with_prefix(prefix: str):
     """
     Get all matching tesseract langcodes with this prefix (may be one or multiple variants).
     """
-    return [langcode for langcode in PYTESSERACT_LANGS if langcode.startswith(prefix)]
+    return [langcode for langcode in PYTESSERACT_LANG_CODES if langcode.startswith(prefix)]
 
 
 def _convert_to_standard_langcode(lang: str) -> str:
     """
-    Convert a language code to the standard internal language code format.
+    Convert a single language or language code to the standard internal language code format.
     """
     if TESSERACT_LANGUAGES_AND_CODES.get(lang.lower()):
         lang = TESSERACT_LANGUAGES_AND_CODES.get(lang.lower())
     # convert to standard ISO 639-3 language code
-    lang_iso639 = iso639.Language.match(lang[:3].lower())
-    return lang_iso639.part3
+    lang_iso639 = _get_iso639_language_object(lang[:3])
+    if lang_iso639:
+        return lang_iso639.part3
+    return ""
 
 
 def detect_languages(
