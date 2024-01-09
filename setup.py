931d0d9094865deba86c2d1147bcd01e2e788e17
Roman Isecke <roman@unstructured.io>
2024-01-09 11:44:27 -0500

    Remove all uses of python3.8

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
     long_description_content_type="text/markdown",
     keywords="NLP PDF HTML CV XML parsing preprocessing",
     url="https://github.com/Unstructured-IO/unstructured",
-    python_requires=">=3.7.0",
+    python_requires=">=3.10.0",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
@@ -92,7 +92,6 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
