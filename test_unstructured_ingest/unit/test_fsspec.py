60fda41caee6e95570842c9ea7b4ff6e3369e894
Ryan Nikolaidis <1208590+ryannikolaidis@users.noreply.github.com>
2024-01-18 21:22:29 -0800

    cast source version to string add test to validate

---
+++ test_unstructured_ingest/unit/test_fsspec.py
@@ -0,0 +1,30 @@
+import unittest
+from unittest.mock import MagicMock, patch
+
+from fsspec import AbstractFileSystem
+
+from unstructured.ingest.connector.fsspec.fsspec import FsspecIngestDoc, SimpleFsspecConfig
+from unstructured.ingest.interfaces import ProcessorConfig, ReadConfig
+
+
+class TestFsspecIngestDoc(unittest.TestCase):
+    @patch("fsspec.get_filesystem_class")
+    def test_version_is_string(self, mock_get_filesystem_class):
+        """
+        Test that the version is a string even when the filesystem checksum is an integer.
+        """
+        # Setup
+        mock_fs = MagicMock(spec=AbstractFileSystem)
+        mock_fs.checksum.return_value = 1234567890
+        mock_fs.info.return_value = {"etag": ""}
+        mock_get_filesystem_class.return_value = lambda **kwargs: mock_fs
+        config = SimpleFsspecConfig("s3://my-bucket", access_config={})
+        doc = FsspecIngestDoc(
+            processor_config=ProcessorConfig(),
+            read_config=ReadConfig(),
+            connector_config=config,
+            remote_file_path="test.txt",
+        )
+
+        # Check the version is a string
+        self.assertIsInstance(doc.source_metadata.version, str)
