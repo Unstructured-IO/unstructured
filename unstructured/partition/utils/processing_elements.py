89f6b91bbe0d2da462f6bf5fafb2c9d4f23c13a6
Antonio Jose Jimeno Yepes <antonio.jimeno@gmail.com>
2024-01-24 18:33:32 -0800

    feat: replace `Rectangle.is_in()` with `Rectangle.is_almost_subregion_of()` when removing pdfminer (embedded) elements merged with inferred elements

--- unstructured/partition/utils/processing_elements.py
+++ unstructured/partition/utils/processing_elements.py
@@ -10,13 +10,14 @@ if TYPE_CHECKING:
 def clean_pdfminer_inner_elements(document: "DocumentLayout") -> "DocumentLayout":
     """Clean pdfminer elements from inside tables and stores them in extra_info dictionary
     with the table id as key"""
+
     defaultdict(list)
     for page in document.pages:
         tables = [e for e in page.elements if e.type == "Table"]
         for i, element in enumerate(page.elements):
             if element.source != Source.PDFMINER:
                 continue
-            element_inside_table = [element.bbox.is_in(t.bbox, error_margin=15) for t in tables]
+            element_inside_table = [element.bbox.is_almost_subregion_of(t.bbox) for t in tables]
             if sum(element_inside_table) == 1:
                 page.elements[i] = None
         page.elements = [e for e in page.elements if e]
