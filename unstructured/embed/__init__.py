8aa03169584a1b20d1c890bd66200d32166166e5
Pedro Torruella <5025399+ptorru@users.noreply.github.com>
2024-01-29 13:48:41 -0800

    adding octoai

--- unstructured/embed/__init__.py
+++ unstructured/embed/__init__.py
@@ -1,8 +1,10 @@
 from unstructured.embed.bedrock import BedrockEmbeddingEncoder
 from unstructured.embed.huggingface import HuggingFaceEmbeddingEncoder
+from unstructured.embed.octoai import OctoAIEmbeddingEncoder
 from unstructured.embed.openai import OpenAIEmbeddingEncoder
 
 EMBEDDING_PROVIDER_TO_CLASS_MAP = {
+    "langchain-octoai": OctoAIEmbeddingEncoder,
     "langchain-openai": OpenAIEmbeddingEncoder,
     "langchain-huggingface": HuggingFaceEmbeddingEncoder,
     "langchain-aws-bedrock": BedrockEmbeddingEncoder,
---
+++ unstructured/embed/octoai.py
@@ -0,0 +1,80 @@
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING, List, Optional
+
+import numpy as np
+
+from unstructured.documents.elements import (
+    Element,
+)
+from unstructured.embed.interfaces import BaseEmbeddingEncoder, EmbeddingConfig
+from unstructured.ingest.error import EmbeddingEncoderConnectionError
+from unstructured.utils import requires_dependencies
+
+if TYPE_CHECKING:
+    from langchain.embeddings.openai import OpenAIEmbeddings
+
+
+@dataclass
+class OctoAiEmbeddingConfig(EmbeddingConfig):
+    api_key: str
+    model_name: str = "thenlper/gte-large"
+
+
+@dataclass
+class OctoAIEmbeddingEncoder(BaseEmbeddingEncoder):
+    config: OctoAiEmbeddingConfig
+    _client: Optional["OpenAIEmbeddings"] = field(init=False, default=None)
+    _exemplary_embedding: Optional[List[float]] = field(init=False, default=None)
+
+    @property
+    def client(self) -> "OpenAIEmbeddings":
+        if self._client is None:
+            self._client = self.create_client()
+        return self._client
+
+    @property
+    def exemplary_embedding(self) -> List[float]:
+        if self._exemplary_embedding is None:
+            self._exemplary_embedding = self.client.embed_query("Q")
+        return self._exemplary_embedding
+
+    def initialize(self):
+        pass
+
+    def num_of_dimensions(self):
+        return np.shape(self.exemplary_embedding)
+
+    def is_unit_vector(self):
+        return np.isclose(np.linalg.norm(self.exemplary_embedding), 1.0)
+
+    def embed_query(self, query):
+        return self.client.embed_query(str(query))
+
+    def embed_documents(self, elements: List[Element]) -> List[Element]:
+        embeddings = self.client.embed_documents([str(e) for e in elements])
+        elements_with_embeddings = self._add_embeddings_to_elements(elements, embeddings)
+        return elements_with_embeddings
+
+    def _add_embeddings_to_elements(self, elements, embeddings) -> List[Element]:
+        assert len(elements) == len(embeddings)
+        elements_w_embedding = []
+        for i, element in enumerate(elements):
+            element.embeddings = embeddings[i]
+            elements_w_embedding.append(element)
+        return elements
+
+    @EmbeddingEncoderConnectionError.wrap
+    @requires_dependencies(
+        ["langchain", "openai", "tiktoken"],
+        extras="openai",
+    )
+    def create_client(self) -> "OpenAIEmbeddings":
+        """Creates a langchain OpenAI python client to embed elements."""
+        from langchain.embeddings.openai import OpenAIEmbeddings
+
+        octoai_client = OpenAIEmbeddings(
+            openai_api_key=self.config.api_key,
+            openai_api_base="https://text.octoai.run/v1",
+            model=self.config.model_name,  # type:ignore
+        )
+        return octoai_client
