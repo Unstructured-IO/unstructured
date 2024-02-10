455d4b3037f6761951b511fa1bac4abbf873ba79
potter-potter <david.potter@gmail.com>
2024-02-09 17:05:55 -0800

    fix init

--- unstructured/embed/__init__.py
+++ unstructured/embed/__init__.py
@@ -1,10 +1,11 @@
 from unstructured.embed.bedrock import BedrockEmbeddingEncoder
 from unstructured.embed.huggingface import HuggingFaceEmbeddingEncoder
 from unstructured.embed.openai import OpenAIEmbeddingEncoder
+from unstructured.embed.octoai import OctoAIEmbeddingEncoder
 
 EMBEDDING_PROVIDER_TO_CLASS_MAP = {
     "langchain-openai": OpenAIEmbeddingEncoder,
     "langchain-huggingface": HuggingFaceEmbeddingEncoder,
     "langchain-aws-bedrock": BedrockEmbeddingEncoder,
-    "octoai": BedrockEmbeddingEncoder,
+    "octoai": OctoAIEmbeddingEncoder,
 }
