b4333b66be88ae1d560f97de88761bfd93e3b2be
Filip Knefel <filip@unstructured.io>
2024-02-20 13:55:09 +0100

    test: date_from_file_object parameter in convert_and_partition_html based partitions

--- test_unstructured/partition/epub/test_epub.py
+++ test_unstructured/partition/epub/test_epub.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+from tempfile import SpooledTemporaryFile
 
 from test_unstructured.unit_utils import assert_round_trips_through_JSON
 from unstructured.chunking.title import chunk_by_title
@@ -137,6 +138,23 @@ def test_partition_epub_from_file_metadata_date(
     with open(filename, "rb") as f:
         elements = partition_epub(file=f)
 
+    assert elements[0].metadata.last_modified is None
+
+
+def test_partition_epub_from_file_explicit_get_metadata_date(
+    mocker,
+    filename="example-docs/winter-sports.epub",
+):
+    mocked_last_modification_date = "2029-07-05T09:24:28"
+
+    mocker.patch(
+        "unstructured.partition.html.get_last_modified_date_from_file",
+        return_value=mocked_last_modification_date,
+    )
+
+    with open(filename, "rb") as f:
+        elements = partition_epub(file=f, date_from_file_object=True)
+
     assert elements[0].metadata.last_modified == mocked_last_modification_date
 
 
@@ -156,6 +174,20 @@ def test_partition_epub_from_file_custom_metadata_date(
         elements = partition_epub(file=f, metadata_last_modified=expected_last_modification_date)
 
     assert elements[0].metadata.last_modified == expected_last_modification_date
+
+
+def test_partition_docx_from_file_without_metadata_date(
+    filename="example-docs/winter-sports.epub",
+):
+    """Test partition_epub() with file that are not possible to get last modified date"""
+
+    with open(filename, "rb") as f:
+        sf = SpooledTemporaryFile()
+        sf.write(f.read())
+        sf.seek(0)
+        elements = partition_epub(file=sf, date_from_file_object=True)
+
+    assert elements[0].metadata.last_modified is None
 
 
 def test_partition_epub_with_json():
