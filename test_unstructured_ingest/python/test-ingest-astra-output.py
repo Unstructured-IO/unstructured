e10e20e8d4d3aece6ed25f440dd74cb56de8b025
David Potter <potterdavidm@gmail.com>
2024-02-21 14:32:50 -0800

    vector test2

--- test_unstructured_ingest/python/test-ingest-astra-output.py
+++ test_unstructured_ingest/python/test-ingest-astra-output.py
@@ -1,7 +1,8 @@
-import click
 import random
 
+import click
 from astrapy.db import AstraDB
+
 
 @click.command()
 @click.option("--token", type=str)
@@ -17,7 +18,7 @@ def run_check(token, api_endpoint, collection_name, embedding_dimension):
 
     # Tally up the embeddings
     docs_count = astra_db_collection.count_documents()
-    number_of_embeddings = docs_count["status"]["count"] 
+    number_of_embeddings = docs_count["status"]["count"]
 
     # Print the results
     expected_embeddings = 3
@@ -37,9 +38,11 @@ def run_check(token, api_endpoint, collection_name, embedding_dimension):
     find_one = astra_db_collection.find_one()
     random_vector = find_one["data"]["document"]["vector"]
     random_text = find_one["data"]["document"]["content"]
+    print(random_text)
 
     # Perform a similarity search
     find_result = astra_db_collection.vector_find(random_vector, limit=1)
+    print(find_result)
 
     # Check that we retrieved the coded cleats copy data
     # assert find_result[0]["name"] == random_text
