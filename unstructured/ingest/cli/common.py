939a34bc81b56834a8f5e3e2efe1c30dc3bb15e2
David Potter <potterdavidm@gmail.com>
2024-03-16 07:51:10 -0700

    initial commit

--- unstructured/ingest/cli/common.py
+++ unstructured/ingest/cli/common.py
@@ -6,10 +6,10 @@ from unstructured.ingest.logger import ingest_log_streaming_init, logger
 def options_redactions(options: dict) -> dict:
     # handle any logic needed to redact not already caught by the logging filter
     options = options.copy()
-    if "uri" in options and options["uri"].startswith("mongodb"):
-        from unstructured.ingest.connector.mongodb import redact
+    # if "uri" in options and options["uri"].startswith("mongodb"):
+    #     from unstructured.ingest.connector.mongodb import redact
 
-        options["uri"] = redact(options["uri"])
+    #     options["uri"] = redact(options["uri"])
     return options
 
 
