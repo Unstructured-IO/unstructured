4386c7a5919b6d73c775eefdeadc564a92b91324
David Potter <potterdavidm@gmail.com>
2024-03-18 11:46:15 -0700

    fix unit test

--- test_unstructured_ingest/unit/test_common.py
+++ test_unstructured_ingest/unit/test_common.py
@@ -2,15 +2,10 @@ from unstructured.ingest.cli.common import options_redactions
 
 
 def test_options_redactions():
-    given_options = {
-        "uri": "mongodb+srv://myDatabaseUser:D1fficultP%40ssw0rd@mongodb0.example.com/"
-        "?authSource=admin&replicaSet=myRepl"
-    }
+    """There are no longer any redactions via this method. But may be added in the future."""
+    given_options = {"uri": "http://uri.example.com/" "?authSource=admin&replicaSet=myRepl"}
 
     when_options = options_redactions(options=given_options)
 
-    assert given_options["uri"] != when_options["uri"]
-    assert (
-        when_options["uri"] == "mongodb+srv://myDatabaseUser:***REDACTED***@mongodb0.example.com/"
-        "?authSource=admin&replicaSet=myRepl"
-    )
+    assert given_options["uri"] == when_options["uri"]
+    assert when_options["uri"] == "http://uri.example.com/?authSource=admin&replicaSet=myRepl"
