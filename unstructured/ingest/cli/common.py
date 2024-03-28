c8cf8f31aceb5d3529950783b4461723ed701f3b
David Potter <potterdavidm@gmail.com>
2024-03-28 11:38:50 -0700

    bug CORE-4225: mongodb url bug (#2662)
    The mongodb redact method was created because we wanted part of the url
    to be exposed to the user during logging. Thus it did not use the
    dataclass `enhanced_field(sensitive=True)` solution.
    
    This changes it to use our standard redacted solution. This also
    minimizes the amount of work to be done in platform.

--- unstructured/ingest/cli/common.py
+++ unstructured/ingest/cli/common.py
@@ -1,18 +1,7 @@
 import logging
 
-from unstructured.ingest.logger import ingest_log_streaming_init, logger
-
-
-def options_redactions(options: dict) -> dict:
-    # handle any logic needed to redact not already caught by the logging filter
-    options = options.copy()
-    if "uri" in options and options["uri"].startswith("mongodb"):
-        from unstructured.ingest.connector.mongodb import redact
-
-        options["uri"] = redact(options["uri"])
-    return options
+from unstructured.ingest.logger import ingest_log_streaming_init
 
 
 def log_options(options: dict, verbose=False):
     ingest_log_streaming_init(logging.DEBUG if verbose else logging.INFO)
-    logger.debug(f"options: {options_redactions(options)}")
