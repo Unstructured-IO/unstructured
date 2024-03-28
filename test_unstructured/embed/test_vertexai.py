3805622044bc105d954276aef4cca8ba2826b371
Ahmet Melek <ahmetmeleq@gmail.com>
2024-03-28 06:45:52 +0000

    add api_key to mock test

--- test_unstructured/embed/test_vertexai.py
+++ test_unstructured/embed/test_vertexai.py
@@ -10,7 +10,7 @@ def test_embed_documents_does_not_break_element_to_dict(mocker):
     # Mock create_client to return our mock_client
     mocker.patch.object(VertexAIEmbeddingEncoder, "create_client", return_value=mock_client)
 
-    encoder = VertexAIEmbeddingEncoder(config=VertexAIEmbeddingConfig())
+    encoder = VertexAIEmbeddingEncoder(config=VertexAIEmbeddingConfig(api_key="api_key"))
     elements = encoder.embed_documents(
         elements=[Text("This is sentence 1"), Text("This is sentence 2")],
     )
