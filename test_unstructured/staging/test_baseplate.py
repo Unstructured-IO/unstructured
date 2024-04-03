2d270576c77a1e0457f1a4ea57dc88179e702eca
Michal Martyniak <michal.martyniak@deepsense.ai>
2024-04-03 13:28:53 +0200

    fix baseplate tests

--- test_unstructured/staging/test_baseplate.py
+++ test_unstructured/staging/test_baseplate.py
@@ -4,6 +4,7 @@ from unstructured.documents.elements import (
     ElementMetadata,
     NarrativeText,
     Title,
+    assign_hash_ids,
 )
 from unstructured.staging.baseplate import stage_for_baseplate
 
@@ -25,13 +26,14 @@ def test_stage_for_baseplate():
             metadata=metadata,
         ),
     ]
+    elements = assign_hash_ids(elements)
 
     rows = stage_for_baseplate(elements)
     assert rows == {
         "rows": [
             {
                 "data": {
-                    "element_id": "ad270eefd1cc68d15f4d3e51666d4dc8",
+                    "element_id": "4982120de228327384e259e169f90e24",
                     "text": "A Wonderful Story About A Fox",
                     "type": "Title",
                 },
@@ -50,7 +52,7 @@ def test_stage_for_baseplate():
             },
             {
                 "data": {
-                    "element_id": "8275769fdd1804f9f2b55ad3c9b0ef1b",
+                    "element_id": "84ce27a5715a722b6581fa40b66a87f7",
                     "text": "A fox ran into the chicken coop and the chickens flew off!",
                     "type": "NarrativeText",
                 },
