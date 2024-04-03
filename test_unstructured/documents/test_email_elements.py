9dbf6ae7f99780b66643411bc1149a2d6a0ac937
Michal Martyniak <michal.martyniak@deepsense.ai>
2024-04-03 12:27:09 +0200

    merge 2 tests into 1

--- test_unstructured/documents/test_email_elements.py
+++ test_unstructured/documents/test_email_elements.py
@@ -12,21 +12,15 @@ def test_text_id():
     assert name_element.id_to_hash(index_in_sequence=0) == "eae4fcad50d11af5cec20276d7d5dc65"
 
 
-def test_text_id_is_uuid_by_default():
+def test_text_id_is_a_string_uuid_by_default():
     def _assert_uuid_like(id_to_test: str) -> None:
         assert isinstance(id_to_test, str)
         assert len(id_to_test) == 36
         assert id_to_test.count("-") == 4
 
+    _assert_uuid_like(EmailElement(text="").id)
     _assert_uuid_like(Name(name="Example", text="hello there!").id)
     _assert_uuid_like(Name(name="Example", text="hello there!", element_id=NoID()).id)
-
-
-def test_element_id_defaults_to_UUID():
-    element = EmailElement(text="")
-    assert element.id
-    # TODO test if UUID
-    assert isinstance(element.id, str)
 
 
 def test_text_element_apply_cleaners():
