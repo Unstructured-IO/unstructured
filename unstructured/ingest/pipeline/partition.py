939f54d5fe120e96316aa0a81e101d512283ec06
Michal Martyniak <michal.martyniak@deepsense.ai>
2024-04-04 14:29:27 +0200

    use hashes in partitioner

--- unstructured/ingest/pipeline/partition.py
+++ unstructured/ingest/pipeline/partition.py
@@ -49,6 +49,10 @@ class Partitioner(PartitionNode):
                 partition_config=self.partition_config,
                 **partition_kwargs,
             )
+
+            for idx, element in enumerate(elements):
+                element.id_to_hash(idx)
+
             with open(json_path, "w", encoding="utf8") as output_f:
                 logger.info(f"writing partitioned content to {json_path}")
                 json.dump(elements, output_f, ensure_ascii=False, indent=2, sort_keys=True)
