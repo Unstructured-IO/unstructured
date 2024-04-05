aa1d6e930bd63e0b475910d4ae137ca782f6c102
Roman Isecke <roman@unstructured.io>
2024-04-05 10:43:44 -0400

    Add all other methods to unit test

--- setup_utils.py
+++ setup_utils.py
@@ -1,48 +1,52 @@
 from pathlib import Path
 from typing import List, Union
 
+current_dir = Path(__file__).parent.absolute()
+requirements_dir = current_dir / "requirements"
+ingest_requirements_dir = requirements_dir / "ingest"
 
-def load_requirements(file_list: Union[str, List[str]]) -> List[str]:
-    file_list = file_list if isinstance(file_list, list) else [file_list]
+
+def load_requirements(file: Union[str, Path]) -> List[str]:
+    path = file if isinstance(file, Path) else Path(file)
     requirements: List[str] = []
-    for file in file_list:
-        path = Path(file)
-        file_dir = path.parent.resolve()
-        with open(file, encoding="utf-8") as f:
-            raw = f.read().splitlines()
-            requirements.extend([r for r in raw if not r.startswith("#") and not r.startswith("-")])
-            recursive_reqs = [r for r in raw if r.startswith("-r")]
-        for recursive_req in recursive_reqs:
-            file_spec = recursive_req.split()[-1]
-            file_path = Path(file_dir) / file_spec
-            requirements.extend(load_requirements(file_list=str(file_path.resolve())))
+    if not path.is_file():
+        raise FileNotFoundError(f"path does not point to a valid file: {path}")
+    file_dir = path.parent.resolve()
+    with open(file, encoding="utf-8") as f:
+        raw = f.read().splitlines()
+        requirements.extend([r for r in raw if not r.startswith("#") and not r.startswith("-")])
+        recursive_reqs = [r for r in raw if r.startswith("-r")]
+    for recursive_req in recursive_reqs:
+        file_spec = recursive_req.split()[-1]
+        file_path = Path(file_dir) / file_spec
+        requirements.extend(load_requirements(file=file_path.resolve()))
     # Remove duplicates and any blank entries
     return list({r for r in requirements if r})
 
 
 def get_base_reqs() -> List[str]:
-    file = "requirements/base.in"
+    file = requirements_dir / "base.in"
     return load_requirements(file)
 
 
 def get_doc_reqs() -> dict[str, List[str]]:
     return {
-        "csv": load_requirements("requirements/extra-csv.in"),
-        "doc": load_requirements("requirements/extra-docx.in"),
-        "docx": load_requirements("requirements/extra-docx.in"),
-        "epub": load_requirements("requirements/extra-epub.in"),
-        "image": load_requirements("requirements/extra-pdf-image.in"),
-        "markdown": load_requirements("requirements/extra-markdown.in"),
-        "msg": load_requirements("requirements/extra-msg.in"),
-        "odt": load_requirements("requirements/extra-odt.in"),
-        "org": load_requirements("requirements/extra-pandoc.in"),
-        "pdf": load_requirements("requirements/extra-pdf-image.in"),
-        "ppt": load_requirements("requirements/extra-pptx.in"),
-        "pptx": load_requirements("requirements/extra-pptx.in"),
-        "rtf": load_requirements("requirements/extra-pandoc.in"),
-        "rst": load_requirements("requirements/extra-pandoc.in"),
-        "tsv": load_requirements("requirements/extra-csv.in"),
-        "xlsx": load_requirements("requirements/extra-xlsx.in"),
+        "csv": load_requirements(requirements_dir / "extra-csv.in"),
+        "doc": load_requirements(requirements_dir / "extra-docx.in"),
+        "docx": load_requirements(requirements_dir / "extra-docx.in"),
+        "epub": load_requirements(requirements_dir / "extra-epub.in"),
+        "image": load_requirements(requirements_dir / "extra-pdf-image.in"),
+        "markdown": load_requirements(requirements_dir / "extra-markdown.in"),
+        "msg": load_requirements(requirements_dir / "extra-msg.in"),
+        "odt": load_requirements(requirements_dir / "extra-odt.in"),
+        "org": load_requirements(requirements_dir / "extra-pandoc.in"),
+        "pdf": load_requirements(requirements_dir / "extra-pdf-image.in"),
+        "ppt": load_requirements(requirements_dir / "extra-pptx.in"),
+        "pptx": load_requirements(requirements_dir / "extra-pptx.in"),
+        "rtf": load_requirements(requirements_dir / "extra-pandoc.in"),
+        "rst": load_requirements(requirements_dir / "extra-pandoc.in"),
+        "tsv": load_requirements(requirements_dir / "extra-csv.in"),
+        "xlsx": load_requirements(requirements_dir / "extra-xlsx.in"),
     }
 
 
@@ -55,58 +59,62 @@ def get_all_doc_reqs() -> List[str]:
 
 def get_connector_reqs() -> dict[str, List[str]]:
     return {
-        "airtable": load_requirements("requirements/ingest/airtable.in"),
-        "astra": load_requirements("requirements/ingest/astra.in"),
-        "azure": load_requirements("requirements/ingest/azure.in"),
+        "airtable": load_requirements(ingest_requirements_dir / "airtable.in"),
+        "astra": load_requirements(ingest_requirements_dir / "astra.in"),
+        "azure": load_requirements(ingest_requirements_dir / "azure.in"),
         "azure-cognitive-search": load_requirements(
-            "requirements/ingest/azure-cognitive-search.in",
+            ingest_requirements_dir / "azure-cognitive-search.in",
         ),
-        "biomed": load_requirements("requirements/ingest/biomed.in"),
-        "box": load_requirements("requirements/ingest/box.in"),
-        "chroma": load_requirements("requirements/ingest/chroma.in"),
-        "clarifai": load_requirements("requirements/ingest/clarifai.in"),
-        "confluence": load_requirements("requirements/ingest/confluence.in"),
-        "delta-table": load_requirements("requirements/ingest/delta-table.in"),
-        "discord": load_requirements("requirements/ingest/discord.in"),
-        "dropbox": load_requirements("requirements/ingest/dropbox.in"),
-        "elasticsearch": load_requirements("requirements/ingest/elasticsearch.in"),
-        "gcs": load_requirements("requirements/ingest/gcs.in"),
-        "github": load_requirements("requirements/ingest/github.in"),
-        "gitlab": load_requirements("requirements/ingest/gitlab.in"),
-        "google-drive": load_requirements("requirements/ingest/google-drive.in"),
-        "hubspot": load_requirements("requirements/ingest/hubspot.in"),
-        "jira": load_requirements("requirements/ingest/jira.in"),
-        "mongodb": load_requirements("requirements/ingest/mongodb.in"),
-        "notion": load_requirements("requirements/ingest/notion.in"),
-        "onedrive": load_requirements("requirements/ingest/onedrive.in"),
-        "opensearch": load_requirements("requirements/ingest/opensearch.in"),
-        "outlook": load_requirements("requirements/ingest/outlook.in"),
-        "pinecone": load_requirements("requirements/ingest/pinecone.in"),
-        "postgres": load_requirements("requirements/ingest/postgres.in"),
-        "qdrant": load_requirements("requirements/ingest/qdrant.in"),
-        "reddit": load_requirements("requirements/ingest/reddit.in"),
-        "s3": load_requirements("requirements/ingest/s3.in"),
-        "sharepoint": load_requirements("requirements/ingest/sharepoint.in"),
-        "salesforce": load_requirements("requirements/ingest/salesforce.in"),
-        "sftp": load_requirements("requirements/ingest/sftp.in"),
-        "slack": load_requirements("requirements/ingest/slack.in"),
-        "wikipedia": load_requirements("requirements/ingest/wikipedia.in"),
-        "weaviate": load_requirements("requirements/ingest/weaviate.in"),
-        # Legacy extra requirements
-        "huggingface": load_requirements("requirements/huggingface.in"),
-        "local-inference": get_all_doc_reqs(),
-        "paddleocr": load_requirements("requirements/extra-paddleocr.in"),
-        "embed-huggingface": load_requirements("requirements/ingest/embed-huggingface.in"),
-        "embed-octoai": load_requirements("requirements/ingest/embed-octoai.in"),
-        "embed-vertexai": load_requirements("requirements/ingest/embed-vertexai.in"),
-        "openai": load_requirements("requirements/ingest/embed-openai.in"),
-        "bedrock": load_requirements("requirements/ingest/embed-aws-bedrock.in"),
-        "databricks-volumes": load_requirements("requirements/ingest/databricks-volumes.in"),
+        "biomed": load_requirements(ingest_requirements_dir / "biomed.in"),
+        "box": load_requirements(ingest_requirements_dir / "box.in"),
+        "chroma": load_requirements(ingest_requirements_dir / "chroma.in"),
+        "clarifai": load_requirements(ingest_requirements_dir / "clarifai.in"),
+        "confluence": load_requirements(ingest_requirements_dir / "confluence.in"),
+        "delta-table": load_requirements(ingest_requirements_dir / "delta-table.in"),
+        "discord": load_requirements(ingest_requirements_dir / "discord.in"),
+        "dropbox": load_requirements(ingest_requirements_dir / "dropbox.in"),
+        "elasticsearch": load_requirements(ingest_requirements_dir / "elasticsearch.in"),
+        "gcs": load_requirements(ingest_requirements_dir / "gcs.in"),
+        "github": load_requirements(ingest_requirements_dir / "github.in"),
+        "gitlab": load_requirements(ingest_requirements_dir / "gitlab.in"),
+        "google-drive": load_requirements(ingest_requirements_dir / "google-drive.in"),
+        "hubspot": load_requirements(ingest_requirements_dir / "hubspot.in"),
+        "jira": load_requirements(ingest_requirements_dir / "jira.in"),
+        "mongodb": load_requirements(ingest_requirements_dir / "mongodb.in"),
+        "notion": load_requirements(ingest_requirements_dir / "notion.in"),
+        "onedrive": load_requirements(ingest_requirements_dir / "onedrive.in"),
+        "opensearch": load_requirements(ingest_requirements_dir / "opensearch.in"),
+        "outlook": load_requirements(ingest_requirements_dir / "outlook.in"),
+        "pinecone": load_requirements(ingest_requirements_dir / "pinecone.in"),
+        "postgres": load_requirements(ingest_requirements_dir / "postgres.in"),
+        "qdrant": load_requirements(ingest_requirements_dir / "qdrant.in"),
+        "reddit": load_requirements(ingest_requirements_dir / "reddit.in"),
+        "s3": load_requirements(ingest_requirements_dir / "s3.in"),
+        "sharepoint": load_requirements(ingest_requirements_dir / "sharepoint.in"),
+        "salesforce": load_requirements(ingest_requirements_dir / "salesforce.in"),
+        "sftp": load_requirements(ingest_requirements_dir / "sftp.in"),
+        "slack": load_requirements(ingest_requirements_dir / "slack.in"),
+        "wikipedia": load_requirements(ingest_requirements_dir / "wikipedia.in"),
+        "weaviate": load_requirements(ingest_requirements_dir / "weaviate.in"),
+        "embed-huggingface": load_requirements(ingest_requirements_dir / "embed-huggingface.in"),
+        "embed-octoai": load_requirements(ingest_requirements_dir / "embed-octoai.in"),
+        "embed-vertexai": load_requirements(ingest_requirements_dir / "embed-vertexai.in"),
+        "openai": load_requirements(ingest_requirements_dir / "embed-openai.in"),
+        "bedrock": load_requirements(ingest_requirements_dir / "embed-aws-bedrock.in"),
+        "databricks-volumes": load_requirements(ingest_requirements_dir / "databricks-volumes.in"),
     }
 
 
 def get_extras() -> dict[str, List[str]]:
     reqs = get_doc_reqs()
-    reqs["all-docs"] = get_all_doc_reqs()
+    reqs.update(
+        {
+            "all-docs": get_all_doc_reqs(),
+            # Legacy extra requirements
+            "huggingface": load_requirements(requirements_dir / "huggingface.in"),
+            "local-inference": get_all_doc_reqs(),
+            "paddleocr": load_requirements(requirements_dir / "extra-paddleocr.in"),
+        }
+    )
     reqs.update(get_connector_reqs())
     return reqs
--- test_unstructured/test_setup_utils.py
+++ test_unstructured/test_setup_utils.py
@@ -1,9 +1,33 @@
-from setup_utils import load_requirements
+import pytest
+
+import setup_utils
 
 
 def test_load_requirements():
     file = "./files/example.in"
-    reqs = load_requirements(file_list=[file])
+    reqs = setup_utils.load_requirements(file=file)
     desired_deps = ["torch", "httpx", "requests", "sphinx<4.3.2", "pandas"]
     assert len(reqs) == len(desired_deps)
     assert sorted(reqs) == sorted(desired_deps)
+
+
+def test_load_requirements_not_file():
+    file = "./files/nothing.in"
+    with pytest.raises(FileNotFoundError):
+        setup_utils.load_requirements(file=file)
+
+
+def test_load_base():
+    setup_utils.get_base_reqs()
+
+
+def test_load_doc_reqs():
+    setup_utils.get_doc_reqs()
+
+
+def test_load_all_doc_reqs():
+    setup_utils.get_all_doc_reqs()
+
+
+def test_load_extra_reqs():
+    setup_utils.get_extras()
