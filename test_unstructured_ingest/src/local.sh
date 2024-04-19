232c40545f112e97a43a32350974aab4478c0ae7
Michal Martyniak <michal.martyniak@deepsense.ai>
2024-04-19 20:56:12 +0200

    revert all changes to test_unstructured_ingest

--- test_unstructured_ingest/src/local.sh
+++ test_unstructured_ingest/src/local.sh
@@ -32,6 +32,6 @@ PYTHONPATH=${PYTHONPATH:-.} "$RUN_SCRIPT" \
   --input-path example-docs \
   --work-dir "$WORK_DIR"
 
-"$SCRIPT_DIR"/check-num-files-output.sh 13 $OUTPUT_FOLDER_NAME
+"$SCRIPT_DIR"/check-num-files-output.sh 12 $OUTPUT_FOLDER_NAME
 
 "$SCRIPT_DIR"/evaluation-ingest-cp.sh "$OUTPUT_DIR" "$OUTPUT_FOLDER_NAME"
