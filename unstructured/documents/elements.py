20aae6b29a831e4384a27ed47e073f3165f05fa7
Kamil Plucinski <kamil.plucinski@deepsense.ai>
2024-04-23 14:06:49 +0200

    Add consolidation strategy

--- unstructured/documents/elements.py
+++ unstructured/documents/elements.py
@@ -492,6 +492,7 @@ class ConsolidationStrategy(enum.Enum):
             "signature": cls.FIRST,
             "subject": cls.FIRST,
             "text_as_html": cls.FIRST,  # -- only occurs in Table --
+            "table_as_cells": cls.FIRST,  # -- only occurs in Table --
             "url": cls.FIRST,
         }
 
