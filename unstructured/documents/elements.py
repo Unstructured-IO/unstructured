df1f7bcd0e529c7af5e6848b72c65152c8665653
Pluto <Kamil.plucinski97@gmail.com>
2024-04-25 13:14:48 +0200

    Save table prediction in cells format (#2892)
    This pull request allows to return predictions in raw cell
    representation from table transformer. It will be later used to save
    prediction in a cells format for simpler metrics calculation.
    
    This PR has to be merged, after
    https://github.com/Unstructured-IO/unstructured-inference/pull/335

--- unstructured/documents/elements.py
+++ unstructured/documents/elements.py
@@ -201,6 +201,7 @@ class ElementMetadata:
 
     # -- used for Table elements to capture rows/col structure --
     text_as_html: Optional[str]
+    table_as_cells: Optional[dict[str, str | int]]
     url: Optional[str]
 
     # -- debug fields can be assigned and referenced using dotted-notation but are not serialized
@@ -239,6 +240,7 @@ class ElementMetadata:
         signature: Optional[str] = None,
         subject: Optional[str] = None,
         text_as_html: Optional[str] = None,
+        table_as_cells: Optional[dict[str, str | int]] = None,
         url: Optional[str] = None,
     ) -> None:
         self.attached_to_filename = attached_to_filename
@@ -278,6 +280,7 @@ class ElementMetadata:
         self.signature = signature
         self.subject = subject
         self.text_as_html = text_as_html
+        self.table_as_cells = table_as_cells
         self.url = url
 
     def __eq__(self, other: object) -> bool:
@@ -490,6 +493,7 @@ class ConsolidationStrategy(enum.Enum):
             "signature": cls.FIRST,
             "subject": cls.FIRST,
             "text_as_html": cls.FIRST,  # -- only occurs in Table --
+            "table_as_cells": cls.FIRST,  # -- only occurs in Table --
             "url": cls.FIRST,
         }
 
