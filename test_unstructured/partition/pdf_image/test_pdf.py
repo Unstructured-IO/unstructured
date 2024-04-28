e6fe853e610171c19786ede7aac8a39272e9beb8
cragwolfe <crag@unstructured.io>
2024-04-28 16:28:39 -0700

    revert _combine_list_elements logic

--- test_unstructured/partition/pdf_image/test_pdf.py
+++ test_unstructured/partition/pdf_image/test_pdf.py
@@ -887,7 +887,10 @@ def test_partition_pdf_word_bbox_not_char(
     filename=example_doc_path("interface-config-guide-p93.pdf"),
 ):
     try:
-        elements = pdf.partition_pdf(filename=filename)
+        elements = pdf.partition_pdf(filename=filename, strategy="fast")
+        from unstructured.staging.base import elements_to_json
+        #elements_to_json(elements, filename="/tmp/elments-gh-pr-2940.json", indent=2)
+        elements_to_json(elements, filename="/tmp/elments-fast-main-9e46ed016.json", indent=2)
     except Exception as e:
         raise ("Partitioning fail: %s" % e)
     assert len(elements) == 17
