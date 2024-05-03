b7dace4cccbab1b877d7083fbb47ac350be89b75
David Potter <potterdavidm@gmail.com>
2024-05-02 20:47:23 -0700

    uncomment namespace arg

--- unstructured/ingest/connector/astra.py
+++ unstructured/ingest/connector/astra.py
@@ -181,7 +181,7 @@ class AstraSourceConnector(SourceConnectorCleanupMixin, BaseSourceConnector):
             self._astra_db = AstraDB(
                 api_endpoint=self.connector_config.access_config.api_endpoint,
                 token=self.connector_config.access_config.token,
-                # namespace=self.connector_config.namespace,
+                namespace=self.connector_config.namespace,
                 caller_name=integration_name,
                 caller_version=integration_version,
             )
