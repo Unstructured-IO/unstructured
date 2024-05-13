b8b2d002e76e8da4ed6443e6523d6a6b88538913
Roman Isecke <roman@unstructured.io>
2024-05-13 10:44:41 -0400

    Support file level reprocess flag

--- unstructured/ingest/v2/pipeline/steps/stage.py
+++ unstructured/ingest/v2/pipeline/steps/stage.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import TypedDict
 
+from unstructured.ingest.v2.interfaces.file_data import FileData
 from unstructured.ingest.v2.interfaces.upload_stager import UploadStager
 from unstructured.ingest.v2.pipeline.interfaces import PipelineStep, log_error
 
@@ -21,10 +22,14 @@ class UploadStageStep(PipelineStep):
     @log_error()
     def run(self, path: str, file_data_path: str) -> UploadStageStepResponse:
         path = Path(path)
-        staged_output_path = self.process.run(elements_filepath=path)
+        staged_output_path = self.process.run(
+            elements_filepath=path, file_data=FileData.from_file(path=file_data_path)
+        )
         return UploadStageStepResponse(file_data_path=file_data_path, path=str(staged_output_path))
 
     async def run_async(self, path: str, file_data_path: str) -> UploadStageStepResponse:
         path = Path(path)
-        staged_output_path = await self.process.run_async(elements_filepath=path)
+        staged_output_path = await self.process.run_async(
+            elements_filepath=path, file_data=FileData.from_file(path=file_data_path)
+        )
         return UploadStageStepResponse(file_data_path=file_data_path, path=str(staged_output_path))
