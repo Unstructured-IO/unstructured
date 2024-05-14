d58ef635f61553d96074174add630b68754546e5
Matt Robinson <mrobinson@unstructuredai.io>
2024-05-14 14:33:04 -0400

    tidy shell

--- scripts/docker-dl-packages.sh
+++ scripts/docker-dl-packages.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 files=(
-    "libreoffice-7.6.5-r0.apk"
-    "openjpeg-2.5.0-r0.apk"
-    "poppler-23.09.0-r0.apk"
-    "leptonica-1.83.0-r0.apk"
-    "pandoc-3.1.8-r0.apk"
-    "tesseract-5.3.2-r0.apk"
-    "nltk_data.tgz"
+  "libreoffice-7.6.5-r0.apk"
+  "openjpeg-2.5.0-r0.apk"
+  "poppler-23.09.0-r0.apk"
+  "leptonica-1.83.0-r0.apk"
+  "pandoc-3.1.8-r0.apk"
+  "tesseract-5.3.2-r0.apk"
+  "nltk_data.tgz"
 
 )
 
@@ -15,8 +15,8 @@ directory="docker-packages"
 mkdir -p "${directory}"
 
 for file in "${files[@]}"; do
-    echo "Downloading ${file}"
-    wget "https://utic-public-cf.s3.amazonaws.com/$file" -P "$directory"
+  echo "Downloading ${file}"
+  wget "https://utic-public-cf.s3.amazonaws.com/$file" -P "$directory"
 done
 
 echo "Downloads complete."
