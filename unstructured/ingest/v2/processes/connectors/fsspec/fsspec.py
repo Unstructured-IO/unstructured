11312baee4ec3b97abf9d94e8b0857414afaa3f6
Roman Isecke <roman@unstructured.io>
2024-05-20 14:53:53 -0400

    fix s3 uploader

--- unstructured/ingest/v2/processes/connectors/fsspec/fsspec.py
+++ unstructured/ingest/v2/processes/connectors/fsspec/fsspec.py
@@ -308,7 +308,8 @@ class FsspecUploader(Uploader):
             Path(self.upload_config.path_without_protocol)
             / file_data.source_identifiers.relative_path
         )
-        upload_path_str = str(upload_path)
+        updated_upload_path = upload_path.parent / f"{upload_path.stem}.json"
+        upload_path_str = str(updated_upload_path)
         path_str = str(path.resolve())
         if self.fs.exists(path=upload_path_str) and not self.upload_config.overwrite:
             logger.debug(f"Skipping upload of {path} to {upload_path}, file already exists")
