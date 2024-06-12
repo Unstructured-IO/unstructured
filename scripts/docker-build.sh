7e99d6818f7785770ba3889c7f221366c2b349a5
christinestraub <christinemstraub@gmail.com>
2024-06-12 15:44:17 -0700

    disable cache when building a Docker image

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -10,7 +10,8 @@ DOCKER_BUILD_CMD=(docker buildx build --load -f Dockerfile-amd64
   --build-arg BUILDKIT_INLINE_CACHE=1
   --progress plain
   --platform linux/amd64
-  --cache-from "$DOCKER_REPOSITORY":latest
+#  NOTE(christine) - put this back in after we publish a new image to $DOCKER_REPOSITORY
+#  --cache-from "$DOCKER_REPOSITORY":latest
   -t "$DOCKER_IMAGE" .)
 
 # only build for specific platform if DOCKER_BUILD_PLATFORM is set
