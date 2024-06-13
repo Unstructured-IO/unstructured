8a1c15494f65a849ff432f76f5d8cd351594bd21
Roman Isecke <roman@unstructured.io>
2024-06-13 14:53:00 -0400

    avoid duplicate handlers being added to logger

--- unstructured/ingest/v2/logger.py
+++ unstructured/ingest/v2/logger.py
@@ -112,7 +112,8 @@ def make_default_logger(level: int) -> Logger:
     handler.name = "ingest_log_handler"
     formatter = SensitiveFormatter("%(asctime)s %(processName)-10s %(levelname)-8s %(message)s")
     handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    if handler.name not in [h.name for h in logger.handlers]:
+        logger.addHandler(handler)
     logger.setLevel(level)
     remove_root_handlers(logger)
     return logger
