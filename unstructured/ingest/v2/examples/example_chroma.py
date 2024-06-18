aa1bf8b1818c643728131975f4f4154efefd2e74
David Potter <potterdavidm@gmail.com>
2024-06-18 08:52:45 -0700

    fix parent since they are in a new subfolder.

--- unstructured/ingest/v2/examples/example_chroma.py
+++ unstructured/ingest/v2/examples/example_chroma.py
@@ -19,7 +19,7 @@ from unstructured.ingest.v2.processes.connectors.local import (
 from unstructured.ingest.v2.processes.embedder import EmbedderConfig
 from unstructured.ingest.v2.processes.partitioner import PartitionerConfig
 
-base_path = Path(__file__).parent.parent.parent.parent
+base_path = Path(__file__).parent.parent.parent.parent.parent
 docs_path = base_path / "example-docs"
 work_dir = base_path / "tmp_ingest"
 output_path = work_dir / "output"
