cc711f46fec35caad7d6501234c194a5f36064e9
David Potter <potterdavidm@gmail.com>
2024-06-21 06:55:19 -0700

    just making files

--- unstructured/ingest/v2/cli/cmds/__init__.py
+++ unstructured/ingest/v2/cli/cmds/__init__.py
@@ -10,6 +10,7 @@ from .fsspec.gcs import gcs_dest_cmd, gcs_src_cmd
 from .fsspec.s3 import s3_dest_cmd, s3_src_cmd
 from .fsspec.sftp import sftp_dest_cmd, sftp_src_cmd
 from .local import local_dest_cmd, local_src_cmd
+from .opensearch import opensearch_dest_cmd
 from .weaviate import weaviate_dest_cmd
 
 src_cmds = [
@@ -39,6 +40,7 @@ dest_cmds = [
     elasticsearch_dest_cmd,
     gcs_dest_cmd,
     local_dest_cmd,
+    opensearch_dest_cmd,
     s3_dest_cmd,
     sftp_dest_cmd,
     weaviate_dest_cmd,
