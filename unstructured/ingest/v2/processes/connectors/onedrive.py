250c570fd68822f8c77d57ed694b95ff8352bc06
Roman Isecke <roman@unstructured.io>
2024-06-25 15:08:22 -0400

    complete indexer

--- unstructured/ingest/v2/processes/connectors/onedrive.py
+++ unstructured/ingest/v2/processes/connectors/onedrive.py
@@ -1,6 +1,11 @@
+import json
 from dataclasses import dataclass, field
+from time import time
 from typing import TYPE_CHECKING, Any, Generator, Optional
 
+from dateutil import parser
+
+from unstructured.documents.elements import DataSourceMetadata
 from unstructured.ingest.enhanced_dataclass import enhanced_field
 from unstructured.ingest.error import SourceConnectionNetworkError
 from unstructured.ingest.v2.interfaces import (
@@ -11,6 +16,7 @@ from unstructured.ingest.v2.interfaces import (
     FileData,
     Indexer,
     IndexerConfig,
+    SourceIdentifiers,
     download_responses,
 )
 from unstructured.ingest.v2.logger import logger
@@ -22,6 +28,7 @@ from unstructured.utils import requires_dependencies
 
 if TYPE_CHECKING:
     from office365.graph_client import GraphClient
+    from office365.onedrive.driveitems.driveItem import DriveItem
 
 CONNECTOR_TYPE = "onedrive"
 MAX_MB_SIZE = 512_000_000
@@ -77,10 +84,78 @@ class OnedriveIndexerConfig(IndexerConfig):
 @dataclass
 class OnedriveIndexer(Indexer):
     connection_config: OnedriveConnectionConfig
-    indexer_config: OnedriveIndexerConfig
+    index_config: OnedriveIndexerConfig
+
+    def list_objects(self, folder, recursive) -> list["DriveItem"]:
+        drive_items = folder.children.get().execute_query()
+        files = [d for d in drive_items if d.is_file]
+        if not recursive:
+            return files
+        folders = [d for d in drive_items if d.is_folder]
+        for f in folders:
+            files.extend(self.list_objects(f, recursive))
+        return files
+
+    def get_root(self, client: "GraphClient") -> "DriveItem":
+        root = client.users[self.connection_config.user_pname].drive.get().execute_query().root
+        if fpath := self.index_config.path:
+            root = root.get_by_path(fpath).get().execute_query()
+            if root is None or not root.is_folder:
+                raise ValueError(f"Unable to find directory, given: {fpath}")
+        return root
+
+    def get_properties(self, drive_item: "DriveItem") -> dict:
+        properties = drive_item.properties
+        filtered_properties = {}
+        for k, v in properties.items():
+            try:
+                json.dumps(v)
+                filtered_properties[k] = v
+            except TypeError:
+                pass
+        return filtered_properties
+
+    def drive_item_to_file_data(self, drive_item: "DriveItem") -> FileData:
+        file_path = drive_item.parent_reference.path.split(":")[-1]
+        file_path = file_path[1:] if file_path and file_path[0] == "/" else file_path
+        filename = drive_item.name
+        server_path = file_path + "/" + filename
+        rel_path = file_path.replace(self.index_config.path, "").lstrip("/")
+        date_modified_dt = (
+            parser.parse(drive_item.last_modified_datetime)
+            if drive_item.last_modified_datetime
+            else None
+        )
+        date_created_at = (
+            parser.parse(drive_item.created_datetime) if drive_item.created_datetime else None
+        )
+        return FileData(
+            identifier=drive_item.id,
+            connector_type=CONNECTOR_TYPE,
+            source_identifiers=SourceIdentifiers(
+                fullpath=file_path, filename=drive_item.name, rel_path=rel_path
+            ),
+            metadata=DataSourceMetadata(
+                url=drive_item.parent_reference.path + "/" + drive_item.name,
+                version=drive_item.etag,
+                date_modified=str(date_modified_dt.timestamp()) if date_modified_dt else None,
+                date_created=str(date_created_at.timestamp()) if date_modified_dt else None,
+                date_processed=str(time()),
+                record_locator={
+                    "user_pname": self.connection_config.user_pname,
+                    "server_relative_path": server_path,
+                },
+            ),
+            additional_metadata=self.get_properties(drive_item=drive_item),
+        )
 
     def run(self, **kwargs: Any) -> Generator[FileData, None, None]:
-        pass
+        client = self.connection_config.get_client()
+        root = self.get_root(client=client)
+        drive_items = self.list_objects(folder=root, recursive=self.index_config.recursive)
+        for drive_item in drive_items:
+            file_data = self.drive_item_to_file_data(drive_item=drive_item)
+            yield file_data
 
 
 @dataclass
