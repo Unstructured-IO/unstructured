049e14d909e11c79329433672d5451805f55ef4b
rui <rui@mixedbread.ai>
2024-06-25 14:40:51 +0200

    add requirements and script

--- setup.py
+++ setup.py
@@ -173,6 +173,7 @@ setup(
         "embed-octoai": load_requirements("requirements/ingest/embed-octoai.in"),
         "embed-vertexai": load_requirements("requirements/ingest/embed-vertexai.in"),
         "embed-voyageai": load_requirements("requirements/ingest/embed-voyageai.in"),
+        "embed-mixedbreadai": load_requirements("requirements/ingest/embed-mixedbreadai.in"),
         "openai": load_requirements("requirements/ingest/embed-openai.in"),
         "bedrock": load_requirements("requirements/ingest/embed-aws-bedrock.in"),
         "databricks-volumes": load_requirements("requirements/ingest/databricks-volumes.in"),
