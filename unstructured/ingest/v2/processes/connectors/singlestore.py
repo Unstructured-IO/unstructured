cee5151b89e4d3176d3e8657526f36276f84f007
Roman Isecke <roman@unstructured.io>
2024-06-28 09:01:34 -0400

    Fix uploader

--- unstructured/ingest/v2/processes/connectors/singlestore.py
+++ unstructured/ingest/v2/processes/connectors/singlestore.py
@@ -1,5 +1,4 @@
 import json
-import uuid
 from dataclasses import dataclass
 from datetime import date, datetime
 from pathlib import Path
@@ -78,71 +77,6 @@ class SingleStoreUploadStager(UploadStager):
             logger.debug(f"date {date_string} string not a timestamp: {e}")
         return parser.parse(date_string)
 
-    def conform_dict(self, data: dict) -> None:
-        """
-        Updates the element dictionary to conform to the sql schema
-        """
-
-        data["id"] = str(uuid.uuid4())
-
-        # Dict as string formatting
-        if record_locator := data.get("metadata", {}).get("data_source", {}).get("record_locator"):
-            # Explicit casting otherwise fails schema type checking
-            data["metadata"]["data_source"]["record_locator"] = str(json.dumps(record_locator))
-
-        # Array of items as string formatting
-        if embeddings := data.get("embeddings"):
-            data["embeddings"] = str(json.dumps(embeddings))
-
-        if points := data.get("metadata", {}).get("coordinates", {}).get("points"):
-            data["metadata"]["coordinates"]["points"] = str(json.dumps(points))
-
-        if links := data.get("metadata", {}).get("links", {}):
-            data["metadata"]["links"] = str(json.dumps(links))
-
-        if permissions_data := (
-            data.get("metadata", {}).get("data_source", {}).get("permissions_data")
-        ):
-            data["metadata"]["data_source"]["permissions_data"] = json.dumps(permissions_data)
-
-        if sent_from := data.get("metadata", {}).get("sent_from", {}):
-            data["metadata"]["sent_from"] = str(json.dumps(sent_from))
-
-        if sent_to := data.get("metadata", {}).get("sent_to", {}):
-            data["metadata"]["sent_to"] = str(json.dumps(sent_to))
-
-        # Datetime formatting
-        if date_created := data.get("metadata", {}).get("data_source", {}).get("date_created"):
-            data["metadata"]["data_source"]["date_created"] = self.parse_date_string(date_created)
-
-        if date_modified := data.get("metadata", {}).get("data_source", {}).get("date_modified"):
-            data["metadata"]["data_source"]["date_modified"] = self.parse_date_string(date_modified)
-
-        if date_processed := data.get("metadata", {}).get("data_source", {}).get("date_processed"):
-            data["metadata"]["data_source"]["date_processed"] = self.parse_date_string(
-                date_processed
-            )
-
-        if last_modified := data.get("metadata", {}).get("last_modified", {}):
-            data["metadata"]["last_modified"] = self.parse_date_string(last_modified)
-
-        # String casting
-        if version := data.get("metadata", {}).get("data_source", {}).get("version"):
-            data["metadata"]["data_source"]["version"] = str(version)
-
-        if page_number := data.get("metadata", {}).get("page_number"):
-            data["metadata"]["page_number"] = str(page_number)
-
-        if regex_metadata := data.get("metadata", {}).get("regex_metadata"):
-            data["metadata"]["regex_metadata"] = str(json.dumps(regex_metadata))
-
-        if data.get("metadata", {}).get("data_source", None):
-            data.update(data.get("metadata", {}).pop("data_source", None))
-        if data.get("metadata", {}).get("coordinates", None):
-            data.update(data.get("metadata", {}).pop("coordinates", None))
-        if data.get("metadata", {}):
-            data.update(data.pop("metadata", None))
-
     def run(
         self,
         elements_filepath: Path,
@@ -153,8 +87,6 @@ class SingleStoreUploadStager(UploadStager):
     ) -> Path:
         with open(elements_filepath) as elements_file:
             elements_contents = json.load(elements_file)
-        # for element in elements_contents:
-        #     self.conform_dict(data=element)
         output_path = Path(output_dir) / Path(f"{output_filename}.csv")
         output_path.parent.mkdir(parents=True, exist_ok=True)
 
@@ -162,6 +94,18 @@ class SingleStoreUploadStager(UploadStager):
             elements_dict=elements_contents,
             drop_empty_cols=self.upload_stager_config.drop_empty_cols,
         )
+        datetime_columns = [
+            "data_source_date_created",
+            "data_source_date_modified",
+            "data_source_date_processed",
+        ]
+        for column in filter(lambda x: x in df.columns, datetime_columns):
+            df[column] = df[column].apply(self.parse_date_string)
+        if "data_source_record_locator" in df.columns:
+            df["data_source_record_locator"] = df["data_source_record_locator"].apply(
+                lambda x: json.dumps(x) if x else None
+            )
+
         with output_path.open("w") as output_file:
             df.to_csv(output_file, index=False)
         return output_path
@@ -185,8 +129,10 @@ class SingleStoreUploader(Uploader):
             f"uploading {len(df)} entries to {self.connection_config.database} "
             f"db in table {self.upload_config.table_name}"
         )
-        stmt = "INSERT INTO {} VALUES ({})".format(
-            self.upload_config.table_name, ", ".join(["%s"] * len(df.columns))
+        stmt = "INSERT INTO {} ({}) VALUES ({})".format(
+            self.upload_config.table_name,
+            ", ".join(df.columns),
+            ", ".join(["%s"] * len(df.columns)),
         )
         logger.debug(f"sql statement: {stmt}")
         df.replace({np.nan: None}, inplace=True)
@@ -196,17 +142,8 @@ class SingleStoreUploader(Uploader):
                 for chunk in chunk_generator(
                     data_as_tuples, batch_size=self.upload_config.batch_size
                 ):
-                    # cur.executemany(stmt, chunk)
-                    # conn.commit()
-                    for c in chunk:
-                        try:
-                            cur.execute(stmt, c)
-                            conn.commit()
-                        except Exception as e:
-                            for i, cc in enumerate(df.columns):
-                                print(f"{cc}: {type(c[i])} {c[i]}")
-                            logger.error(f"failed to write entry: {c}")
-                            raise e
+                    cur.executemany(stmt, chunk)
+                    conn.commit()
 
     def run(self, contents: list[UploadContent], **kwargs: Any) -> None:
         for content in contents:
