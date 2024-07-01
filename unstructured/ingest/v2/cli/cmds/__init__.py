a18b21c06eeb45c66f8e5964f8ed839c4de9f200
David Potter <potterdavidm@gmail.com>
2024-07-01 13:35:26 -0700

    rfctr [P6M-397]:  opensearch source connector v2 (#3302)
    Updates opensearch source connector to v2. Leverages elasticsearch v2
    heavily.
    
    Expected tests renamed because thats how Elasticsearch names them.

--- unstructured/ingest/v2/cli/cmds/__init__.py
+++ unstructured/ingest/v2/cli/cmds/__init__.py
@@ -14,7 +14,7 @@ from .fsspec.sftp import sftp_dest_cmd, sftp_src_cmd
 from .google_drive import google_drive_src_cmd
 from .local import local_dest_cmd, local_src_cmd
 from .onedrive import onedrive_drive_src_cmd
-from .opensearch import opensearch_dest_cmd
+from .opensearch import opensearch_dest_cmd, opensearch_src_cmd
 from .weaviate import weaviate_dest_cmd
 
 src_cmds = [
@@ -26,6 +26,7 @@ src_cmds = [
     google_drive_src_cmd,
     local_src_cmd,
     onedrive_drive_src_cmd,
+    opensearch_src_cmd,
     s3_src_cmd,
     sftp_src_cmd,
 ]
