dced4f2a81bcc4e3c75a74721caceaa9fa3288e1
Ahmet Melek <ahmetmeleq@gmail.com>
2024-07-02 15:45:18 +0100

    fix import error

--- unstructured/ingest/utils/string_and_date_utils.py
+++ unstructured/ingest/utils/string_and_date_utils.py
@@ -3,7 +3,8 @@ import typing as t
 from datetime import datetime
 
 from dateutil import parser
-from logger import logger
+
+from unstructured.ingest.logger import logger
 
 
 def json_to_dict(json_string: str) -> t.Union[str, t.Dict[str, t.Any]]:
