a4e65605d3a2cb0c8f32f3200aee1292975f0211
Matt Robinson <mrobinson@unstructuredai.io>
2024-07-08 11:35:29 -0400

    cleanup sha256 check

--- unstructured/nlp/tokenize.py
+++ unstructured/nlp/tokenize.py
@@ -55,12 +55,9 @@ def get_nltk_data_dir():
 def download_nltk_packages():
     nltk_data_dir = get_nltk_data_dir()
 
-    def sha256_checksum(filename, block_size=65536):
-        sha256 = hashlib.sha256()
-        with open(filename, "rb") as f:
-            for block in iter(lambda: f.read(block_size), b""):
-                sha256.update(block)
-        return sha256.hexdigest()
+    def sha256_checksum(filename):
+        with open(filename, "rb", buffering=0) as f:
+            return hashlib.file_digest(f, "sha256").hexdigest()
 
     with tempfile.NamedTemporaryFile() as tmp_file:
         tgz_file = tmp_file.name
