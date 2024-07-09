ad3ccdd9c79e9fbb8c2494d8b5c74d6a6e6ef548
David Potter <potterdavidm@gmail.com>
2024-07-09 08:10:59 -0700

    improve example file

--- unstructured/ingest/v2/examples/example_salesforce.py
+++ unstructured/ingest/v2/examples/example_salesforce.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from unstructured.ingest.v2.interfaces import ProcessorConfig
 from unstructured.ingest.v2.logger import logger
 from unstructured.ingest.v2.pipeline.pipeline import Pipeline
+from unstructured.ingest.v2.processes.chunker import ChunkerConfig
 from unstructured.ingest.v2.processes.connectors.local import (
     LocalUploaderConfig,
 )
@@ -13,6 +14,7 @@ from unstructured.ingest.v2.processes.connectors.salesforce import (
     SalesforceDownloaderConfig,
     SalesforceIndexerConfig,
 )
+from unstructured.ingest.v2.processes.embedder import EmbedderConfig
 from unstructured.ingest.v2.processes.partitioner import PartitionerConfig
 
 base_path = Path(__file__).parent.parent.parent.parent.parent
@@ -35,7 +37,7 @@ if __name__ == "__main__":
             username=os.getenv("SALESFORCE_USERNAME"),
         ),
         partitioner_config=PartitionerConfig(strategy="fast"),
-        # chunker_config=ChunkerConfig(chunking_strategy="by_title"),
-        # embedder_config=EmbedderConfig(embedding_provider="langchain-huggingface"),
+        chunker_config=ChunkerConfig(chunking_strategy="by_title"),
+        embedder_config=EmbedderConfig(embedding_provider="langchain-huggingface"),
         uploader_config=LocalUploaderConfig(output_dir=str(output_path.resolve())),
     ).run()
