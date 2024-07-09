3779e9184f042611c5422e4748ff25025972f72c
Roman Isecke <roman@unstructured.io>
2024-07-09 13:09:27 -0400

    persist metadata for fsspec connectors

--- unstructured/ingest/v2/processes/connectors/fsspec/fsspec.py
+++ unstructured/ingest/v2/processes/connectors/fsspec/fsspec.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import contextlib
 import fnmatch
 from dataclasses import dataclass, field
 from datetime import datetime
@@ -179,16 +180,22 @@ class FsspecIndexer(Indexer):
             pass
 
         version = self.fs.checksum(path)
+        metadata: dict[str, str] = {}
+        with contextlib.suppress(NotImplementedError):
+            metadata = self.fs.metadata(path)
+        record_locator = {
+            "protocol": self.index_config.protocol,
+            "remote_file_path": self.index_config.remote_url,
+        }
+        if metadata:
+            record_locator["metadata"] = metadata
         return DataSourceMetadata(
             date_created=date_created,
             date_modified=date_modified,
             date_processed=str(time()),
             version=str(version),
             url=f"{self.index_config.protocol}://{path}",
-            record_locator={
-                "protocol": self.index_config.protocol,
-                "remote_file_path": self.index_config.remote_url,
-            },
+            record_locator=record_locator,
         )
 
     def sterilize_info(self, path) -> dict:
