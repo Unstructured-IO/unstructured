b8f190558436097f2e17f62a2a1a66d42d8283ff
Matt Robinson <mrobinson@unstructuredai.io>
2024-07-11 10:59:23 -0400

    add exit to license script

--- scripts/check-licenses.sh
+++ scripts/check-licenses.sh
@@ -3,6 +3,7 @@
 REQUIREMENTS_FILES=$(find requirements -type f -name "*.txt")
 
 for REQUIREMENTS_FILE in $REQUIREMENTS_FILES; do
+  echo "Checking $REQUIREMENTS_FILE"
   liccheck -r "$REQUIREMENTS_FILE"
   EXIT_CODE=$?
   if [ "$EXIT_CODE" -eq 0 ]; then
@@ -12,3 +13,5 @@ for REQUIREMENTS_FILE in $REQUIREMENTS_FILES; do
     exit 1
   fi
 done
+
+exit 0
