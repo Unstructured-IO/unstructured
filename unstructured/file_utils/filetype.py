5c814ea7c587ce03db1d859bee6d34145a2e5ba5
Steve Canny <stcanny@gmail.com>
2024-07-20 14:06:35 -0700

    rfctr(file): bulk-move detect_filetype() body
    As an initial low-risk refactor, in preparation for more detailed
    refactors, move bulk of `detect_filetype()` function into
    `_FileTypeDetector` as one piece.
    
    No logic changes and no changes to tests required because the interface
    and behavior are both unchanged.

--- unstructured/file_utils/filetype.py
+++ unstructured/file_utils/filetype.py
@@ -35,10 +35,32 @@ def detect_filetype(
     file_filename: Optional[str] = None,
     encoding: Optional[str] = "utf-8",
 ) -> FileType:
-    """Use libmagic to determine a file's type.
+    """Determine file-type of specified file using libmagic and/or fallback methods.
 
-    Helps determine which partition brick to use for a given file. A return value of None indicates
-    a non-supported file type.
+    One of `file_path` or `file` must be specified. A `file_path` that does not
+    correspond to a file on the filesystem raises `ValueError`.
+
+    Args:
+        content_type: MIME-type of document-source, when already known. Providing
+          a value for this argument disables auto-detection unless it does not map
+          to a FileType member or is ambiguous, in which case it is ignored.
+        encoding: Only used for textual file-types. When omitted, `utf-8` is
+          assumed. Should generally be omitted except to resolve a problem with
+          textual file-types like HTML.
+        metadata_file_path: Only used when `file` is provided and then only as a
+          source for a filename-extension that may be needed as a secondary
+          content-type indicator. Ignored with the document is specified using
+          `file_path`.
+
+    Returns:
+        A member of the `FileType` enumeration, `FileType.UNK` when the file type
+        could not be determined or is not supported.
+
+    Raises:
+        ValueError: when:
+        - `file_path` is specified but does not correspond to a file on the
+          fileesystem.
+        - Neither `file_path` nor `file` were specified.
     """
     ctx = _FileTypeDetectionContext.new(
         file_path=filename,
@@ -47,122 +69,7 @@ def detect_filetype(
         content_type=content_type,
         metadata_file_path=file_filename,
     )
-
-    # first check (content_type)
-    if content_type:
-        file_type = FileType.from_mime_type(content_type)
-        if file_type:
-            return file_type
-
-    mime_type = ctx.mime_type
-    extension = ctx.extension
-    # -- second check: if can't guess MIME-type use extension --
-    if mime_type is None:
-        return FileType.from_extension(extension) or FileType.UNK
-
-    """Mime type special cases."""
-    # third check (mime_type)
-
-    # NOTE(Crag): older magic lib does not differentiate between xls and doc
-    if mime_type == "application/msword" and extension == ".xls":
-        return FileType.XLS
-
-    elif mime_type.endswith("xml"):
-        if extension == ".html" or extension == ".htm":
-            return FileType.HTML
-        else:
-            return FileType.XML
-
-    # -- ref: https://www.rfc-editor.org/rfc/rfc822 --
-    elif mime_type == "message/rfc822" or mime_type.startswith("text"):
-        if not encoding:
-            encoding = "utf-8"
-        formatted_encoding = format_encoding_str(encoding)
-
-        if extension in [
-            ".eml",
-            ".p7s",
-            ".md",
-            ".rtf",
-            ".html",
-            ".rst",
-            ".org",
-            ".csv",
-            ".tsv",
-            ".json",
-        ]:
-            return FileType.from_extension(extension) or FileType.TXT
-
-        # NOTE(crag): for older versions of the OS libmagic package, such as is currently
-        # installed on the Unstructured docker image, .json files resolve to "text/plain"
-        # rather than "application/json". this corrects for that case.
-        if _is_text_file_a_json(
-            file=file,
-            filename=filename,
-            encoding=formatted_encoding,
-        ):
-            return FileType.JSON
-
-        if _is_text_file_a_csv(
-            file=file,
-            filename=filename,
-            encoding=formatted_encoding,
-        ):
-            return FileType.CSV
-
-        if file and _check_eml_from_buffer(file=file) is True:
-            return FileType.EML
-
-        if extension in PLAIN_TEXT_EXTENSIONS:
-            return FileType.from_extension(extension) or FileType.UNK
-
-        # Safety catch
-        if file_type := FileType.from_mime_type(mime_type):
-            return file_type
-
-        return FileType.TXT
-
-    elif mime_type == "application/octet-stream":
-        if extension == ".docx":
-            return FileType.DOCX
-        elif file:
-            return _detect_filetype_from_octet_stream(file=file)
-        else:
-            return FileType.from_extension(extension) or FileType.UNK
-
-    elif mime_type == "application/zip":
-        file_type = FileType.UNK
-        if file:
-            file_type = _detect_filetype_from_octet_stream(file=file)
-        elif filename is not None:
-            with open(filename, "rb") as f:
-                file_type = _detect_filetype_from_octet_stream(file=f)
-
-        extension = extension if extension else ""
-        return (
-            FileType.ZIP
-            if file_type in (FileType.UNK, FileType.ZIP)
-            else FileType.from_extension(extension) or file_type
-        )
-
-    elif _is_code_mime_type(mime_type):
-        # NOTE(robinson) - we'll treat all code files as plain text for now.
-        # we can update this logic and add filetypes for specific languages
-        # later if needed.
-        return FileType.TXT
-
-    elif mime_type.endswith("empty"):
-        return FileType.EMPTY
-
-    # For everything else
-    elif file_type := FileType.from_mime_type(mime_type):
-        return file_type
-
-    logger.warning(
-        f"The MIME type{f' of {filename!r}' if filename else ''} is {mime_type!r}. "
-        "This file type is not currently supported in unstructured.",
-    )
-    return FileType.from_extension(extension) or FileType.UNK
+    return _FileTypeDetector.file_type(ctx)
 
 
 def is_json_processable(
@@ -184,6 +91,143 @@ def is_json_processable(
             encoding=encoding,
         )
     return re.match(LIST_OF_DICTS_PATTERN, file_text) is not None
+
+
+class _FileTypeDetector:
+    """Determines file type from a variety of possible inputs."""
+
+    def __init__(self, ctx: _FileTypeDetectionContext):
+        self._ctx = ctx
+
+    @classmethod
+    def file_type(cls, ctx: _FileTypeDetectionContext) -> FileType:
+        """Detect file-type of document-source described by `ctx`."""
+        return cls(ctx)._file_type
+
+    @property
+    def _file_type(self) -> FileType:
+        """FileType member corresponding to this document source."""
+        ctx = self._ctx
+        filename = ctx.file_path
+        file = ctx._file_arg  # pyright: ignore[reportPrivateUsage]
+        content_type = ctx.content_type
+        encoding = ctx.encoding
+
+        # first check (content_type)
+        if content_type:
+            file_type = FileType.from_mime_type(content_type)
+            if file_type:
+                return file_type
+
+        mime_type = ctx.mime_type
+        extension = ctx.extension
+        # -- second check: if can't guess MIME-type use extension --
+        if mime_type is None:
+            return FileType.from_extension(extension) or FileType.UNK
+
+        """Mime type special cases."""
+        # third check (mime_type)
+
+        # NOTE(Crag): older magic lib does not differentiate between xls and doc
+        if mime_type == "application/msword" and extension == ".xls":
+            return FileType.XLS
+
+        elif mime_type.endswith("xml"):
+            if extension == ".html" or extension == ".htm":
+                return FileType.HTML
+            else:
+                return FileType.XML
+
+        # -- ref: https://www.rfc-editor.org/rfc/rfc822 --
+        elif mime_type == "message/rfc822" or mime_type.startswith("text"):
+            if not encoding:
+                encoding = "utf-8"
+            formatted_encoding = format_encoding_str(encoding)
+
+            if extension in [
+                ".eml",
+                ".p7s",
+                ".md",
+                ".rtf",
+                ".html",
+                ".rst",
+                ".org",
+                ".csv",
+                ".tsv",
+                ".json",
+            ]:
+                return FileType.from_extension(extension) or FileType.TXT
+
+            # NOTE(crag): for older versions of the OS libmagic package, such as is currently
+            # installed on the Unstructured docker image, .json files resolve to "text/plain"
+            # rather than "application/json". this corrects for that case.
+            if _is_text_file_a_json(
+                file=file,
+                filename=filename,
+                encoding=formatted_encoding,
+            ):
+                return FileType.JSON
+
+            if _is_text_file_a_csv(
+                file=file,
+                filename=filename,
+                encoding=formatted_encoding,
+            ):
+                return FileType.CSV
+
+            if file and _check_eml_from_buffer(file=file) is True:
+                return FileType.EML
+
+            if extension in PLAIN_TEXT_EXTENSIONS:
+                return FileType.from_extension(extension) or FileType.UNK
+
+            # Safety catch
+            if file_type := FileType.from_mime_type(mime_type):
+                return file_type
+
+            return FileType.TXT
+
+        elif mime_type == "application/octet-stream":
+            if extension == ".docx":
+                return FileType.DOCX
+            elif file:
+                return _detect_filetype_from_octet_stream(file=file)
+            else:
+                return FileType.from_extension(extension) or FileType.UNK
+
+        elif mime_type == "application/zip":
+            file_type = FileType.UNK
+            if file:
+                file_type = _detect_filetype_from_octet_stream(file=file)
+            elif filename is not None:
+                with open(filename, "rb") as f:
+                    file_type = _detect_filetype_from_octet_stream(file=f)
+
+            extension = extension if extension else ""
+            return (
+                FileType.ZIP
+                if file_type in (FileType.UNK, FileType.ZIP)
+                else FileType.from_extension(extension) or file_type
+            )
+
+        elif _is_code_mime_type(mime_type):
+            # NOTE(robinson) - we'll treat all code files as plain text for now.
+            # we can update this logic and add filetypes for specific languages
+            # later if needed.
+            return FileType.TXT
+
+        elif mime_type.endswith("empty"):
+            return FileType.EMPTY
+
+        # For everything else
+        elif file_type := FileType.from_mime_type(mime_type):
+            return file_type
+
+        logger.warning(
+            f"The MIME type{f' of {filename!r}' if filename else ''} is {mime_type!r}. "
+            "This file type is not currently supported in unstructured.",
+        )
+        return FileType.from_extension(extension) or FileType.UNK
 
 
 class _FileTypeDetectionContext:
