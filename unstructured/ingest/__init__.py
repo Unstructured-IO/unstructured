d04424511b5d7d3e1673d1f9237dea66a79615b4
Roman Isecke <roman@unstructured.io>
2024-07-29 19:50:51 -0400

    Update deprecation warning with mention of new project

--- unstructured/ingest/__init__.py
+++ unstructured/ingest/__init__.py
@@ -3,5 +3,8 @@ from __future__ import annotations
 import warnings
 
 warnings.warn(
-    "unstructured.ingest will be removed in a future version", DeprecationWarning, stacklevel=2
+    "unstructured.ingest will be removed in a future version. "
+    "Functionality moved to the unstructured-ingest project.",
+    DeprecationWarning,
+    stacklevel=2,
 )
