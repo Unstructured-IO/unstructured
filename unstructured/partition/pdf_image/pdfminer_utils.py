fa53e53a0d82294365222e2a5a676ac795a49d3d
christinestraub <christinemstraub@gmail.com>
2024-07-31 09:13:42 -0700

    refactor: remove unused `get_images_from_pdf_element`

--- unstructured/partition/pdf_image/pdfminer_utils.py
+++ unstructured/partition/pdf_image/pdfminer_utils.py
@@ -45,41 +45,6 @@ def extract_text_and_image_objects(parent_object) -> List[LTComponent]:
     return objects
 
 
-def get_images_from_pdf_element(layout_object: Any) -> List[LTImage]:
-    """
-    Recursively extracts LTImage objects from a PDF layout element.
-
-    This function takes a PDF layout element (could be LTImage or LTContainer) and recursively
-    extracts all LTImage objects contained within it.
-
-    Parameters:
-    - layout_object (Any): The PDF layout element to extract images from.
-
-    Returns:
-    - List[LTImage]: A list of LTImage objects extracted from the layout object.
-
-    Note:
-    - This function recursively traverses through the layout_object to find and accumulate all
-     LTImage objects.
-    - If the input layout_object is an LTImage, it will be included in the returned list.
-    - If the input layout_object is an LTContainer, the function will recursively search its
-     children for LTImage objects.
-    - If the input layout_object is neither LTImage nor LTContainer, an empty list will be
-     returned.
-    """
-
-    # recursively locate Image objects in layout_object
-    if isinstance(layout_object, LTImage):
-        return [layout_object]
-    if isinstance(layout_object, LTContainer):
-        img_list: List[LTImage] = []
-        for child in layout_object:
-            img_list = img_list + get_images_from_pdf_element(child)
-        return img_list
-    else:
-        return []
-
-
 def rect_to_bbox(
     rect: Tuple[float, float, float, float],
     height: float,
