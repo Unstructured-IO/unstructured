e46d8aabfa4a50ac8a213d7862fc3c4bcce62efb
David Potter <potterdavidm@gmail.com>
2024-08-26 15:01:27 -0700

    update expected test

--- test_unstructured_ingest/src/local-embed-mixedbreadai.sh
+++ test_unstructured_ingest/src/local-embed-mixedbreadai.sh
@@ -29,7 +29,7 @@ RUN_SCRIPT=${RUN_SCRIPT:-./unstructured/ingest/main.py}
 PYTHONPATH=${PYTHONPATH:-.} "$RUN_SCRIPT" \
   local \
   --num-processes "$max_processes" \
-  --metadata-exclude coordinates,filename,file_directory,metadata.data_source.date_created,metadata.data_source.date_modified,metadata.data_source.date_processed,metadata.last_modified,metadata.detection_class_prob,metadata.parent_id,metadata.category_depth \
+  --metadata-exclude coordinates,filename,file_directory,metadata.data_source.record_locator.path,metadata.data_source.date_created,metadata.data_source.date_modified,metadata.data_source.date_processed,metadata.last_modified,metadata.detection_class_prob,metadata.parent_id,metadata.category_depth \
   --output-dir "$OUTPUT_DIR" \
   --verbose \
   --reprocess \
