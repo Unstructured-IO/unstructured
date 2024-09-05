4c61c3503cac58e6f2588aee36b2b83479bc41d2
cragwolfe <crag@unstructured.io>
2024-09-05 14:49:05 -0700

    shlfmt

--- scripts/user/process-pdf-parallel-through-api.sh
+++ scripts/user/process-pdf-parallel-through-api.sh
@@ -5,31 +5,31 @@
 set -eu -o pipefail
 
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 <pdf_filename>"
-    echo "Please provide a PDF filename as the first argument."
-    echo
-    echo "Optionally, set the following env vars: "
-    echo
-    echo "* STRATEGY (default hi_res)"
-    echo "* BATCH_SIZE (default 30) as the number of parts (AKA splits) to process in parallel"
-    echo "* PDF_SPLIT_PAGE_SIZE (default 10) as the number of pages per split"
-    echo
-    echo "BATCH_SIZE=20 PDF_SPLIT_PAGE_SIZE=6 STRATEGY=hi_res ./process-pdf-parallel-through-api.sh example-docs/pdf/layout-parser-paper.pdf"
-    exit 1
+  echo "Usage: $0 <pdf_filename>"
+  echo "Please provide a PDF filename as the first argument."
+  echo
+  echo "Optionally, set the following env vars: "
+  echo
+  echo "* STRATEGY (default hi_res)"
+  echo "* BATCH_SIZE (default 30) as the number of parts (AKA splits) to process in parallel"
+  echo "* PDF_SPLIT_PAGE_SIZE (default 10) as the number of pages per split"
+  echo
+  echo "BATCH_SIZE=20 PDF_SPLIT_PAGE_SIZE=6 STRATEGY=hi_res ./process-pdf-parallel-through-api.sh example-docs/pdf/layout-parser-paper.pdf"
+  exit 1
 fi
 
 ALLOWED_STRATEGIES=("hi_res" "fast" "auto")
 
 # Validate STRATEGY environment variable if it's set
 if [ -n "${STRATEGY:-}" ] && [[ ! " ${ALLOWED_STRATEGIES[*]} " =~ ${STRATEGY} ]]; then
-    echo "Error: STRATEGY must be one of ${ALLOWED_STRATEGIES[*]}" >&2
-    exit 1
+  echo "Error: STRATEGY must be one of ${ALLOWED_STRATEGIES[*]}" >&2
+  exit 1
 fi
 
 # Check if PAID_UNST_API_KEY is set
 if [ -z "${PAID_UNST_API_KEY}" ]; then
-    echo "Error: PAID_UNST_API_KEY is not set or is empty" >&2
-    exit 1
+  echo "Error: PAID_UNST_API_KEY is not set or is empty" >&2
+  exit 1
 fi
 
 PDF_FILE="$1"
@@ -56,35 +56,35 @@ incomplete=0  # Flag to track incomplete processing
 
 # Function to process a single PDF part file
 process_file_part() {
-    local file="$1"
-    local STARTING_PAGE_NUMBER="$2"
-    local OUTPUT_JSON="$3"
+  local file="$1"
+  local STARTING_PAGE_NUMBER="$2"
+  local OUTPUT_JSON="$3"
 
-    
-    if [ -f "$OUTPUT_JSON" ]; then
-      echo "Skipping processing for $OUTPUT_JSON as it already exists."
-      return
-    fi
+  
+  if [ -f "$OUTPUT_JSON" ]; then
+    echo "Skipping processing for $OUTPUT_JSON as it already exists."
+    return
+  fi
 
-    curl -q -X POST https://api.unstructuredapp.io/general/v0/general \
-      -H "unstructured-api-key: $PAID_UNST_API_KEY" \
-      -H 'accept: application/json' \
-      -H 'Content-Type: multipart/form-data' \
-      -F strategy="${STRATEGY:-hi_res}" \
-      -F 'skip_infer_table_types="[]"' \
-      -F starting_page_number="$STARTING_PAGE_NUMBER" \
-      -F files=@"$file;filename=$PDF_FILE" \
-      -o "$OUTPUT_JSON"
-    
-    # Verify JSON content
-    if ! jq -e 'if type=="array" then all(.[]; type=="object" or length==0) else empty end' "$OUTPUT_JSON" > /dev/null; then
-      echo "Invalid JSON structure in $OUTPUT_JSON (contents below), deleting file."
-      cat "$OUTPUT_JSON"
-      rm "$OUTPUT_JSON"
-      incomplete=1
-    else
-      echo "Valid JSON output created: $OUTPUT_JSON"
-    fi
+  curl -q -X POST https://api.unstructuredapp.io/general/v0/general \
+    -H "unstructured-api-key: $PAID_UNST_API_KEY" \
+    -H 'accept: application/json' \
+    -H 'Content-Type: multipart/form-data' \
+    -F strategy="${STRATEGY:-hi_res}" \
+    -F 'skip_infer_table_types="[]"' \
+    -F starting_page_number="$STARTING_PAGE_NUMBER" \
+    -F files=@"$file;filename=$PDF_FILE" \
+    -o "$OUTPUT_JSON"
+  
+  # Verify JSON content
+  if ! jq -e 'if type=="array" then all(.[]; type=="object" or length==0) else empty end' "$OUTPUT_JSON" > /dev/null; then
+    echo "Invalid JSON structure in $OUTPUT_JSON (contents below), deleting file."
+    cat "$OUTPUT_JSON"
+    rm "$OUTPUT_JSON"
+    incomplete=1
+  else
+    echo "Valid JSON output created: $OUTPUT_JSON"
+  fi
 }
 
 # Function to process a batch of files
@@ -105,7 +105,7 @@ mapfile -t pdf_parts < <(find "$PDF_DIR" -name '*.pdf' -print)
 
 # Process PDF parts in batches of 30, by default
 batch_size=${BATCH_SIZE:-30}
-for (( i=0; i < ${#pdf_parts[@]}; i+=batch_size )); do
+for ((i=0; i < ${#pdf_parts[@]}; i+=batch_size)); do
   process_batch "${pdf_parts[@]:i:batch_size}"
 done
 
@@ -119,6 +119,6 @@ else
 fi
 
 # Combine JSON outputs in numerical order
-find "$PDF_OUTPUT_DIR" -name '*.json' -print0 | sort -zV | xargs -0 jq -s 'add' > "$PDF_OUTPUT_DIR/$combined_output_filename"
+find "$PDF_OUTPUT_DIR" -name '*.json' -print0 | sort -zV | xargs -0 jq -s 'add' >"$PDF_OUTPUT_DIR/$combined_output_filename"
 
 echo "Processing complete. Combined JSON saved to $PDF_OUTPUT_DIR/$combined_output_filename"
