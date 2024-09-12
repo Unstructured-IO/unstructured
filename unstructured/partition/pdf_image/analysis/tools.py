15e5a4323af899f0feb4b40b03ddedcaf902d4c5
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-09-12 10:17:35 -0400

    spelling: dump
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- unstructured/partition/pdf_image/analysis/tools.py
+++ unstructured/partition/pdf_image/analysis/tools.py
@@ -126,7 +126,7 @@ def render_bboxes_for_file(
     resize: Optional[float] = None,
     format: str = "png",
 ):
-    """Render the bounding boxes for a given layout dimp file.
+    """Render the bounding boxes for a given layout dump file.
     To be used for analysis after the partition is performed for
     only dumping the layouts - the bboxes can be rendered later.
 
