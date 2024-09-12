d76f39f4cc0a41690889da55c166b80b2c8f8a06
Roman Isecke <roman@unstructured.io>
2024-09-12 10:30:26 -0400

    fix param in azure cognitive search dest test

--- test_unstructured_ingest/dest/azure-cognitive-search.sh
+++ test_unstructured_ingest/dest/azure-cognitive-search.sh
@@ -84,7 +84,7 @@ PYTHONPATH=. unstructured-ingest \
   --chunk-new-after-n-chars 1500 \
   --chunk-max-characters 2500 \
   --chunk-multipage-sections \
-  --chunk-no-include-orig-elements \
+  --no-chunk-include-orig-elements \
   --embedding-provider "langchain-huggingface" \
   azure-cognitive-search \
   --key "$AZURE_SEARCH_API_KEY" \
