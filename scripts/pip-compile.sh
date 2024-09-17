d49092d7e6b2934d4c79d917fe62767167ca99a9
Roman Isecke <roman@unstructured.io>
2024-09-17 10:08:06 -0400

    update deps

--- scripts/pip-compile.sh
+++ scripts/pip-compile.sh
@@ -8,7 +8,8 @@ if ! python -c "import sys; assert sys.version_info.major == $major and sys.vers
   exit 1
 fi
 
-pushd ./requirements || exit
-make clean
-make all
-popd || exit
+
+find ./requirements -type f -name "*.txt" ! -name "constraints.txt" -exec rm '{}' ';'
+pip-compile --upgrade ./requirements/deps/base.in
+pip-compile --upgrade ./requirements/deps/test.in
+find ./requirements -type f -name "*.in" -maxdepth 1 -exec pip-compile --upgrade '{}' ';'
