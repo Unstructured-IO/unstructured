868739124f5e00cffe2f9f93ca97b85b456166af
Steve Canny <stcanny@gmail.com>
2024-09-26 19:51:05 -0700

    rfctr(rst): remove double decoration

--- unstructured/partition/rst.py
+++ unstructured/partition/rst.py
@@ -2,10 +2,8 @@ from __future__ import annotations
 
 from typing import IO, Any, Optional
 
-from unstructured.chunking import add_chunking_strategy
-from unstructured.documents.elements import Element, process_metadata
+from unstructured.documents.elements import Element
 from unstructured.file_utils.file_conversion import convert_file_to_html_text_using_pandoc
-from unstructured.file_utils.filetype import add_metadata_with_filetype
 from unstructured.file_utils.model import FileType
 from unstructured.partition.common.common import exactly_one
 from unstructured.partition.common.metadata import get_last_modified_date
@@ -14,17 +12,12 @@ from unstructured.partition.html import partition_html
 DETECTION_ORIGIN: str = "rst"
 
 
-@process_metadata()
-@add_metadata_with_filetype(FileType.RST)
-@add_chunking_strategy
 def partition_rst(
     filename: Optional[str] = None,
     *,
     file: Optional[IO[bytes]] = None,
     metadata_filename: Optional[str] = None,
     metadata_last_modified: Optional[str] = None,
-    languages: Optional[list[str]] = ["auto"],
-    detect_language_per_element: bool = False,
     **kwargs: Any,
 ) -> list[Element]:
     """Partitions an RST document. The document is first converted to HTML and then
@@ -38,13 +31,6 @@ def partition_rst(
         A file-like object using "rb" mode --> open(filename, "rb").
     metadata_last_modified
         The last modified date for the document.
-    languages
-        User defined value for `metadata.languages` if provided. Otherwise language is detected
-        using naive Bayesian filter via `langdetect`. Multiple languages indicates text could be
-        in either language.
-        Additional Parameters:
-            detect_language_per_element
-                Detect language per element instead of at the document level.
     """
     exactly_one(filename=filename, file=file)
 
@@ -57,9 +43,9 @@ def partition_rst(
     return partition_html(
         text=html_text,
         encoding="unicode",
-        metadata_filename=metadata_filename,
+        metadata_filename=metadata_filename or filename,
+        metadata_file_type=FileType.RST,
         metadata_last_modified=metadata_last_modified or last_modified,
-        languages=languages,
-        detect_language_per_element=detect_language_per_element,
         detection_origin=DETECTION_ORIGIN,
+        **kwargs,
     )
