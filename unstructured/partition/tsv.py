bab4ce6f19e4ed6ecbd96b4381a9a6cb8323b286
Steve Canny <stcanny@gmail.com>
2024-09-26 20:32:30 -0700

    rfctr(tsv): install @apply_metadata() decorator

--- unstructured/partition/tsv.py
+++ unstructured/partition/tsv.py
@@ -6,36 +6,23 @@ import pandas as pd
 from lxml.html.soupparser import fromstring as soupparser_fromstring
 
 from unstructured.chunking import add_chunking_strategy
-from unstructured.documents.elements import (
-    Element,
-    ElementMetadata,
-    Table,
-    process_metadata,
-)
-from unstructured.file_utils.filetype import add_metadata_with_filetype
+from unstructured.documents.elements import Element, ElementMetadata, Table
 from unstructured.file_utils.model import FileType
 from unstructured.partition.common.common import (
     exactly_one,
     spooled_to_bytes_io_if_needed,
 )
-from unstructured.partition.common.lang import apply_lang_metadata
-from unstructured.partition.common.metadata import get_last_modified_date
+from unstructured.partition.common.metadata import apply_metadata, get_last_modified_date
 
 DETECTION_ORIGIN: str = "tsv"
 
 
-@process_metadata()
-@add_metadata_with_filetype(FileType.TSV)
+@apply_metadata(FileType.TSV)
 @add_chunking_strategy
 def partition_tsv(
     filename: Optional[str] = None,
     file: Optional[IO[bytes]] = None,
-    metadata_filename: Optional[str] = None,
-    metadata_last_modified: Optional[str] = None,
     include_header: bool = False,
-    languages: Optional[list[str]] = ["auto"],
-    # NOTE (jennings) partition_tsv generates a single TableElement
-    # so detect_language_per_element is not included as a param
     **kwargs: Any,
 ) -> list[Element]:
     """Partitions TSV files into document elements.
@@ -48,16 +35,8 @@ def partition_tsv(
         A file-like object using "rb" mode --> open(filename, "rb").
     include_header
         Determines whether or not header info info is included in text and medatada.text_as_html.
-    metadata_last_modified
-        The day of the last modification.
-    languages
-        User defined value for `metadata.languages` if provided. Otherwise language is detected
-        using naive Bayesian filter via `langdetect`. Multiple languages indicates text could be
-        in either language.
     """
     exactly_one(filename=filename, file=file)
-
-    last_modified = get_last_modified_date(filename) if filename else None
 
     header = 0 if include_header else None
 
@@ -75,14 +54,9 @@ def partition_tsv(
 
     metadata = ElementMetadata(
         text_as_html=html_text,
-        filename=metadata_filename or filename,
-        last_modified=metadata_last_modified or last_modified,
-        languages=languages,
+        filename=filename,
+        last_modified=get_last_modified_date(filename) if filename else None,
     )
     metadata.detection_origin = DETECTION_ORIGIN
 
-    elements = apply_lang_metadata(
-        [Table(text=text, metadata=metadata)],
-        languages=languages,
-    )
-    return list(elements)
+    return [Table(text=text, metadata=metadata)]
