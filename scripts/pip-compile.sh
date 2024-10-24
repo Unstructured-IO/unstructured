3e6cdbe5e3e7c0af75e7149120072487c4666a18
Yuming Long <yuming@unstructured.io>
2024-10-24 11:05:40 -0700

    compile check just minum not match

--- scripts/pip-compile.sh
+++ scripts/pip-compile.sh
@@ -3,7 +3,7 @@
 # python version must match lowest supported (3.9)
 major=3
 minor=9
-if ! python -c "import sys; assert sys.version_info.major == $major and sys.version_info.minor == $minor"; then
+if ! python -c "import sys; assert sys.version_info.major == $major and sys.version_info.minor >= $minor"; then
   echo "python version not equal to expected $major.$minor: $(python --version)"
   exit 1
 fi
