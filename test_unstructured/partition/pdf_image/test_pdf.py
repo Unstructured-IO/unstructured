7e8ad86a352e3b1bd2e9c2df02206cedb7681eb0
Christine Straub <christinemstraub@gmail.com>
2024-10-28 12:46:39 -0700

    fix import error

--- test_unstructured/partition/pdf_image/test_pdf.py
+++ test_unstructured/partition/pdf_image/test_pdf.py
@@ -32,7 +32,7 @@ from unstructured.documents.elements import (
 )
 from unstructured.errors import PageCountExceededError
 from unstructured.partition import pdf, strategies
-from unstructured.partition.pdf import get_uris_from_annots
+from unstructured.partition.pdf_image.pdfminer_processing import get_uris_from_annots
 from unstructured.partition.pdf_image import ocr, pdfminer_processing
 from unstructured.partition.utils.constants import (
     UNSTRUCTURED_INCLUDE_DEBUG_METADATA,
