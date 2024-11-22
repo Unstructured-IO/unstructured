0052a3c7f63e92316cc03da179db47cfdeeb7e3a
ryannikolaidis <ryannikolaidis@users.noreply.github.com>
2024-11-22 02:08:50 +0000

    deploy: 711074df1698c6e31ece78239871bd568fb57e4f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.16.5',
+    VERSION: '0.16.6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
