ca9e8ef5cb046a838e7bda29c9d1a0849356fad5
Marianna Parzych <marianna.parzych@deepsense.ai>
2024-11-27 11:58:08 +0100

    fix line too long error

--- scripts/html/rendered_html_from_elements.py
+++ scripts/html/rendered_html_from_elements.py
@@ -3,9 +3,10 @@
 """
 Script to render HTML from unstructured elements.
 NOTE: This script is not intended to be used as a module.
-NOTE: For now script is only intended to be used with elements generated with `partition_html(html_parser_version=v2)`
+NOTE: For now script is only intended to be used with elements generated with
+      `partition_html(html_parser_version=v2)`
 TODO: It was noted that unstructured_elements_to_ontology func always returns a single page
-This script is using helper functions to handle multiple pages.
+      This script is using helper functions to handle multiple pages.
 """
 
 import argparse
