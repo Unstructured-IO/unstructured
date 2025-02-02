23fc0fb2603b75d037d22bf9850235675777bee9
cragwolfe <crag@unstructured.io>
2025-02-01 21:05:51 -0800

    add the other URL

--- unstructured/utils.py
+++ unstructured/utils.py
@@ -306,6 +306,7 @@ def scarf_analytics():
                     + "&gpu="
                     + str(gpu_present)
                     + "&dev=true",
+                    timeout=10,
                 )
             else:
                 requests.get(
@@ -334,6 +335,7 @@ def scarf_analytics():
                     + "&gpu="
                     + str(gpu_present)
                     + "&dev=false",
+                    timeout=10,
                 )
     except Exception:
         pass
