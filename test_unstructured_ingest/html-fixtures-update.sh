d1d29a3eac72977229c0a6a8ede5a07db67150db
Marek Połom <marek.polom@deepsense.ai>
2025-02-14 17:00:33 +0100

    generate html fixtures script

---
+++ test_unstructured_ingest/html-fixtures-update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define the input and output top directories
+INPUT_DIR="test_unstructured_ingest/expected-structured-output"
+OUTPUT_DIR="test_unstructured_ingest/expected-structured-output-html"
+PYTHON_SCRIPT="scripts/html/elements_json_to_html.py"
+EXCLUDE_IMG=0
+NO_GROUP=1
+
+# Function to process JSON files
+process_json_files() {
+    # Add flags based on the variables
+    cmd="python \"$PYTHON_SCRIPT\" \"$INPUT_DIR\" --outdir \"$OUTPUT_DIR\""
+    if [ "$EXCLUDE_IMG" -eq 1 ]; then
+        cmd+=" --exclude-img"
+    fi
+    if [ "$NO_GROUP" -eq 1 ]; then
+        cmd+=" --no-group"
+    fi
+    # Run the Python script with the constructed command
+    eval $cmd
+}
+
+# Start processing from the input directory
+process_json_files
