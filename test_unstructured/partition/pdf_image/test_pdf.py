d570f4624bb8c5dc75f0009775925fdaa40defb0
Philippe PRADOS <github@prados.fr>
2025-04-07 17:57:20 +0200

    Fix sort_page_element. ensures that sorting is stable and not random. (#3978)
    The sort_page_element() use the element id to sort the elements.
    Two executions of the same code, on the same file, produce different
    results. The order of the elements is random.
    This makes it impossible to write stable unit tests, for example, or to
    obtain reproducible results.

--- test_unstructured/partition/pdf_image/test_pdf.py
+++ test_unstructured/partition/pdf_image/test_pdf.py
@@ -1603,3 +1603,24 @@ def test_partition_pdf_with_specified_ocr_agents(mocker):
 
     assert spy.call_args_list[0][1] == {"language": "eng", "ocr_agent_module": OCR_AGENT_TESSERACT}
     assert spy.call_args_list[1][1] == {"language": "en", "ocr_agent_module": OCR_AGENT_PADDLE}
+
+
+def test_reproductible_pdf_loader():
+    from glob import glob
+
+    for f in glob(example_doc_path("pdf/layout-parser-paper.pdf")):
+        elements_1 = pdf.partition_pdf(
+            filename=f,
+            strategy=PartitionStrategy.AUTO,
+            infer_table_structure=False,
+        )
+        for _ in range(4):
+            elements_2 = pdf.partition_pdf(
+                filename=f,
+                strategy=PartitionStrategy.AUTO,
+                infer_table_structure=False,
+            )
+            for e1, e2 in zip(elements_1, elements_2):
+                assert e1.text == e2.text, f"load two time {f=} return differents results"
+            else:
+                break
