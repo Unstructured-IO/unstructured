7a802f8cffda7c781e416065f987f39f5cf8c60f
Yao You <yao@unstructured.io>
2025-04-28 12:23:30 -0500

    cat json file as new command requires str

--- test_unstructured_ingest/src/box.sh
+++ test_unstructured_ingest/src/box.sh
@@ -42,7 +42,7 @@ RUN_SCRIPT=${RUN_SCRIPT:-unstructured-ingest}
 PYTHONPATH=${PYTHONPATH:-.} "$RUN_SCRIPT" \
   box \
   --download-dir "$DOWNLOAD_DIR" \
-  --box-app-config "$BOX_APP_CONFIG_PATH" \
+  --box-app-config $(cat "$BOX_APP_CONFIG_PATH") \
   --remote-url box://utic-test-ingest-fixtures \
   --metadata-exclude coordinates,filename,file_directory,metadata.data_source.date_processed,metadata.data_source.filesize_bytes,metadata.last_modified,metadata.detection_class_prob,metadata.parent_id,metadata.category_depth \
   --num-processes "$max_processes" \
