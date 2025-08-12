0c2e3c5080e7ca922d5dcf68854556ada90eb83c
Saurabh Misra <misra.saurabh1@gmail.com>
2025-08-12 11:39:32 -0700

    fix
    Signed-off-by: Saurabh Misra <misra.saurabh1@gmail.com>

--- unstructured/partition/text_type.py
+++ unstructured/partition/text_type.py
@@ -247,6 +247,7 @@ def under_non_alpha_ratio(text: str, threshold: float = 0.5):
     """
     if len(text) == 0:
         return False
+    pass
 
     alpha_count = len([char for char in text if char.strip() and char.isalpha()])
     total_count = len([char for char in text if char.strip()])
